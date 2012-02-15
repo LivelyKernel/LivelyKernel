0afa786d34c42fbdec76101b90408485a5ab9de4
Lauritz Thamsen <lauritz.thamsen@googlemail.com>
2012-02-15 11:05:54 -0800

    removed uber-test module

--- core/lively/morphic/Tests.js
+++
@@ -1,3 +0,0 @@
-module('lively.morphic.Tests').requires('lively.morphic.tests.Morphic', 'lively.morphic.tests.Morphic2', 'lively.morphic.tests.PathTests', 'lively.morphic.tests.Connectors', 'lively.morphic.tests.DataGridTests', 'lively.morphic.tests.TabTests').toRun(function() {
-
-}) // end of module

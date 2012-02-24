ef859b415ba11036a558953ff059201f1d075956
Robert Krahn <robert.krahn@gmail.com>
2012-02-24 23:09:40 +0100

    script that can be invoked from a svn post-commit to sync to a git kernel branch. currently there are issues with the ssh authentication, otherwise it works fine

--- scripts/svn-sync.js
+++ scripts/svn-sync.js
@@ -1,7 +1,8 @@
-/*global require, process, console*/
+/*global require, process, console, setTimeout*/
 
 var optparse = require('optparse'),
     exec = require('child_process').exec,
+    fs = require('fs'),
     Seq = require('seq');
 
 var switches = [
@@ -10,22 +11,22 @@ var switches = [
     ['--svn-repo DIR', "Path to svn repository"],
     ['--svn-wc DIR', "Path to SVN workingcopy that is svn updated and used as a source for the sync"],
     ['--git-repo DIR', "local path to the git repository with the branch that mirrors the svn reository"],
-    ['--rev NUM', "svn revision that should be synced"]],
+    ['--rev NUM', "svn revision that should be synced"],
+    ['--lockfile FILE', "path to lock file used for synchronisation"]],
     parser = new optparse.OptionParser(switches),
-    svnRepo, svnWc, rev, targetDir, gitRepoDir;
+    svnRepo, svnWc, rev, targetDir, gitRepoDir, lockFile;
 
 function showHelpAndExit() { console.log(parser.toString()); process.exit(1); }
 parser.on("help", showHelpAndExit);
 parser.on("svn-repo", function(name, value) { svnRepo = value });
 parser.on("svn-wc", function(name, value) { svnWc = value });
 parser.on("rev", function(name, value) { rev = value });
-parser.on("git-repo-dir", function(name, value) { gitRepoDir = value });
+parser.on("git-repo", function(name, value) { gitRepoDir = value });
+parser.on("lockfile", function(name, value) { lockFile = value });
 
 parser.parse(process.argv);
 
-// if (!targetDir || !rev || !svnRepo || !svnWc || !gitRepoDir) showHelpAndExit();
-
-// --repository /etc/environments/svn_repositories/webwerkstatt --rev 140250
+if (!rev || !svnRepo || !svnWc || !gitRepoDir || !lockFile) showHelpAndExit();
 
 function checkIfCoreCommit(thenDo) {
     var next = this;
@@ -42,27 +43,73 @@ function checkIfCoreCommit(thenDo) {
 }
 
 function updateWebwerkstattWorkingCopy() {
+    var next = this;
     Seq().
         seq(function() {
             console.log('svn updating: ' + ['svn up', svnWc, '-r', rev].join(' '));
             exec(['svn up', svnWc, '-r', rev].join(' '), this);
         }).
-        seq(function(out) {
-            console.log('updated: ' + out);
-        });
+        seq(function(out) { console.log('updated: ' + out); next(); });
 }
 
 function rsyncWithGit() {
-    // Seq().
-    //     seq(function() {
-    //         console.log('svn updating: ' + ['svn up', svnWc, '-r', rev].join(' '));
-    //         exec(['svn up', svnWc, '-r', rev].join(' '), this);
-    //     }).
-    //     seq(function(out) {
-    //         console.log('updated: ' + out);
-    //     });
+    var next = this;
+    Seq().
+        seq(exec, ['rsync -ra --delete --exclude=".svn" --exclude="localconfig.js"',
+                   svnWc, gitRepoDir + '/core/'].join(' '), Seq).
+        seq(function() { console.log('sync done'); next(); });
+}
+
+function gitPush() {
+    var gitMirrorBranchName = 'ww-mirror',
+        next = this;
+    Seq().
+        seq(exec, ['git commit -am \'[mirror commit] {"rev": "' + rev + '"}\''].join(' '),
+            {cwd: gitRepoDir}, Seq).
+        seq(exec, ['git push origin', gitMirrorBranchName].join(' '),
+            {cwd: gitRepoDir, env: process.env}, Seq).
+        // seq(exec, 'ssh-agent ssh-add /home/robert/.ssh/webwerkstatt_mirror_rsa; git push origin ' + gitMirrorBranchName, {cwd: gitRepoDir}, Seq).
+        seq(function() { console.log('push done'); next(); });
+}
+
+function sshSetup() {
+    var next = this;
+    Seq().
+        seq(exec, 'eval `ssh-agent`; ssh-add /home/robert/.ssh/webwerkstatt_mirror_rsa', Seq).
+        seq(function() { console.log('ssh setup done'); next(); });
+}
+
+var timeout = Date.now() + 5000;
+function lock() {
+    if (Date.now() > timeout) {
+        console.error('lock file was not unlocked, abort mirror');
+        process.exit(1);
+    }
+    var next = this;
+    try {
+        fs.statSync(lockFile);
+        console.log('is locked, waiting');
+        setTimeout(function() { lock.call(next) }, 200);
+    } catch (e) {
+        // if error, means that file not there, we are ready to go
+        fs.writeFileSync(lockFile, 'locked by mirror script, rev ' + rev);
+        console.log('lock aquired');
+        next();
+    }
+}
+
+function unlock() {
+    var next = this;
+    Seq().
+        seq(fs.unlink, lockFile, Seq).
+        seq(function() { console.log('unlocked'); next(); });
 }
 
 Seq().
+    seq(lock).
     seq(checkIfCoreCommit).
-    seq(updateWebwerkstattWorkingCopy);
+    seq(updateWebwerkstattWorkingCopy).
+    seq(rsyncWithGit).
+    seq(sshSetup).
+    seq(gitPush).
+    seq(unlock);

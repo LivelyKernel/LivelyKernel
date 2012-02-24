56097bef8e6c5583f84bc0bf6049337ab418871d
Fabian Bornhofen <fbornhofen@googlemail.com>
2012-02-23 18:31:35 -0800

    part tests runnable

--- core/lively/PartsTestFramework.js
+++ core/lively/PartsTestFramework.js
@@ -11,11 +11,45 @@ TestCase.subclass('lively.PartTestCase', 'accessing', {
         var that = this;
         return this.allTestSelectors().collect(function(sel) {
                 debugger;
-                var testCase = new that.constructor(that.result, sel);
+                var testCase = new that.constructor(that.result, sel, that.getPartUnderTest());
                 testCase.addScript(that[sel]);
                 return testCase;
             });
     },
+    initialize: function($super, testResult, optTestSelector, partUnderTest) {
+        $super(testResult, optTestSelector);
+        this.setPartUnderTest(partUnderTest);
+    },
+    setPartUnderTest: function(partUnderTest) {
+        this.partUnderTest = partUnderTest;
+    },
+    getPartUnderTest: function() {
+        return this.partUnderTest;
+    },
+    runTest: function(aSelector) {
+        if (!this.shouldRun) return;
+        this.currentSelector = aSelector || this.currentSelector;
+
+        this.running();
+        try {
+            this.setUp();
+            this[this.currentSelector](this.getPartUnderTest());
+            this.addAndSignalSuccess();
+        } catch (e) {
+            this.addAndSignalFailure(e);
+         } finally {
+            try {
+                this.tearDown();
+            } catch(e) {
+                this.log('Couldn\'t run tearDown for ' + this.id() + ' ' + e);
+            }
+        }
+        return this.result;
+    },
+
+
+
+
 
 });
 
@@ -33,7 +67,7 @@ lively.morphic.Morph.addMethods('parts testing', {
         aFunction.asScriptOf(this.ensurePartTestsObject(), aFunction.name);
     }, 
     runPartTests: function() {
-        var testCase = new lively.PartTestCase(),
+        var testCase = new lively.PartTestCase(null, null, this),
             tests = this.ensurePartTestsObject();
         for (funcName in tests) {
             testCase.addScript(tests[funcName], funcName);

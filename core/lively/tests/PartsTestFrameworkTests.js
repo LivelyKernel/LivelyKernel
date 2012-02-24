3b9166b71ae9a780852c87d0e2a2f6354d679211
Fabian Bornhofen <fbornhofen@googlemail.com>
2012-02-24 13:22:43 -0800

    add tests for part tests

---
+++ core/lively/tests/PartsTestFrameworkTests.js
@@ -0,0 +1,46 @@
+module('lively.tests.PartsTestFrameworkTests').requires('lively.PartsTestFramework').toRun(function() {
+
+TestCase.subclass('lively.tests.PartsTestFrameworkTests.PartTestTest', 'testing', {
+
+    setUp: function() {
+        this.part = lively.morphic.Morph.makeRectangle(0, 0, 100, 100);
+        this.part.openInWorld();
+    },
+    tearDown: function() {
+        this.part.remove();
+    },
+    test01AddTestMethods: function() {
+        this.part.addPartTest(function test01() {});
+        this.part.addPartTest(function test02() {});
+        this.part.addPartTest(function test03() {});
+        this.assertEquals(Functions.own(this.part.getTestsObject()).length, 3, 
+                'part under test was expected to have 3 test methods');
+    },
+
+    test02AssertTrueAndFalse: function() {
+        var result;
+        this.part.addPartTest(function test01AssertTrue() {
+                this.assert(true, 'pass');
+            });
+        this.part.addPartTest(function test02AssertFalse() {
+                this.assert(false, 'fail');
+            });
+        result = this.part.runPartTests();
+        this.assertEquals(result.result.failed.length, 1, 'expected 1 test to fail');
+        this.assertEquals(result.result.succeeded.length, 1, 'expected 1 test to succeed');
+    },
+    test03PassPartAsArgument: function() {
+        var result;
+        this.part.setFill(Color.blue);
+        this.part.addPartTest(function test01RectangleIsBlue(aPart) {
+                        this.assert(aPart.getFill().equals(Color.blue), 
+                            'Rectangle was expected to be blue');
+                    });
+        result = this.part.runPartTests();
+        this.assertEquals(result.result.failed.length, 0, 'expected no tests to fail');
+        this.assertEquals(result.result.succeeded.length, 1, 'expected 1 test to succeed');
+    },
+
+});
+
+}) // end of module

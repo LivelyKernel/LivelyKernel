ab7a473c72d9d4c4c04136b344fe00070858e9a9
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-02-26 21:55:07 +0100

    [mirror commit] {   "rev": "141237",   "author": "cschuster",   "changes": "U   core/lively/ast/tests/AstTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/tests/AstTests.js\n" }

--- core/lively/ast/tests/AstTests.js
+++ core/lively/ast/tests/AstTests.js
@@ -208,7 +208,7 @@ TestCase.subclass('lively.ast.tests.AstTests.JSToAstTest',
         this.assertEquals(4, funcAst.body.children[0].trueExpr.children[0].expr.astIndex());
         this.assertEquals(5, funcAst.body.children[0].trueExpr.children[0].astIndex());
         this.assertEquals(6, funcAst.body.children[0].trueExpr.astIndex());
-        this.assertEquals(8, funcAst.body.children[0].falseExpr.astIndex());
+        this.assertEquals(7, funcAst.body.children[0].falseExpr.astIndex());
         this.assertEquals(9, funcAst.body.children[0].astIndex());
         this.assertEquals(10, funcAst.body.children[1].fn.astIndex());
         this.assertEquals(11, funcAst.body.children[1].astIndex());

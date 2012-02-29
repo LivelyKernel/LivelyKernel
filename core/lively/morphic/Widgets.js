5534253d402f1b04e6f3de354a8c52b0b4552875
jenslincke <lively-kernel@hpi.uni-potsdam.de>
2012-02-29 15:51:21 +0100

    [mirror commit] {   "rev": "142109",   "author": "jenslincke",   "changes": "U   core/lively/morphic/Widgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Widgets.js\n" }

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -698,7 +698,8 @@ lively.morphic.Box.subclass('lively.morphic.Menu',
     offsetForOwnerMenu: function() {
         var owner = this.ownerMenu,
             visibleBounds = this.world().visibleBounds(),
-            localVisibleBounds = this.ubMenuBoundsForVisibility(
+            localVisibleBounds = this.getTransform().inverse().transformRectToRect(visibleBounds),
+            newBounds = this.moveSubMenuBoundsForVisibility(
                 this.innerBounds(),
                 owner.overItemMorph ? owner.overItemMorph.bounds() : new Rectangle(0,0,0,0),
                 localVisibleBounds);

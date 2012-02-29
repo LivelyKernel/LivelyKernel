be8de8a51581d47aa4e44a13e65c3161214833d5
jenslincke <lively-kernel@hpi.uni-potsdam.de>
2012-02-29 15:49:46 +0100

    [mirror commit] {   "rev": "142104",   "author": "jenslincke",   "changes": "U   core/lively/morphic/Widgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Widgets.js\n" }

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -604,7 +604,7 @@ lively.morphic.Box.subclass('lively.morphic.Menu',
     },
 },
 'sub menu', {
-    openSubMenu: function(evt, name, items) {
+    openSubMenu: function (evt, name, items) {
         var m = new lively.morphic.Menu(null, items);
         this.addMorph(m);
         m.fitToItems()
@@ -612,9 +612,13 @@ lively.morphic.Box.subclass('lively.morphic.Menu',
         m.ownerMenu = this;
 
         // we do this twice because effect of fitToItems is delayed
+        m.setVisible(false); // we hide it because it is first shown at the wrong position
         m.offsetForOwnerMenu();
-        m.offsetForOwnerMenu.bind(m).delay(0);
+        (function() {
+            m.offsetForOwnerMenu()
+            m.setVisible(true);
 
+        }).delay(0);
         return m;
     },
     removeSubMenu: function() { if (this.subMenu) { var m = this.subMenu; m.ownerMenu = null; this.subMenu = null; m.remove() } },

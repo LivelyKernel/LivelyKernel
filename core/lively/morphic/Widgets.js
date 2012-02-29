d302a6fb14f91dcdddc3dcebe9345b446ffba697
jenslincke <lively-kernel@hpi.uni-potsdam.de>
2012-02-29 19:33:54 +0100

    [mirror commit] {   "rev": "142313",   "author": "jenslincke",   "changes": "U   core/lively/morphic/Widgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Widgets.js\n" }

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -2445,7 +2445,7 @@ Trait('SelectionMorphTrait',
         this.resetSelection()
         if (this.selectionMorph.owner !== this)
             this.addMorph(this.selectionMorph);
-        var pos = this.localize(this.eventStartPos);
+        var pos = this.localize(this.eventStartPos || evt.getPosition());
         this.selectionMorph.withoutPropagationDo(function() {
             this.selectionMorph.setPosition(pos)
             this.selectionMorph.setExtent(pt(1, 1))

9740706f23ddddde8b9ef35301acecf205caf5af
Robert Krahn <robert.krahn@gmail.com>
2012-03-01 21:54:01 +0100

    renamed no continous but a watch option, simpler to write

--- scripts/lk-jshint.js
+++ scripts/lk-jshint.js
@@ -10,15 +10,19 @@ var args = require('./helper/args'),
 var env = process.env,
     options = args.options([
     ['-h', '--help', 'show this help'],
-    ['-c', '--continuous', 'Run with nodemon and watch for file changes']]);
+    ['-w', '--watch', 'Run with nodemon and watch for file changes']]);
 
+
+// -=-=-=-=-=-=-=-=-=-=-
+// the real thing
+// -=-=-=-=-=-=-=-=-=-=-
 var filesToCheck = shell.files(env.MINISERVER_DIR)
                    .concat(shell.files(env.LK_TEST_SCRIPT_DIR, /js$/)),
     cmdAndArgs   = [env.JSHINT]
                    .concat(filesToCheck)
                    .concat(['--config', env.JSHINT_CONFIG]);
 
-if (options.defined('continuous')) {
+if (options.defined('watch')) {
   cmdAndArgs = [env.NODEMON, '--exec'].concat(cmdAndArgs);
 }
 

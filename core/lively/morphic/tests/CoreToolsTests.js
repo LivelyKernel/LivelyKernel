6cc9c0b6774f6dbc2462da5607cb046606d9c08a
fbo <lively-kernel@hpi.uni-potsdam.de>
2012-03-08 08:50:13 +0100

    [mirror commit] {   "rev": "144105",   "author": "fbo",   "changes": "UU  core/lively/morphic/tests/CoreToolsTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/CoreToolsTests.js\n" }

--- core/lively/morphic/tests/CoreToolsTests.js
+++ core/lively/morphic/tests/CoreToolsTests.js
@@ -5,16 +5,19 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.CoreToolsTests.Pa
     setUp: function($super) {
     },
     tearDown: function($super) {
+        if (this.publishDialog) {
+            this.publishDialog.remove();
+        }
     },
 },
 'testing', {
     test01OpenPublishDialog: function() {
-        var morph = new lively.morphic.Morph(),
-            publishDialog;
+        var morph = new lively.morphic.Morph();
         morph.openInWorld();
         morph.copyToPartsBinWithUserRequest();
-        publishDialog = $world.get('PublishPartDialog');
-        this.assert(publishPartDialog && publishDialog.isMorph, 'publish part dialog was expected to be in $world');
+        this.publishDialog = $world.get('PublishPartDialog');
+        this.assert(this.publishDialog && this.publishDialog.isMorph, 
+                'publish part dialog was expected to be in $world');
     },
 });
 

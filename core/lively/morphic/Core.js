bdd92543f22d3499a7ca19be523b9d23c921208d
bsiegmund <lively-kernel@hpi.uni-potsdam.de>
2012-03-09 14:52:40 +0100

    [mirror commit] {   "rev": "144607",   "author": "bsiegmund",   "changes": "U   core/lively/morphic/Core.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Core.js\n" }

--- core/lively/morphic/Core.js
+++ core/lively/morphic/Core.js
@@ -195,6 +195,7 @@ Object.subclass('lively.morphic.Morph',
 
         if (morph.isAncestorOf(this)) {
             alert('addMorph: Circular relationships between morphs not allowed');
+            laert('tried to drop ' + morph + ' on ' + this);
             return;
         }
 

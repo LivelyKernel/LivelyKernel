867049a12b913ca3c02bf2137505619f5c463167
bsiegmund <lively-kernel@hpi.uni-potsdam.de>
2012-03-09 15:04:27 +0100

    [mirror commit] {   "rev": "144612",   "author": "bsiegmund",   "changes": "U   core/lively/morphic/Events.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Events.js\n" }

--- core/lively/morphic/Events.js
+++ core/lively/morphic/Events.js
@@ -994,6 +994,7 @@ handleOnCapture);
             this.submorphs.invoke('remove')
         })
         shadow.setTransform(local ? this.getTransform() : this.getGlobalTransform());
+        shadow.disableDropping();
         //shadow.originalMorph = this;
         //this.grabShadow = shadow;
         return shadow;

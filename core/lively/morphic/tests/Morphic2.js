6e0c0ffc8927ae6143a736bbd7b0f0271a1ef419
athomschke <lively-kernel@hpi.uni-potsdam.de>
2012-03-16 14:52:13 +0100

    [mirror commit] {   "rev": "146455",   "author": "athomschke",   "changes": "U   core/lively/morphic/tests/Morphic2.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/Morphic2.js\n" }

--- core/lively/morphic/tests/Morphic2.js
+++ core/lively/morphic/tests/Morphic2.js
@@ -593,7 +593,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.Morphic2.HtmlPars
         this.assertEquals(node.textContent, "1<2", "pasting with & is broken");
     },
     testSanitizeNodeWithLt3: function() {
-        var s = '<span>&lt;</span>';
+        var s = '<span><</span>';
         var node = lively.morphic.HTMLParser.sourceToNode(s);
         lively.morphic.HTMLParser.sanitizeNode(node);
         this.assertEquals(node.textContent, "<", "pasting with < is broken");

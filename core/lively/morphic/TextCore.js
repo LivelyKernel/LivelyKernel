3f262f96a563793a81bad3462fd46460b9d4ac7b
conradcalmez <lively-kernel@hpi.uni-potsdam.de>
2012-03-21 14:13:48 +0100

    [mirror commit] {   "rev": "147082",   "author": "conradcalmez",   "changes": "U   core/lively/morphic/TextCore.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/TextCore.js\n" }

--- core/lively/morphic/TextCore.js
+++ core/lively/morphic/TextCore.js
@@ -1006,7 +1006,7 @@ lively.morphic.Morph.subclass('lively.morphic.Text', Trait('ScrollableTrait'), T
 
         // restore selection range on focus
         var s = this.savedSelectionRange;
-        if (s && (s[0] <= a[0] && s[1] >= s[1])) { 
+        if (s && (s[0] <= a[0] && s[1] >= a[1])) { 
             this.setSelectionRange(s[0], s[1]);
             delete this.savedSelectionRange;
             evt.stop();

26ebc087b165f98fc1cc8ee2a0cd197ded54d77b
athomschke <lively-kernel@hpi.uni-potsdam.de>
2012-03-23 01:36:52 +0100

    [mirror commit] {   "rev": "147250",   "author": "athomschke",   "changes": "U   core/lively/PartsBin.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/PartsBin.js\n" }

--- core/lively/PartsBin.js
+++ core/lively/PartsBin.js
@@ -328,7 +328,7 @@ Object.subclass('lively.PartsBin.PartItem',
         //if there is a PartsBin representation, this returns true. If a Part was deleted from PartsBin, but an artifact of is published, this returns false
         return (new WebResource(this.getFileURL())).exists()
     },
-    newMethod: function() {
+    handleSaveStatus: function(status) {
         // enter comment here
     },
 

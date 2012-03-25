71c1675ad3e76f714d698c00c07fb912def4130c
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-03-25 22:46:48 +0200

    [mirror commit] {   "rev": "147840",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/tests/TabTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/TabTests.js\n" }

--- core/lively/morphic/tests/TabTests.js
+++ core/lively/morphic/tests/TabTests.js
@@ -31,10 +31,14 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.TabTests.TabTests
         this.assertEquals(this.tabContainer.getExtent().y, extent.y-tabBarExtent.y, 'TabContainer extent did not shrink when hiding tab bar');
     },
     test04GetActiveTab: function() {
-        var t1 = this.tabContainer.addTabLabeled('Tab1'),
+        var bar = this.tabContainer.getTabBar(),
+            t1 = this.tabContainer.addTabLabeled('Tab1'),
             t2 = this.tabContainer.addTabLabeled('Tab2');
-        
-        this.tabContainer.topSubmorph()
+        bar.activateTab(t1);
+        bar.activateTab(t2);
+        this.assert(t2.isActive, 't2 not active');
+        this.assert(!t1.isActive, 't1 is active?');
+        this.assertEquals(t2.getPane(), this.tabContainer.topSubmorph());
     },
 
 

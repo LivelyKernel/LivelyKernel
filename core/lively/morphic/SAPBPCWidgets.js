0b589bbb5f1bddebc682b35de309401850f29ef8
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-03-25 22:16:13 +0200

    [mirror commit] {   "rev": "147826",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPBPCWidgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPBPCWidgets.js\n" }

--- core/lively/morphic/SAPBPCWidgets.js
+++ core/lively/morphic/SAPBPCWidgets.js
@@ -211,7 +211,8 @@ lively.morphic.Morph.subclass('lively.morphic.SAPDataGrid',
             var arrColumns = {};
             for (var i = 0; i < that.numCols; i++) {
                 if (that.colNames[i] != undefined) {
-					arrColumns[i]= ea[i].getContent();
+                    debugger;
+		  arrColumns[i]= ea[i].getContent();
                     //obj[that.colNames[i]] = ea[i].getContent();
                 }
             }

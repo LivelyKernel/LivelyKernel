17587be5b3fe7cd9828a9b34a61dff0bd1cf1e63
huberthesse <lively-kernel@hpi.uni-potsdam.de>
2012-03-27 10:52:18 +0200

    [mirror commit] {   "rev": "147984",   "author": "huberthesse",   "changes": "U   core/lively/morphic/Core.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Core.js\n" }

--- core/lively/morphic/Core.js
+++ core/lively/morphic/Core.js
@@ -216,12 +216,13 @@ Object.subclass('lively.morphic.Morph',
         if (tfm) {
             morph.setTransform(tfm);
         }
-
+        console.log("before context: " + morph.getExtent())
         var parentRenderCtxt = this.renderContext(),
             subRenderCtxt = morph.renderContext(),
             ctx = parentRenderCtxt.constructor !== subRenderCtxt.constructor ?
                 parentRenderCtxt.newForChild() : subRenderCtxt;
         morph.renderAfterUsing(ctx, optMorphBefore);
+        console.log("after context: " + morph.getExtent())
 
         morph.resumeSteppingAll();
 

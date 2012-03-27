d5d4271b56feae96ae49b60f24ebadc9ca42c210
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-03-27 16:29:41 +0200

    [mirror commit] {   "rev": "148154",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/HTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTML.js\n" }

--- core/lively/morphic/HTML.js
+++ core/lively/morphic/HTML.js
@@ -859,6 +859,7 @@ debugger;
             width = $element.width() || 0,
             height = $element.height() || 0,
             extent = pt(width || height);
+alert(extent);
         this.setExtent(extent);
         this.shapeNode = element;
     },

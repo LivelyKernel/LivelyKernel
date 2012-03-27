484166e365950db07d25133b1bb06f413473171e
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-03-27 16:03:23 +0200

    [mirror commit] {   "rev": "148105",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/tests/ShapeTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/ShapeTests.js\n" }

--- core/lively/morphic/tests/ShapeTests.js
+++ core/lively/morphic/tests/ShapeTests.js
@@ -1,5 +1,12 @@
 module('lively.morphic.tests.ShapeTests').requires('lively.TestFramework').toRun(function() {
 
+TestCase.subclass('lively.morphic.tests.ShapeTests.ExternalShapeTest',
+'testing', {
+
+    test01InitExternalShapeSetsExtent: function() {
+
+    },
+})
 
 
 }) // end of module

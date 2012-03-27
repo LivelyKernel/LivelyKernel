98cdcff7f40c44492a8f121858aa187b5b14a9e6
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-03-27 16:16:32 +0200

    [mirror commit] {   "rev": "148148",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/tests/ShapeTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/ShapeTests.js\n" }

--- core/lively/morphic/tests/ShapeTests.js
+++ core/lively/morphic/tests/ShapeTests.js
@@ -7,6 +7,7 @@ TestCase.subclass('lively.morphic.tests.ShapeTests.ExternalShapeTest',
         var elem = document.createElement('input'),
             shape = new lively.morphic.Shapes.External(elem);
         shape.stringifiedShapeNode = '<input xmlns="http://www.w3.org/1999/xhtml" type="checkbox" style="width: 3px; height: 15px; " />';
+debugger
         shape.initFromStringifiedShapeNode();
         this.assertEquals(pt(3, 15), shape.getExtent());
     },

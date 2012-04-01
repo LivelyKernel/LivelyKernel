a32b48476722f86702bb135bf844e9511df108f2
fbo <lively-kernel@hpi.uni-potsdam.de>
2012-04-01 12:57:33 +0200

    CoreToolsTests
    [mirror commit]
    {
      "rev": "144105",
      "author": "fbo",
      "changes": "UU  core/lively/morphic/tests/CoreToolsTests.js\n",
      "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/CoreToolsTests.js\n"
    }
    
    [mirror commit]
    {
      "rev": "144104",
      "author": "fbo",
      "changes": "A   core/lively/morphic/tests/CoreToolsTests.js\n",
      "msg": "add core tools tests\n"
    }

---
+++ core/lively/morphic/tests/CoreToolsTests.js
@@ -0,0 +1,24 @@
+module('lively.morphic.tests.CoreToolsTests').requires('lively.morphic.tests.Morphic').toRun(function() {
+
+lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.CoreToolsTests.PartsBinTests',
+'running', {
+    setUp: function($super) {
+    },
+    tearDown: function($super) {
+        if (this.publishDialog) {
+            this.publishDialog.remove();
+        }
+    },
+},
+'testing', {
+    test01OpenPublishDialog: function() {
+        var morph = new lively.morphic.Morph();
+        morph.openInWorld();
+        morph.copyToPartsBinWithUserRequest();
+        this.publishDialog = $world.get('PublishPartDialog');
+        this.assert(this.publishDialog && this.publishDialog.isMorph, 
+                'publish part dialog was expected to be in $world');
+    },
+});
+
+}) // end of module

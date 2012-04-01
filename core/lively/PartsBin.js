63482c8a9e56bd9dae8b987713a8e5d3cd19fb77
meineerde <lively-kernel@hpi.uni-potsdam.de>
2012-04-01 12:45:49 +0200

    meineerde, encoding parts name
    [mirror commit]
    {
      "rev": "142304",
      "author": "meineerde",
      "changes": "U   core/lively/PartsBin.js\n",
      "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/PartsBin.js\n"
    }
    
    [mirror commit]
    {
      "rev": "142302",
      "author": "meineerde",
      "changes": "U   core/lively/PartsBin.js\n",
      "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/PartsBin.js\n"
    }

--- core/lively/PartsBin.js
+++ core/lively/PartsBin.js
@@ -43,7 +43,7 @@ Object.subclass('lively.PartsBin.PartItem',
     },
 
     getPartsSpace: function() {
-        return lively.PartsBin.partsSpaceNamed(this.escapePartName(this.partsSpaceName));
+        return lively.PartsBin.partsSpaceNamed(this.partsSpaceName);
     },
 
     setPartFromJSON: function(json, metaInfo, rev) {
@@ -423,7 +423,7 @@ Object.subclass('lively.PartsBin.PartsSpace',
     },
 
 
-    getURL: function() { return URL.ensureAbsoluteRootPathURL(this.name).asDirectory() },
+    getURL: function() { return URL.ensureAbsoluteRootPathURL(encodeURI(this.name)).asDirectory() },
     getPartItems: function() {
         return Properties.ownValues(this.partItems);
     },

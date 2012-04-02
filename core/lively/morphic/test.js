8ebb9d6f86d6983318546e7029ded7e4748f353e
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-02 20:36:36 +0200

    [mirror commit] {   "rev": "149394",   "author": "kimhw",   "changes": "U   core/lively/morphic/test.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/test.js\n" }

--- core/lively/morphic/test.js
+++ core/lively/morphic/test.js
@@ -1,5 +1,15 @@
 module('lively.morphic.test').requires().toRun(function() {
 
-// Enter your code here
+lively.morphic.Morph.subclass('lively.morphic.testRec',
+'method category', {
+    initialize: function($super) {
+        $super(new lively.morphic.Shapes.Rectangle(new Rectangle(0,0,100,100)))
+        this.setFill(Color.red)
+    },
+    addMorph: function($super, morph) {
+        $super(morph);
+    },
+    newMethod: function() {},
+});
 
 }) // end of module

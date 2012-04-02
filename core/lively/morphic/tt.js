030cc398104c417880f80fad9db15e9a45553cd2
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-02 19:58:40 +0200

    [mirror commit] {   "rev": "149390",   "author": "kimhw",   "changes": "U   core/lively/morphic/tt.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tt.js\n" }

--- core/lively/morphic/tt.js
+++ core/lively/morphic/tt.js
@@ -1,5 +1,15 @@
 module('lively.morphic.tt').requires().toRun(function() {
 
-// Enter your code here
+lively.morphic.Morph.subclass('lively.morphic.testxx',
+'method category', {
+    initialize: function($super) {
+        $super(new lively.morphic.Shapes.Rectangle(new Rectangle(0,0,100,100)))
+        this.setFill(Color.red)
+    },
+    addMorph: function($super, morph) {
+        $super(morph);
+    },
+    newMethod: function() {},
+});
 
 }) // end of module

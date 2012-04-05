777db60f542a27867d9b315a45e96ae3d0953b46
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-05 18:38:06 +0200

    [mirror commit] {   "rev": "150076",   "author": "kimhw",   "changes": "U   core/lively/morphic/testPerformance.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/testPerformance.js\n" }

--- core/lively/morphic/testPerformance.js
+++ core/lively/morphic/testPerformance.js
@@ -1,5 +1,12 @@
 module('lively.morphic.testPerformance').requires().toRun(function() {
 
-// Enter your code here
+ initialize: function($super) {
+        $super(new lively.morphic.Shapes.Rectangle(new Rectangle(0,0,100,100)))
+        this.setFill(Color.red)
+    },
+    addMorph: function($super, morph) {
+        $super(morph);
+    },
+    newMethod: function() {},
 
 }) // end of module

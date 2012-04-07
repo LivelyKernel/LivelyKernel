21e5bc57046046b6b1b3181e09726e312fd2287a
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-07 06:11:48 +0200

    [mirror commit] {   "rev": "150363",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWidgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWidgets.js\n" }

--- core/lively/morphic/SAPWidgets.js
+++ core/lively/morphic/SAPWidgets.js
@@ -734,38 +734,7 @@ lively.morphic.Text.subclass('lively.morphic.SAPGridAnnotation',
     addToGrid: function(aGrid) {
         this.grid = aGrid;
         this.grid.addMorph(this);
-    },
-    /*onKeyPress: function($super, evt) {
-       console.log("SAPGridAnnotation.onKeyPress");
-        $super(evt);
-        this.textString += String.fromCharCode(evt.getKeyCode());
-    },
-    onBackspacePressed: function($super, evt) {
-        $super(evt);
-        if (!this.textString) {
-            evt.stop(); 
-            return true; 
-        }
-        this.textString = this.textString.substring(0, this.textString.length-1);
-        evt.stop();
-    },*/
-    onMouseDown: function (evt) {
-    //debugger;
-        if (evt.isLeftMouseButtonDown()) {
-            this.displayExpression();
-        }
-    },
-    displayExpression: function() {
-        if (this.evalExpression !== undefined) {
-            this.textString = '=' + this.evalExpression;
-        }
-    },
-
-    put: function(aValue) {
-        console.log("Annotation.put")
-        this.textString = aValue;
-    },
-
+    }
 
 });
 

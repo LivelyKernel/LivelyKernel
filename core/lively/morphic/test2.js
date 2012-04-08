94fee3eaf5e0f659d260f5461e7fe2f9576c3d97
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-08 04:27:44 +0200

    [mirror commit] {   "rev": "150415",   "author": "kimhw",   "changes": "U   core/lively/morphic/test2.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/test2.js\n" }

--- core/lively/morphic/test2.js
+++ core/lively/morphic/test2.js
@@ -1,5 +1,40 @@
 module('lively.morphic.test2').requires().toRun(function() {
 
-// Enter your code here
+lively.morphic.Morph.subclass('lively.morphic.test2',
+'method category', {
+    initialize: function($super,nNumofRecs) {
+        $super();
+        this.numofRecs = nNumofRecs;
+        this.addTextField()
+    },
+    addTextField: function() {
+        var start = new Date().getTime();    
+
+        var oRec;
+        
+         for (var n= 0; n< this.numofRecs; n++) {
+            oRec = new lively.morphic.testText();
+            oRec.addToMorph(this);
+        }
+        var elapsed = new Date().getTime() - start;
+	elapsed = elapsed/1000;
+	console.log('Time =' + elapsed);
+        alert("done Time to add " +  this.numofRecs + " rectangles took "+ elapsed + " sec.");
+    },
+});
+
+lively.morphic.Text.subclass('lively.morphic.testText',
+'method category', {
+    initialize: function($super,arg1, arg2) {
+        $super(arg1, arg2);
+        this.setFill(Color.rgb(255, 255, 225));
+        this.setBorderColor(Color.rgb(0,0,0));
+    },
+     addToMorph: function(oParent) {
+        this.oParent = oParent;
+        this.oParent.addMorph(this);
+    },
+});
+
 
 }) // end of module

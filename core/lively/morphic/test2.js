074f7b9094a1387d1695d49a530585891b88e8ae
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-08 05:08:12 +0200

    [mirror commit] {   "rev": "150450",   "author": "kimhw",   "changes": "U   core/lively/morphic/test2.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/test2.js\n" }

--- core/lively/morphic/test2.js
+++ core/lively/morphic/test2.js
@@ -35,18 +35,4 @@ lively.morphic.Morph.subclass('lively.morphic.testText',
     },
 });
 
-lively.morphic.Text.subclass('lively.morphic.testText2',
-'method category', {
-    initialize: function($super,arg1, arg2) {
-        $super(arg1, arg2);
-        this.setFill(Color.rgb(255,0,0));
-        this.setBorderColor(Color.rgb(0,0,0));
-        this.setExtent(lively.pt(50,100));
-    },
-     addToMorph: function(oParent) {
-        this.oParent = oParent;
-        this.oParent.addMorph(this);
-    },
-});
-
 }) // end of module

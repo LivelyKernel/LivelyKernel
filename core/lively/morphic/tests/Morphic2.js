cef0260eea6d3e74084555430956a3085b2825f9
athomschke <lively-kernel@hpi.uni-potsdam.de>
2012-04-09 16:29:06 +0200

    [mirror commit] {   "rev": "150718",   "author": "athomschke",   "changes": "U   core/lively/morphic/tests/Morphic2.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/Morphic2.js\n" }

--- core/lively/morphic/tests/Morphic2.js
+++ core/lively/morphic/tests/Morphic2.js
@@ -645,7 +645,8 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.DiffMergeTests',
 
 
     testFindCurrentPartVersion: function() {
-        var m1 = $world.loadPartItem('Rectangle', '/PartsBin/Basic');
+        var m1 = Morph.makeRectangle(0,0,100,100);
+        m1.getPartsBinMetaInfo().revisionOnLoad = 2;
         this.assertEquals(m1.getPartsBinMetaInfo().revisionOnLoad, m1.findCurrentPartVersion().getPartsBinMetaInfo().revisionOnLoad, 'Wrong revision number')
     },
 

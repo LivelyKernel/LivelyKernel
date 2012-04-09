e29b74e3b568465441f057f08e4cda269083c900
athomschke <lively-kernel@hpi.uni-potsdam.de>
2012-04-09 17:39:08 +0200

    [mirror commit] {   "rev": "150776",   "author": "athomschke",   "changes": "U   core/lively/morphic/tests/Morphic2.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/Morphic2.js\n" }

--- core/lively/morphic/tests/Morphic2.js
+++ core/lively/morphic/tests/Morphic2.js
@@ -754,7 +754,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.DiffMergeTests',
         this.assertEquals(m6.findSiblingInRelative(m7, m4), m7, 'Wrong sibling with real parent');
         this.assertEquals(m6.submorphs[0].findSiblingInRelative(m7, m4), m7.submorphs[0], 'Wrong submorphs sibling with real parent')
         this.assertEquals(m8.findSiblingInRelative(m7, m4), m7, 'Wrong sibling with grand parent')
-        //this.assert(!m6.submorphs[0].findSiblingInRelative(m7, m3), 'Wrong submorph sibling with grand parent')
+        this.assert(!m8.submorphs[0].findSiblingInRelative(m7, m4), 'Wrong submorph sibling with grand parent')
     },
 
     testFindCommonParentPartVersion: function() {

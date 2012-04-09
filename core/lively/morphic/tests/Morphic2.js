a133f261bbca629295454b9e9ee5a78cb25f2bf3
athomschke <lively-kernel@hpi.uni-potsdam.de>
2012-04-09 16:51:00 +0200

    [mirror commit] {   "rev": "150735",   "author": "athomschke",   "changes": "U   core/lively/morphic/tests/Morphic2.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/Morphic2.js\n" }

--- core/lively/morphic/tests/Morphic2.js
+++ core/lively/morphic/tests/Morphic2.js
@@ -679,7 +679,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.DiffMergeTests',
         // simulate copyToPartsBin
         var pbv = m1.copy();
         // simulate copyFromPartsBin
-        var m2 = pbv.copy();
+        var m2 = pbv.copy().copy();
 
         this.assert(m2.findDerivationParent(m1), "No parent found.")
         this.assert(m2.findDerivationParent(m1) === m1, "Wrong parent found 1.")

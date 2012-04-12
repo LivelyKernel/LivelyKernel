80df85dd03b01357ef991f0222105477dadbae99
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-13 01:00:23 +0200

    [mirror commit] {   "rev": "151783",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWidgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWidgets.js\n" }

--- core/lively/morphic/SAPWidgets.js
+++ core/lively/morphic/SAPWidgets.js
@@ -30,7 +30,7 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGrid',
 
     },
     updateRowDisplay: function(evt) {
-       
+       debugger;
         //var nScrollValue = parseInt(evt);
         if (isNaN(evt)){
             console.log("nan " + evt);

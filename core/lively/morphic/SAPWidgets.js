9c0f454d036f543a64dffcef8b09f4be1874275f
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-13 04:43:33 +0200

    [mirror commit] {   "rev": "151845",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWidgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWidgets.js\n" }

--- core/lively/morphic/SAPWidgets.js
+++ core/lively/morphic/SAPWidgets.js
@@ -181,6 +181,8 @@ console.log('End createLayout =' + elapsed);
     },
     setAnnotation: function(nColumn,nRow,sText) {
         this.at(nColumn,nRow).annotation = sText;
+        //this.arrData[nRow][nColumn].annotation = sText;
+
         
     },
 

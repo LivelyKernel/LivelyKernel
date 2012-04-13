017ab9683cb6351a4bf71db002761badf00f8544
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-13 02:39:12 +0200

    [mirror commit] {   "rev": "151805",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWidgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWidgets.js\n" }

--- core/lively/morphic/SAPWidgets.js
+++ core/lively/morphic/SAPWidgets.js
@@ -83,6 +83,7 @@ console.log("nEndRow  " + nStartRow);
         this.dataModel = [];
         this.addScript(function renderFunction(value) { return value; });
         //looping takes about 0.4 sec.  maybe we should add rows when scroll is moving.
+        debugger;
         for (var nRow = 0; nRow < this.maxNoofRow ; nRow++) {
 		var arrColumns=[];
 		for (var nCol = 0; nCol < this.maxNoofColumn ; nCol++) {

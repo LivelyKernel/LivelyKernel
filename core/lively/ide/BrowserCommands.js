f783566d9790ce9c34f3744df512d95ef25135d2
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-04-14 22:07:00 +0200

    [mirror commit] {   "rev": "152249",   "author": "robertkrahn",   "changes": "U   core/lively/ide/BrowserCommands.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ide/BrowserCommands.js\n" }

--- core/lively/ide/BrowserCommands.js
+++ core/lively/ide/BrowserCommands.js
@@ -395,8 +395,7 @@ lively.ide.BrowserCommand.subclass('lively.ide.ClassHierarchyViewCommand', {
         //listPane.innerMorph().updateList(list);
         // w.addFramedMorph(listPane, klass.type + ' and its subclasses');
         var asText = list.pluck('string').join('\n');
-            text = new lively.morphic.Text(new Rectangle(0,0, 200, 300), asText);
-        w.addFramedMorph(text, klass.type + ' and its subclasses');
+        w.addTextWindow({title: klass.type + ' and its subclasses', content: asText})
     },
 
 });

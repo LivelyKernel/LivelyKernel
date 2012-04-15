6f128c10eb61780e2e9d8a438cedc574481d0039
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-16 01:29:41 +0200

    [mirror commit] {   "rev": "152290",   "author": "kimhw",   "changes": "U   core/lively/morphic/test_1.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/test_1.js\n" }

--- core/lively/morphic/test_1.js
+++ core/lively/morphic/test_1.js
@@ -1,5 +1,36 @@
 module('lively.morphic.test_1').requires().toRun(function() {
 
-// Enter your code here
+lively.morphic.Morph.subclass('lively.morphic.testPerform',
+'method category', {
+    initialize: function($super,nNumofRecs) {
+        $super();
+        this.arrData=[];
+        this.numofRecs = nNumofRecs;
+        this.drawRecs()
+    },
+    drawRecs: function() {
+        var oRec;
+        oRec = new lively.morphic.rectMorph();
+        oRec.addToMorph(this);
+    },
+    setData: function(aJsArray) {
+        this.clear();
+        this.dataModel = [];
+
+        var nRow;
+        var nCol;
+        var arrColumns=[];
+
+        for (nRow = 0; nRow < aJsArray.length; nRow++) {
+	   for (nCol = 0; nCol < aJsArray[nRow].length ; nCol++) {
+		this.arrData[nRow][nCol].value=aJsArray[nRow][nCol];
+	   }
+	}
+       
+      
+    },
+});
+
+
 
 }) // end of module

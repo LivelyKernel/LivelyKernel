0ad2823f9dd06c36364ad704df51bbd6b71ed49b
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-16 19:24:01 +0200

    [mirror commit] {   "rev": "152589",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -411,7 +411,7 @@ console.log('End createLayout =' + elapsed);
                     x < this.numCols; x++) {
                 nOrgRow = y  + this.startRow;
                 nOrgCol = x + this.startColumn;
-                
+                console.log("nOrgRow,nOrgCol : " +this.startColumn+ "," + this.startRow);
                 if (this.arrData[nOrgRow][nOrgCol].annotation){
                     this.at(x,y).annotationCell();
                     console.log("annotationCell: " +nOrgCol+ "," + nOrgRow);

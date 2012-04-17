09d6aa4cc67113e17f7c283844c3450c59b4d984
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-17 19:22:40 +0200

    [mirror commit] {   "rev": "152936",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -462,7 +462,7 @@ console.log('End createLayout =' + elapsed);
         var nOrgCol;
 debugger;
 var start = new Date().getTime();
-        for (var y = 1; y < this.dataModel.length &&
+        for (var y = 0; y < this.dataModel.length &&
                 y < this.numRows; y++) {
             for (var x = 0; x < this.dataModel[y].length &&
                     x < this.numCols; x++) {
@@ -483,7 +483,7 @@ var start = new Date().getTime();
                     this.at(x,y).setBorderStyle("dotted");
                     sValue = this.parseFormula(sValue);
                 }
-                this.at(x,y).textString = sValue;
+                this.at(x,y+1).textString = sValue;
             }
         }
         //debugger;

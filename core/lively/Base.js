d019ff1da75eba29a42a1cadbdd2a9f4ecc604bc
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-04-17 11:10:00 +0200

    [mirror commit] {   "rev": "152698",   "author": "robertkrahn",   "changes": "U   core/lively/Base.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/Base.js\n" }

--- core/lively/Base.js
+++ core/lively/Base.js
@@ -1083,7 +1083,7 @@ function logStack() {
 (function setupjQuery(Global) {
     var lively = Global.lively,
         jQuery = Global.jQuery;
-    lively.$ = jQuery.noConflict();
+    lively.$ = jQuery.noConflict(/*true -- really removes $*/);
 })(Global);
 
 namespace('lively');

d7d580323971fd565ac60e66369edd61ac8e06a4
Robert Krahn <robert.krahn@gmail.com>
2012-04-18 23:14:03 +0200

    extending #setClipMode: Paramter can either be a string or an object. String: "visible", "hidden", "auto", "scroll" Object: should have x and/or y field with a string og the choices above.

--- core/lively/morphic/tests/Morphic.js
+++ core/lively/morphic/tests/Morphic.js
@@ -1674,34 +1674,6 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.SelectionTest',
 
 lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.RenderingTest',
 'testing', {
-    test01NodeIsInDOM: function() {
-        var m1 = lively.morphic.Morph.makeRectangle(0,0,100,100),
-            m2 = lively.morphic.Morph.makeRectangle(0,0,100,100);
-        m1.addMorph(m2);
-        var domInterface = m1.renderContext().domInterface;
-        this.assert(!domInterface.isInDOM(m1.renderContext().morphNode), 'm1 node in DOM?')
-        this.assert(!domInterface.isInDOM(m2.renderContext().morphNode), 'm2 node in DOM?')
-        this.world.addMorph(m1)
-        this.assert(domInterface.isInDOM(m1.renderContext().morphNode), 'm1 node not in DOM?')
-        this.assert(domInterface.isInDOM(m2.renderContext().morphNode), 'm2 node not in DOM?')
-    },
-    test02RenderCrossBrowserLinearGradient: function() {
-        var gradient = new lively.morphic.LinearGradient(
-            [{offset: 0, color: Color.red}, {offset: 0.8, color: Color.green}], "northSouth");
-            expectedWebkit = '-webkit-gradient(linear, 0% 0%, 0% 100%,color-stop(0, rgb(204,0,0)),color-stop(0.8, rgb(0,204,0)))',
-            expectedFirefox = '-moz-linear-gradient(90deg, rgb(204,0,0) 0%, rgb(0,204,0) 80%)';
-            webkitResult = gradient.toCSSString(new Rectangle(0,0,100,100), '-webkit-'),
-            firefoxResult = gradient.toCSSString(new Rectangle(0,0,100,100), '-moz-');
-        this.assert(expectedWebkit == webkitResult || expectedFirefox == firefoxResult, 'browser does not render gradients correctly');
-    },
-    test02bRenderCrossBrowserRadialGradient: function() {
-        var gradient = new lively.morphic.RadialGradient(
-            [{offset: 0, color: Color.red}, {offset: 0.8, color: Color.green}], pt(0.5,0.3));
-            expectedWebkit = "-webkit-gradient(radial,50%30%,0,50%50%,50,color-stop(0,rgba(204,0,0,1)),color-stop(0.8,rgba(0,204,0,1)))",
-            expectedFirefox = '-moz-radial-gradient(50% 50%, circle cover, rgb(204,0,0) 0%, rgb(0,204,0) 80%)'
-            webkitResult = gradient.toCSSString(new Rectangle(0,0,100,100), '-webkit-'),
-            firefoxResult = gradient.toCSSString(new Rectangle(0,0,100,100), '-moz-');
-        this.assert(expectedWebkit == webkitResult.replace(/\s/g, '') || expectedFirefox == firefoxResult, 'browser does not render gradients correctly');
     },
 
 

cf34a1ee257160a5abe6a80c01b67971b71db6dd
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-04-19 02:30:39 +0200

    [mirror commit] {   "rev": "153301",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/HTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTML.js\n" }

--- core/lively/morphic/HTML.js
+++ core/lively/morphic/HTML.js
@@ -184,6 +184,7 @@ lively.morphic.Morph.addMethods(
     setClipModeHTMLForNode: function(ctx, node, state) {
         if (!node) return;
         var style = node.style;
+debugger;
 console.log(state);
         if (typeof state === "string") {
             style.removeProperty('overflow-x');

fe0e11b3ce9f8e024a82d5260ecc9e6abf5fbde5
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-19 21:09:48 +0200

    [mirror commit] {   "rev": "153990",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -72,6 +72,7 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGrid',
 	   }
            this.dataModel.push(arrColumns);
         }
+        debugger;
         this.updateDisplay();
     },
     //fires when horizontal scroll moves

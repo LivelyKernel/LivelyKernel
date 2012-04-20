5d0ce76d5159265e50bd901006bc34f32942756a
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-04-20 22:49:40 +0200

    [mirror commit] {   "rev": "154203",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/Core.js\n",   "msg": "withAllSubmorphsDetect\n\n" }

--- core/lively/morphic/Core.js
+++ core/lively/morphic/Core.js
@@ -239,19 +239,29 @@ Object.subclass('lively.morphic.Morph',
     withAllSubmorphsDo: function(func, context, depth) {
         if (!depth) depth = 0;
         func.call(context || Global, this, depth);
-        for (var i = 0; i < this.submorphs.length; i++)
+        for (var i = 0; i < this.submorphs.length; i++) {
             this.submorphs[i].withAllSubmorphsDo(func, context, depth + 1);
+        }
     },
     withAllSubmorphsSelect: function(func, context, depth) {
         if (!depth) depth = 0;
         var res = [];
-        if (func.call(context || Global, this, depth)) {
-            res.push(this);
-        }
+        if (func.call(context || Global, this, depth)) { res.push(this); }
         for (var i = 0; i < this.submorphs.length; i++) {
             res.pushAll(this.submorphs[i].withAllSubmorphsSelect(func, context, depth + 1));
         }
         return res;
+    },
+
+    withAllSubmorphsDetect: function(func, context, depth) {
+        if (!depth) depth = 0;
+        var res = [];
+        if (func.call(context || Global, this, depth)) { return this; }
+        for (var i = 0; i < this.submorphs.length; i++) {
+            var found = this.submorphs[i].withAllSubmorphsDetect(func, context, depth + 1);
+            if (found) return found;
+        }
+        return null;
     },
 
     submorphBounds: function(tfm) {
@@ -575,7 +585,7 @@ lively.morphic.Morph.subclass('lively.morphic.World',
     } else {
         width = topmost.clientWidth * scale;
         height = topmost.clientHeight * scale;
-    } 
+    }
     return topLeft.scaleBy(scale).extent(pt(width, height));
     },
 

759470f07bc75382a8563795a758a4caba6355bb
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-21 23:48:37 +0200

    [mirror commit] {   "rev": "154297",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -1203,8 +1203,8 @@ lively.morphic.Text.subclass('lively.morphic.SAPGridToolBar',
 'default category', {
     initialize: function($super, arg1, arg2) {
         $super(new lively.morphic.Shapes.Rectangle(new Rectangle(0,0,100,100)))
-        this.setFill(Color.rgb(255, 255, 225));
-        this.setBorderColor(Color.rgb(0,0,0));
+        //this.setFill(Color.rgb(255, 255, 225));
+        //this.setBorderColor(Color.rgb(0,0,0));
     }
     
    

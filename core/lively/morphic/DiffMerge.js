19829f4d60677f7bfbf3fb67c06fc1dd2a841434
athomschke <lively-kernel@hpi.uni-potsdam.de>
2012-04-24 12:38:08 +0200

    [mirror commit] {   "rev": "155085",   "author": "athomschke",   "changes": "U   core/lively/morphic/DiffMerge.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/DiffMerge.js\n" }

--- core/lively/morphic/DiffMerge.js
+++ core/lively/morphic/DiffMerge.js
@@ -259,7 +259,10 @@ Object.subclass('AtomicDiff',
     diffAgainst: function(otherDiff) {
         // performs an atomic diff diff (sic!) based on an atomic merge matrix
         if (this.newValue && typeof(this.newValue.equals) == "function") {
-            if (this.newValue.equals(otherDiff.newValue)) return undefined
+            if (typeOf(otherDiff.newValue) === 'undefined')
+                return new AtomicDiff(this.type, this.newValue, otherDiff.newValue)
+
+            else if (this.newValue.equals(otherDiff.newValue)) return undefined
             else {
                 return new AtomicDiff(this.type, this.newValue, otherDiff.newValue)
             }

8e9aa256b18bfc08fe83603c0185f966d2065451
willy.scheibel <lively-kernel@hpi.uni-potsdam.de>
2012-04-25 11:17:42 +0200

    [mirror commit] {   "rev": "155469",   "author": "willy.scheibel",   "changes": "U   core/games/TowerDefense.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/games/TowerDefense.js\n" }

--- core/games/TowerDefense.js
+++ core/games/TowerDefense.js
@@ -1,41 +1,41 @@
 module('games.TowerDefense').requires().toRun(function() {
 
 Object.subclass('games.TowerDefense.TowerDefense', {
-    buildMap: function() {
-        var mapMorph = new Morph();
-        
-        mapMorph.settings = {
-            tileSize: 32,
-            xTiles: 15,
-            yTiles: 15
-        };
+buildMap: function() {
+    var mapMorph = new Morph();
     
-        this.addMorph(mapMorph);
-        mapMorph.setPosition(pt(this.settings.borderWidth, this.settings.menuHeight + this.settings.borderWidth));
-        mapMorph.setExtent(pt(
-            this.settings.tileSize*this.settings.xTiles,
-            this.settings.tileSize*this.settings.yTiles
-        ));
-        //mapMorph.setBorderColor(Color.black);
-        //mapMorph.setBorderWidth(this.settings.borderWidth);
-        mapMorph.addScript(function towerDefense() {
-            return this.owner;
-        });
-        this.map = mapMorph;
-        
-        for(i=0; i<this.settings.yTiles; ++i) {
-            for (j=0; j<this.settings.xTiles; ++j) {
-                var tile = new Morph();
-                tile.setExtent(pt(this.settings.tileSize, this.settings.tileSize));
-                tile.setPosition(pt(
-                    j*this.settings.tileSize,
-                    i*this.settings.tileSize
-                ));
-                tile.setFill((j+i)%2?Color.black:Color.white);
-                this.map.addMorph(tile);
-            }
+    mapMorph.settings = {
+        tileSize: 32,
+        xTiles: 15,
+        yTiles: 15
+    };
+    
+    this.addMorph(mapMorph);
+    mapMorph.setPosition(pt(this.settings.borderWidth, this.settings.menuHeight + this.settings.borderWidth));
+    mapMorph.setExtent(pt(
+        this.settings.tileSize*this.settings.xTiles,
+        this.settings.tileSize*this.settings.yTiles
+    ));
+    //mapMorph.setBorderColor(Color.black);
+    //mapMorph.setBorderWidth(this.settings.borderWidth);
+    mapMorph.addScript(function towerDefense() {
+        return this.owner;
+    });
+    this.map = mapMorph;
+    
+    for(i=0; i<this.settings.yTiles; ++i) {
+        for (j=0; j<this.settings.xTiles; ++j) {
+            var tile = new Morph();
+            tile.setExtent(pt(this.settings.tileSize, this.settings.tileSize));
+            tile.setPosition(pt(
+                j*this.settings.tileSize,
+                i*this.settings.tileSize
+            ));
+            tile.setFill((j+i)%2?Color.black:Color.white);
+            this.map.addMorph(tile);
         }
-    },
+    }
+},
 buildMenu: function() {
         var towerDefense = this;
     

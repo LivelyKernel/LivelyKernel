11718672d3860c9e068ee814dedfe990f817d5ae
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-26 19:50:10 +0200

    [mirror commit] {   "rev": "156528",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -1381,10 +1381,11 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGridToolBar',
         
         this.imgSave = new lively.morphic.Image(new Rectangle(10,3,24,24), "images/Save-icon.png"); 
         this.imgSaveAs = new lively.morphic.Image(new Rectangle(24*1 + 10 + nGapWidth,3,24,24), "images/Save-as-icon.png"); 
-
-        this.imgCopy= new lively.morphic.Image(new Rectangle(nGapGroupWidth + 24*2 + 10 + 2*nGapWidth,3,24,24), "images/Actions-edit-copy-icon.png"); 
-        this.imgCut= new lively.morphic.Image(new Rectangle(nGapGroupWidth + 24*3 + 10 + 3*nGapWidth,3,24,24), "images/Cut-icon.png"); 
-        this.imgPaste= new lively.morphic.Image(new Rectangle(nGapGroupWidth + 24*4 + 10 + 4*nGapWidth,3,24,24), "images/Action-paste-icon.png"); 
+        
+        this.imgPaste= new lively.morphic.Image(new Rectangle(nGapGroupWidth + 24*2 + 10 + 4*nGapWidth,3,24,24), "images/Action-paste-icon.png"); 
+        this.imgCopy= new lively.morphic.Image(new Rectangle(nGapGroupWidth + 24*3 + 10 + 2*nGapWidth,3,24,24), "images/Actions-edit-copy-icon.png"); 
+        this.imgCut= new lively.morphic.Image(new Rectangle(nGapGroupWidth + 24*4 + 10 + 3*nGapWidth,3,24,24), "images/Cut-icon.png"); 
+        
         this.imgClear= new lively.morphic.Image(new Rectangle(nGapGroupWidth + 24*5 + 10 + 5*nGapWidth,3,24,24), "images/Actions-edit-clear-icon.png"); 
 
         this.imgBold= new lively.morphic.Image(new Rectangle(2*nGapGroupWidth + 24*6 + 10 + 6*nGapWidth,3,24,24), "images/Actions-format-text-bold-icon.png"); 

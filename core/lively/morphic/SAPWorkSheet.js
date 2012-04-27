b79263c42061dbec249f316c0853687350c046e5
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-27 04:23:25 +0200

    [mirror commit] {   "rev": "156596",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -1509,8 +1509,6 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGridToolBar',
     fontPicker_callBack: function(sFont){
         var that = this.owner;
         that.ddlFont.updateList([sFont]);
-        //fontFamily: font,
-
         for (i= 0; i< that.grid.arrSelectedCells.length; i++) {
             that.grid.arrSelectedCells[i].emphasizeAll({fontFamily:sFont});
         }

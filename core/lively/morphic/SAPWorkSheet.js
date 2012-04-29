eddf5382549f27f69900d3a1c374028ea4b27fda
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-29 18:30:29 +0200

    [mirror commit] {   "rev": "157180",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -1665,6 +1665,10 @@ format: currency & percentage
 
         for (i= 0; i< this.grid.arrSelectedCells.length; i++) {
             this.grid.arrSelectedCells[i].emphasizeAll({textDecoration: 'underline'});
+ 	    nRow  = this.grid.arrSelectedCells[i].gridCoords.y - (this.grid.hideColHeads ? 0 : 1);
+            nColumn = this.grid.arrSelectedCells[i].gridCoords.x;
+            nOrgRow = nRow  + this.grid.startRow;
+            nOrgCol = nColumn + this.grid.startColumn;
         }
     },
     imgBold_Click: function() {

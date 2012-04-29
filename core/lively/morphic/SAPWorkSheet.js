dc2248b683e3e86b2edc3b9d71cda16ba8335601
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-04-29 23:49:59 +0200

    [mirror commit] {   "rev": "157265",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -1606,7 +1606,7 @@ format: currency & percentage
     },
     setfontFamily: function(sFontFamily){
         if (this.fontPicker){
-            
+            //setDefaultFont
         }else{
             this.fontPicker= new lively.morphic.SAPFontPicker(sFontFamily,this.fontPicker_callBack);
             this.fontPicker.setPosition(pt(181,24));

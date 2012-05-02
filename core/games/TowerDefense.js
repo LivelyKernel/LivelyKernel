6e2b371afc08a647e993babc05ec7ce35a15f98b
willy.scheibel <lively-kernel@hpi.uni-potsdam.de>
2012-05-02 14:58:44 +0200

    [mirror commit] {   "rev": "158177",   "author": "willy.scheibel",   "changes": "U   core/games/TowerDefense.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/games/TowerDefense.js\n" }

--- core/games/TowerDefense.js
+++ core/games/TowerDefense.js
@@ -17,9 +17,9 @@ settings: {
 level1: new TD.LevelDescription({
     name: 'Level 1',
     paths: [
-        { x: 0, y:0 , description: 'DDDDDRRRRDDLLLDDDDDDD' },
-        'DDRRDDDRRDDLLLDDDDDDD',
-        'DDRRDRRRDDDLDLLLDDDDDDD'
+        { x: 0, y: 0, description: 'DDDDDRRRRDDLLLDDDDDDD' },
+        { x: 0, y: 0, description: 'DDRRDDDRRDDLLLDDDDDDD' },
+        { x: 0, y: 0, description: 'DDRRDRRRDDDLDLLLDDDDDDD' }
     ]
 }),
 buildMap: function() {

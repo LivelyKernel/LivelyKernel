0e1d60cd01c92e7255ad58305d0fabdd6250cfd0
roland.lux <lively-kernel@hpi.uni-potsdam.de>
2012-05-02 18:28:27 +0200

    [mirror commit] {   "rev": "158435",   "author": "roland.lux",   "changes": "U   core/games/TowerDefense.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/games/TowerDefense.js\n" }

--- core/games/TowerDefense.js
+++ core/games/TowerDefense.js
@@ -308,7 +308,7 @@ level1: function() {
         name: 'Level 1',
         paths: [
             { x: 0, y: 7, description: 'RRRRRRRRRRRRUUUUULLLLLLLDDDDDDDDDDDD' },
-            { x: 0, y: 9, description: '11R5U7L' },
+            { x: 0, y: 9, description: '11R' },//5U7L
             { x: 14, y: 4, description: 'LLLLLLDDDDDDDDDD' }
         ]
     });

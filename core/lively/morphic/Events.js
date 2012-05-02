2fb93dd10bbe36f4b6b171a64e46451bc22086c1
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-05-02 14:57:01 +0200

    [mirror commit] {   "rev": "158176",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/Events.js\nU   core/lively/morphic/video/Video.js\n",   "msg": "trying to fix video\n\n" }

--- core/lively/morphic/Events.js
+++ core/lively/morphic/Events.js
@@ -1735,25 +1735,29 @@ Object.subclass('lively.FileUploader',
 
     openVideo: function(url, mime, pos) {
         // new lively.FileUploader().openVideo('http://lively-kernel.org/repository/webwerkstatt/documentation/videoTutorials/110419_ManipulateMorphs.mov', 'video/mp4')
+        module('lively.morphic.video.Video').load();
+        mime = mime || '';
+        var videoNode;
         if (/*mime.include('webm')*/true) {
-            var videoNode = XHTMLNS.create('video');
+            videoNode = XHTMLNS.create('video');
             videoNode.width = 400;
             videoNode.height = 300;
             videoNode.controls = true;
+            videoNode.preload = true;
             var sourceNode = XHTMLNS.create('source');
             sourceNode.src = url;
             videoNode.appendChild(sourceNode);
 
-            if (mime.include('quicktime')) mime = mime.replace('quicktime', 'mp4');
+            // if (mime.include('quicktime')) mime = mime.replace('quicktime', 'mp4');
 
-            if (mime.include('mp4')) {
-                sourceNode.type = mime + '; codecs="avc1.42E01E, mp4a.40.2"'
-            } else if (mime.include('webm')) {
-                sourceNode.type = mime //+ '; codecs="vp8, vorbis"'
-            } else {
-                sourceNode.type = mime;
-                alert('video with type ' + mime + ' currently not supported');
-            }
+            // if (mime.include('mp4')) {
+            //     sourceNode.type = mime + '; codecs="avc1.42E01E, mp4a.40.2"'
+            // } else if (mime.include('webm')) {
+            //     sourceNode.type = mime //+ '; codecs="vp8, vorbis"'
+            // } else {
+            //     sourceNode.type = mime;
+            //     alert('video with type ' + mime + ' currently not supported');
+            // }
         } else {
             var embedNode = XHTMLNS.create('object');
             embedNode.type = mime;
@@ -1762,7 +1766,7 @@ Object.subclass('lively.FileUploader',
             // embedNode.scale="tofit"
             embedNode.width="400"
             embedNode.height="400"
-            var videoNode = embedNode
+            videoNode = embedNode
             // XHTMLNS.create('object');
             // videoNode.appendChild(embedNode)
         }
--- core/lively/morphic/video/Video.js
+++ core/lively/morphic/video/Video.js
@@ -1,101 +1,15 @@
-module('lively.morphic.video.Video').requires('lively.morphic.Core', 'lively.morphic.Events', 'lively.WidgetsTraits', 'lively.morphic.Styles').toRun(function() {
+module('lively.morphic.video.Video').requires().toRun(function() {
 
-var res = [
-		          "lib/jquery.min.js", 
-		          "lib/jquery-ui/jquery-ui.min.js",
-		          "lib/jme/utils/a11y-slider.ext.js",
-		          "lib/jme/packages/mm.full.min.js",
-		          "lib/jme/plugins/fullwindow.js",
-		          "lib/jme/plugins/timerange.js",
-		          "lib/jme/plugins/track.js",
-		          "lib/mustache.js",
-//		          ]
-//var css = [
-          "lib/jquery-ui/themes/redmond/jquery-ui.css",
-          "lib/jme/css/styles.css",
-          "lib/jme/css/player-controls.css",
-          "lib/jme/css/custom.css"
-          ]
+(function loadHTML5MediaLib() {
+    JSLoader.loadJs("http://api.html5media.info/1.1.5/html5media.min.js", function() {
+        lively.morphic.video.Video.ready = true;
+    }, null, true);
+})();
 
-JSLoader.resolveAndLoadAll(LivelyLoader.codeBase, res) //shall insert all css and js to the head, albeit asynchronously
-	
-lively.morphic.Morph.subclass('lively.morphic.Video',
-'settings', {
-		style: {
-			enableGrabbing: false,
-			enableDropping: false 
-		},
+// JSLoader.removeAllScriptsThatLinkTo("http://api.html5media.info/1.1.5/html5media.min.js")
+// $('script').last().attr('src')
 
-		template: undefined
-},
-'initializing', {
-	
-	initialize: function($super, json, bounds) {
-		
-		$super();
-		
-//		this.loadResources(this.css)
-//		this.loadResources(this.scriptS)
-		
-		var mainDiv = XHTMLNS.create('div');
 
-		mainDiv.id =  "lively-morphic-Video-internal-" + this.id
-		json.id = mainDiv.id
-		
-		template = this.getTemplate()
-
-		mainDiv.innerHTML = Mustache.to_html(template, json).replace(/^\s*/mg, '');
-		
-		this.setShape(new lively.morphic.Shapes.External(mainDiv));
-		if (bounds) this.setBounds(bounds);
-		
-		//this.setExtent(pt(mainDiv.offsetWidth, mainDiv.offsetHeight))
-		this.setExtent(pt(432.0,277.0))// taken from player-controls.css
-
-		
-	}
-},
-'util',{
-	
-	getTemplate: function(){
-		
-		if(this.template){
-			return this.template;
-		}
-		
-		req = new WebResource(LivelyLoader.codeBase + 'lively/morphic/video/video.xhtml.mustache')
-		this.template = req.get().content
-		return this.template
-	},
-	
-//	loadResources: function (res){
-//			
-//		for ( var i=0, len=res.length; i<len; ++i ){
-//			this.loadResource(LivelyLoader.codeBase + res[i])
-//		}		
-//	},
-//	loadResource: function(res){
-//		if(JSLoader.scriptInDOM(res)){
-//			return
-//		}
-//		
-//		if(res.match(/\.js$/)){
-//			
-//			var el=document.createElement('script')
-//			el.setAttribute("type","text/ecmascript")
-//			el.setAttribute("src", res)
-//		} else if(res.match(/\.css$/)){
-//			 var el=document.createElement("link")
-//			  el.setAttribute("rel", "stylesheet")
-//			  el.setAttribute("type", "text/css")
-//			  el.setAttribute("href",res)
-//		}
-//		if (typeof el!="undefined") {
-//			el.setAttribute("id", res)
-//			document.getElementsByTagName("head")[0].appendChild(el)
-//		}
-			
-//	}
-})
+// new lively.FileUploader().openVideo('http://lively-kernel.org/repository/webwerkstatt/documentation/videoTutorials/110419_ManipulateMorphs.mov')
 
 }) // end of module

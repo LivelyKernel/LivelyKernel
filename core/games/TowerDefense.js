c5c51ef77d37e325b860d5055b4f9590da7ccfa9
willy.scheibel <lively-kernel@hpi.uni-potsdam.de>
2012-05-02 17:26:47 +0200

    [mirror commit] {   "rev": "158393",   "author": "willy.scheibel",   "changes": "U   core/games/TowerDefense.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/games/TowerDefense.js\n" }

--- core/games/TowerDefense.js
+++ core/games/TowerDefense.js
@@ -186,9 +186,11 @@ update: function(delta) {
         
 
         if (this.path.length < this.currentPathIndex) {
+            this.currentPathIndex++;
+
             this.step.to = this.path.directions[this.currentPathIndex].apply(this.step.to);
             
-            this.currentPathIndex++;
+            
         } else {
             this.setFill(Color.black);
         }

3d6ce6ef53ae11598e4ab46286da6125bc18bda8
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-04 17:08:02 +0200

    [mirror commit] {   "rev": "159357",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -692,6 +692,12 @@ console.log('End createLayout =' + elapsed);
                 if (this.arrData[nOrgRow][nOrgCol].textAlign){
                     sTextAlign =this.arrData[nOrgRow][nOrgCol].textAlign;
                 }
+                
+
+//oColor = eval('Color.rgb(220,220,220)')
+//oText.applyStyle({borderColor: oColor ,textColor: oColor,fill:null});
+
+
                 //bug in applystyle textDecoration does not work
                 this.at(x,y).emphasizeAll({fontWeight: sFontWeight,fontStyle: sFontStyle,textDecoration: sTextDecoration,fontSize:sFontSize,fontFamily:sFontFamily});
                 //this.at(x,y).applyStyle({fontWeight: sFontWeight,fontStyle: sFontStyle,textDecoration: sTextDecoration,fontSize:sFontSize,fontFamily:sFontFamily});

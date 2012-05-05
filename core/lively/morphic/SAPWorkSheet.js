a3fafa96f3387757b5de1e1e42155e01b450f123
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-05 06:43:59 +0200

    [mirror commit] {   "rev": "159586",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -709,10 +709,10 @@ console.log('End createLayout =' + elapsed);
                     oTextColor=eval(this.arrData[nOrgRow][nOrgCol].textColor);
                 } 
                 //oText.applyStyle({borderColor: oBorderColor, fill: oFill ,textColor: oTextColor});
-                //bug in applystyle textDecoration & fontStyle do not work
+                //bug in applystyle textDecoration & fontStyle & fontWeight do not work
                 
-                this.at(x,y).applyStyle({fontWeight: sFontWeight,fontSize:sFontSize,fontFamily:sFontFamily,fill: oFill ,textColor: oTextColor});
-                this.at(x,y).emphasizeAll({fontStyle: sFontStyle,textDecoration: sTextDecoration});    
+                this.at(x,y).applyStyle({fontSize:sFontSize,fontFamily:sFontFamily,fill: oFill ,textColor: oTextColor});
+                this.at(x,y).emphasizeAll({fontWeight: sFontWeight,fontStyle: sFontStyle,textDecoration: sTextDecoration});    
                 //borderColor does not take null value.
                 if (oBorderColor){
                     this.at(x,y).applyStyle({borderColor: oBorderColor});

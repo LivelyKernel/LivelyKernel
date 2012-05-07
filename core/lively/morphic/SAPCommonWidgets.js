41a8cfed6b489f2ff43fc6188baf81cd1a427c1d
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-07 20:31:54 +0200

    [mirror commit] {   "rev": "159924",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPCommonWidgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPCommonWidgets.js\n" }

--- core/lively/morphic/SAPCommonWidgets.js
+++ core/lively/morphic/SAPCommonWidgets.js
@@ -11,7 +11,7 @@ lively.morphic.Morph.subclass('lively.morphic.SAPCellFormatter',
     },
      initializeLayout: function() {
         //var oTextCategory = new lively.morphic.Text(new Rectangle(0,0, 100, 30), 'Category');
-        this.appleStyle({fill:Color.white});
+        this.setFill(Color.rgb(255,255,255));
         var oTextCategory =new lively.morphic.Text(pt(0,0).extent(pt(100,30)),'Category');
         oTextCategory.applyStyle({borderWidth: 0, strokeOpacity: 0, fill: null})
         this.lstCategory = new lively.morphic.List(new Rectangle(0, 30, 130, 200), ['Number', 'Currency', 'Percentage','Data','Time']);

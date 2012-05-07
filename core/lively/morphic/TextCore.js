955d68b1f0d9e96d653f2eb0a45d241eb3fc051b
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-05-07 15:54:40 +0200

    [mirror commit] {   "rev": "159859",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/TextCore.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/TextCore.js\n" }

--- core/lively/morphic/TextCore.js
+++ core/lively/morphic/TextCore.js
@@ -2871,7 +2871,7 @@ Object.subclass('lively.morphic.Text.ShortcutHandler',
                                    (evt.isShiftDown()  == b.evtSpec.shift) &&
                                    (evt.isCommandKey() == b.evtSpec.cmd),
                 charKeyMatches   = (evt.charCode || evt.keyCode) === b.evtSpec.charCode;
-// alert(evt.keyCode + 'vs' + b.evtSpec.charCode);
+alert(evt.isCommandKey() + 'vs' + b.evtSpec.cmd);
 // alert('specialKeysMatch' + specialKeysMatch + ' ' + charKeyMatches);
             if (!specialKeysMatch || !charKeyMatches) continue;
             return b.handler.call(this, target, b);

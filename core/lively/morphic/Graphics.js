3337487d118da34484afaa1a79cb8bd8e6ece506
sstamm <lively-kernel@hpi.uni-potsdam.de>
2012-05-08 11:37:25 +0200

    [mirror commit] {   "rev": "160102",   "author": "sstamm",   "changes": "U   core/lively/morphic/Graphics.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Graphics.js\n" }

--- core/lively/morphic/Graphics.js
+++ core/lively/morphic/Graphics.js
@@ -29,8 +29,8 @@ Object.subclass("Point",
 
         return new lively.Point(this.x + p.x, this.y + p.y);
     },
-    newMethod: function() {
-        // enter comment here
+    moveBy: function(p) {
+        return this.addPt(p);
     },
 
     

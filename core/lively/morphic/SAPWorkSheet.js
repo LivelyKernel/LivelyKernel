79e6785f7ba210181c71041ec684b12f2c9fa954
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-12 05:11:56 +0200

    [mirror commit] {   "rev": "161689",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -44,13 +44,13 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGrid',
         this.initializeMorph();
         this.initializeScrolls();
         this.initializeAnnotation();
-        $.getScript("../../core/lively/date.format.js")
+        /*$.getScript("../../core/lively/date.format.js")
             .done(function(script, textStatus) {
             console.log( "success loading date.format.js");
             })
             .fail(function(jqxhr, settings, exception) {
                 console.log("error loading date.format.js");
-        }); 
+        });*/ 
 
     },
     getAlignforValueType: function(sValue) {

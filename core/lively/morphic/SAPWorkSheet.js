376eecc0c3eb08e1f2e1ebb9d7d6eab677082c76
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-14 15:45:13 +0200

    [mirror commit] {   "rev": "162141",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -2326,7 +2326,7 @@ dataformat: currency & percentage & date & time
                 sValue = this.grid.applyDataFormates(sValue ,oDataFormat);
                 this.grid.arrSelectedCells[i].textString= sValue;
                 if (oDataFormat.type=="currency" || oDataFormat.type=="number"){
-                    nValue = nValue.replace(/[^0-9\.]+/g,"");
+                    nValue = nValue.replace(/[^0-9\.\-]+/g,"");
                     debugger;
                     if (!isNaN(nValue )){
                         if (oDataFormat.negativeType==1 || oDataFormat.negativeType==3){

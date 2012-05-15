a4365908088ea8eac649d623294718bfcfb97b65
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-15 16:32:37 +0200

    [mirror commit] {   "rev": "162858",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheetTest.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheetTest.js\n" }

--- core/lively/morphic/SAPWorkSheetTest.js
+++ core/lively/morphic/SAPWorkSheetTest.js
@@ -328,6 +328,7 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGrid',
             this.numCols * this.defaultCellWidth  + 2 * this.borderSize,
             this.numRows * this.defaultCellHeight + 2 * this.borderSize));
         this.setFill(Color.rgb(255,255,255));
+        debugger;
         if (!this.hideColHeads) {
             this.createColHeads();
         }

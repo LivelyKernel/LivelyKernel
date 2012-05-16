3f7f27638e6889848e9341094ef437fc7f22bf88
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-16 21:44:31 +0200

    [mirror commit] {   "rev": "163638",   "author": "kimhw",   "changes": "U   core/lively/morphic/Chris.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Chris.js\n" }

--- core/lively/morphic/Chris.js
+++ core/lively/morphic/Chris.js
@@ -5,7 +5,7 @@ module('lively.morphic.Chris').requires().toRun(function() {
         },
         alertMe: function(text){
             debugger;
-            alert(text + 'yyyy ccc');
+            alert(text + 'yyyy ccc ss');
         }
     });    
 

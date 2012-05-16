06c356fde128317641cfe78a3bfbb84cda8e9523
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-05-16 19:32:34 +0200

    [mirror commit] {   "rev": "163558",   "author": "cschuster",   "changes": "U   core/lively/ast/StaticAnalysis.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/StaticAnalysis.js\n" }

--- core/lively/ast/StaticAnalysis.js
+++ core/lively/ast/StaticAnalysis.js
@@ -212,7 +212,7 @@ cop.create('AdvancedSyntaxHighlighting').refineClass(lively.morphic.Text, {
     },
 });
 Object.extend(AdvancedSyntaxHighlighting, {
-    errorStyle: { backgroundColor: Color.web.salmon.lighter() }
+    errorStyle: { backgroundColor: Color.web.salmon.lighter() },
     globalStyle: { color: Color.red }
 });
 cop.create('SystemCodeEditorHighlighting').refineClass(lively.ide.BasicBrowser, {

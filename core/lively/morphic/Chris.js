dc7314202007584d74147e91465d54686acc4b44
cmanciero <lively-kernel@hpi.uni-potsdam.de>
2012-05-16 17:53:09 +0200

    [mirror commit] {   "rev": "163474",   "author": "cmanciero",   "changes": "U   core/lively/morphic/Chris.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Chris.js\n" }

--- core/lively/morphic/Chris.js
+++ core/lively/morphic/Chris.js
@@ -1,7 +1,12 @@
 module('lively.morphic.Chris').requires().toRun(function() {
 
     lively.morphic.Morph.subclass('lively.morphic.Buttons', 'events', {
-
+        initialize: function($super){
+            $super();
+        },
+        alertMe: function(){
+            alert('hello world');
+        }
     });    
 
 }) // end of module

6db81b228b472ed7cda024c1f6eee2463e38e70b
cmanciero <lively-kernel@hpi.uni-potsdam.de>
2012-05-16 20:59:16 +0200

    [mirror commit] {   "rev": "163620",   "author": "cmanciero",   "changes": "U   core/lively/morphic/Chris.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Chris.js\n" }

--- core/lively/morphic/Chris.js
+++ core/lively/morphic/Chris.js
@@ -5,7 +5,7 @@ module('lively.morphic.Chris').requires().toRun(function() {
         },
         alertMe: function(text){
             debugger;
-            alert(text + ' me');
+            alert(text + 'you and me');
         }
     });    
 

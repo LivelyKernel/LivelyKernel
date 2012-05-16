728e5409f85adc92c871ea6939fdc9c4e145502c
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-16 04:16:24 +0200

    [mirror commit] {   "rev": "163120",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheetTest.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheetTest.js\n" }

--- core/lively/morphic/SAPWorkSheetTest.js
+++ core/lively/morphic/SAPWorkSheetTest.js
@@ -357,11 +357,9 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGrid',
         }
         
         //Expend column names
-        for (var nCol = nStartColumn ; nCol < nEndColumn ; nCol++) {
-            //this.getColumnName(i+1)
-            //this.colNames.push('Col' + nCol);
-            this.colNames.push(this.getColumnName(nCol + 1));
-        }
+        //for (var nCol = nStartColumn ; nCol < nEndColumn ; nCol++) {
+          //  this.colNames.push(this.getColumnName(nCol + 1));
+        //}
 
         //Expend column cell
         for (var nRow = 0; nRow < this.arrData.length; nRow++) {

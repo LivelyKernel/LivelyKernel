14c890a581c6f1d46b829dbc2b24ca8c2aa06668
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-17 22:10:09 +0200

    [mirror commit] {   "rev": "163835",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheetTest.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheetTest.js\n" }

--- core/lively/morphic/SAPWorkSheetTest.js
+++ core/lively/morphic/SAPWorkSheetTest.js
@@ -2323,7 +2323,7 @@ dataformat: currency & percentage & date & time
     imgClear_Click: function() {
         debugger;
         if (this.oClearMenu){
-            this.oClearMenu.openInWorld(this.imgClear.getPositionInWorld());
+            this.oClearMenu.openInWorld(this.imgClear.getPositionInWorld().addXY(2, 2));
         }else{
             //'Clear All','Clear Formats','Clear Contents','Clear Comments'
 	    var arrItems=[];
@@ -2333,7 +2333,7 @@ dataformat: currency & percentage & date & time
                 ['Clear Contents', function() { alert('3rd') }],
                 ['Clear Comments', function() { alert('4th') }]
             ];
-            this.oClearMenu= lively.morphic.Menu.openAt(this.imgClear.getPositionInWorld(), null, arrItems);
+            this.oClearMenu= lively.morphic.Menu.openAt(this.imgClear.getPositionInWorld().addXY(2, 2), null, arrItems);
         }
     },
     imgTextAlignCenter_Click: function() {

f4874b7961c21e731a73eb9c9c13ea7b3d84eb57
aileen.zhou <lively-kernel@hpi.uni-potsdam.de>
2012-05-18 18:16:43 +0200

    [mirror commit] {   "rev": "164243",   "author": "aileen.zhou",   "changes": "U   core/lively/Network.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/Network.js\n" }

--- core/lively/Network.js
+++ core/lively/Network.js
@@ -1614,7 +1614,7 @@ Object.subclass('WebResource',
     },
 
     pvtProcessPropfindForSubElements: function(doc) {
-        alert(doc.documentElement);
+        //alert(doc.documentElement);
         if (!this.status.isSuccess())
             throw new Error('Cannot access subElements of ' + this.getURL());
         var davNs = this.ensureDavXmlNs(doc);

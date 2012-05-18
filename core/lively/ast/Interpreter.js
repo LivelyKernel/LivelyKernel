2eb9b476ea218a8c4d35545cda103642708321df
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-05-19 01:08:22 +0200

    [mirror commit] {   "rev": "164473",   "author": "cschuster",   "changes": "U   core/lively/ast/Interpreter.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/Interpreter.js\n" }

--- core/lively/ast/Interpreter.js
+++ core/lively/ast/Interpreter.js
@@ -280,7 +280,7 @@ Object.subclass('lively.ast.Interpreter.Frame',
         text.setTextString(this.getFuncSource());
         text.highlightJavaScriptSyntax();
         if (this.pc !== null) {
-            var style = { backgroundColor: Color.blue.lighter().lighter() };
+            var style = { backgroundColor: Color.yellow.lighter() };
             text.emphasize(style, this.pc.pos[0], this.pc.pos[1]);
         }
     },

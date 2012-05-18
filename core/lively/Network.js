08db2d20b3153f6b2ee60a348e6c37ddb71733fd
aileen.zhou <lively-kernel@hpi.uni-potsdam.de>
2012-05-18 23:08:09 +0200

    [mirror commit] {   "rev": "164411",   "author": "aileen.zhou",   "changes": "U   core/lively/Network.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/Network.js\n" }

--- core/lively/Network.js
+++ core/lively/Network.js
@@ -512,7 +512,6 @@ View.subclass('NetRequest', {
                 this.setResponseText(this.getResponseText());
             }    
             if (this.transport.responseXML !== undefined){
-                alert("xml: " + this.getResponseXML());
                 this.setResponseXML(this.getResponseXML());
             }
             if (this.transport.getAllResponseHeaders() !== undefined)

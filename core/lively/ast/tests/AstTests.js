6b0146f2522a4ef13229bd5678e29f787f045821
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-05-18 23:19:33 +0200

    [mirror commit] {   "rev": "164417",   "author": "cschuster",   "changes": "U   core/lively/ast/tests/AstTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/tests/AstTests.js\n" }

--- core/lively/ast/tests/AstTests.js
+++ core/lively/ast/tests/AstTests.js
@@ -133,7 +133,7 @@ TestCase.subclass('lively.ast.tests.AstTests.ParserTest',
     test10ParseCategoryFragment: function() {
         var src1 = '"accessing", { method: 23 }',
             src2 = '"accessing", { method: 23 },',
-            expected = [["string", [1, 9], "accessing"],
+            expected = ["accessing",
                      ["binding", [10, 20], "method", ["number", [17, 20]]]],
             result1 = this.parseJS(src1, 'categoryFragment'),
             result2 = this.parseJS(src2, 'categoryFragment');

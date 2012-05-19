11f8b8899ea7f6e7ae3d373841613041c2882d85
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-05-19 13:07:33 +0200

    [mirror commit] {   "rev": "164490",   "author": "robertkrahn",   "changes": "U   core/lively/Helper.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/Helper.js\n" }

--- core/lively/Helper.js
+++ core/lively/Helper.js
@@ -43,95 +43,95 @@ var evt = new Event(rawEvent);
 
 Object.subclass('lively.Helper.XMLConverter', {
 
-	documentation: 'Converts JS -> XML and XML -> JS, not complete but works for most cases, namespace support',
-	
-	convertToJSON: function(xml) {
-		return this.storeXMLDataInto(xml, {});
-	},
+    documentation: 'Converts JS -> XML and XML -> JS, not complete but works for most cases, namespace support',
+    
+    convertToJSON: function(xml) {
+        return this.storeXMLDataInto(xml, {});
+    },
 
-	storeXMLDataInto: function(xml, jsObj) {
-		jsObj.tagName = xml.tagName;
-		jsObj.toString = function() { return jsObj.tagName };
-		$A(xml.attributes).forEach(function(attr) { jsObj[attr.name] = attr.value	});
-		if (!xml.childNodes || xml.childNodes.length === 0) return jsObj;
-		jsObj.children = $A(xml.childNodes).collect(function(node) {
-			if (node.nodeType == Global.document.CDATA_SECTION_NODE) {
-				return {tagName: 'cdataSection', data: node.data, toString: function() { return 'CDATA'}};
-			}
-			if (node.nodeType == Global.document.TEXT_NODE) {
-				return {tagName: 'textNode', data: node.data, toString: function() { return 'TEXT'}};
-			}
-			return this.storeXMLDataInto(node, {});
-		}, this);
-		return jsObj;
-	},
+    storeXMLDataInto: function(xml, jsObj) {
+        jsObj.tagName = xml.tagName;
+        jsObj.toString = function() { return jsObj.tagName };
+        $A(xml.attributes).forEach(function(attr) { jsObj[attr.name] = attr.value    });
+        if (!xml.childNodes || xml.childNodes.length === 0) return jsObj;
+        jsObj.children = $A(xml.childNodes).collect(function(node) {
+            if (node.nodeType == Global.document.CDATA_SECTION_NODE) {
+                return {tagName: 'cdataSection', data: node.data, toString: function() { return 'CDATA'}};
+            }
+            if (node.nodeType == Global.document.TEXT_NODE) {
+                return {tagName: 'textNode', data: node.data, toString: function() { return 'TEXT'}};
+            }
+            return this.storeXMLDataInto(node, {});
+        }, this);
+        return jsObj;
+    },
 
-	toJSONString: function(jsObj, indent) {
-	        if (!indent) indent = '';
-			result = '{';
-			for (var key in jsObj) {
-				var value = jsObj[key];
-				result += '\n\t' + indent + '"' + key + '": ';
-			
-				if (Object.isNumber(value)) {
-					result += value;
-				} else if (Object.isString(value)) {
-					result += '"' + value + '"';
-				} else if (Object.isArray(value)) {
-					result += '[' + value.collect(function(item) {
-						return this.toJSONString(item, indent + '\t');
-					}, this).join(', ') + ']';
-				} else {
-					result += this.toJSONString(value, indent + '\t');
-				}
+    toJSONString: function(jsObj, indent) {
+            if (!indent) indent = '';
+            result = '{';
+            for (var key in jsObj) {
+                var value = jsObj[key];
+                result += '\n\t' + indent + '"' + key + '": ';
+            
+                if (Object.isNumber(value)) {
+                    result += value;
+                } else if (Object.isString(value)) {
+                    result += '"' + value + '"';
+                } else if (Object.isArray(value)) {
+                    result += '[' + value.collect(function(item) {
+                        return this.toJSONString(item, indent + '\t');
+                    }, this).join(', ') + ']';
+                } else {
+                    result += this.toJSONString(value, indent + '\t');
+                }
 
-				result += ',';
-			}
-			result += '\n' + indent + '}';
-			return result;
-	},
+                result += ',';
+            }
+            result += '\n' + indent + '}';
+            return result;
+    },
 
-	convertToXML: function(jsObj, nsMapping, baseDoc, nsWereDeclared) {
-		if (!jsObj.tagName)
-			throw new Error('Cannot convert JS object without attribute "tagName" to XML!');
+    convertToXML: function(jsObj, nsMapping, baseDoc, nsWereDeclared) {
+        if (!jsObj.tagName)
+            throw new Error('Cannot convert JS object without attribute "tagName" to XML!');
 
-		// deal with special nodes 
-		if (jsObj.tagName === 'cdataSection')
-			return baseDoc.createCDATASection(jsObj.data);
-		if (jsObj.tagName === 'textNode')
-			return baseDoc.createTextNode(jsObj.data);
+        // deal with special nodes 
+        if (jsObj.tagName === 'cdataSection')
+            return baseDoc.createCDATASection(jsObj.data);
+        if (jsObj.tagName === 'textNode')
+            return baseDoc.createTextNode(jsObj.data);
 
-		// create node
-		var nsDecl = nsWereDeclared ? '' : Properties.own(nsMapping).collect(function(prefix) {
-			return Strings.format('xmlns:%s="%s"', prefix, nsMapping[prefix])
-		}).join(' ');
-		var node = this.createNodeFromString(Strings.format('<%s %s/>', jsObj.tagName, nsDecl), baseDoc);
-	
-		// set attributes
-		Properties.own(jsObj)
-			.reject(function(key) { return key == 'tagName' || key == 'children' })
-			.forEach(function(key) {
-				var value = jsObj[key];
-				if (key.include(':')) {
-					var prefix = key.split(':')[0];
-					var ns = nsMapping[prefix];
-					if (!ns) throw new Error('JS object includes node with tagname having a NS prefix but the NS cannot be found in the nsMapping!');
-					node.setAttributeNS(ns, key, value);
-				} else {
-					node.setAttribute(key, value);
-				}
-			})
-	
-		// add childnodes
-		jsObj.children && jsObj.children.forEach(function(childJsObj) {
-			node.appendChild(this.convertToXML(childJsObj, nsMapping, baseDoc, true));
-		}, this);
-		return node;
-	},
+        // create node
+        var nsDecl = nsWereDeclared ? '' : Properties.own(nsMapping).collect(function(prefix) {
+            return Strings.format('xmlns:%s="%s"', prefix, nsMapping[prefix])
+        }).join(' ');
+        var node = this.createNodeFromString(Strings.format('<%s %s/>', jsObj.tagName, nsDecl), baseDoc);
+    
+        // set attributes
+        Properties.own(jsObj)
+            .reject(function(key) { return key == 'tagName' || key == 'children' })
+            .forEach(function(key) {
+                var value = jsObj[key];
+                if (key.include(':')) {
+                    var prefix = key.split(':')[0];
+                    var ns = nsMapping[prefix];
+                    if (!ns) throw new Error('JS object includes node with tagname having a NS prefix but the NS cannot be found in the nsMapping!');
+                    node.setAttributeNS(ns, key, value);
+                } else {
+                    node.setAttribute(key, value);
+                }
+            })
+    
+        // add childnodes
+        jsObj.children && jsObj.children.forEach(function(childJsObj) {
+            node.appendChild(this.convertToXML(childJsObj, nsMapping, baseDoc, true));
+        }, this);
+        return node;
+    },
 
-	createNodeFromString: function(string, baseDoc) {
-		return baseDoc.adoptNode(new DOMParser().parseFromString(string, "text/xml").documentElement);
-	},
+    createNodeFromString: function(string, baseDoc) {
+        return baseDoc.adoptNode(new DOMParser().parseFromString(string, "text/xml").documentElement);
+    },
 
 });
 

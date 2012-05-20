113d7086af71c9e90b80c8fc5f6ac09d527dad9b
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-05-20 22:30:58 +0200

    [mirror commit] {   "rev": "164655",   "author": "robertkrahn",   "changes": "U   core/lively/ide/tests/SCBTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ide/tests/SCBTests.js\n" }

--- core/lively/ide/tests/SCBTests.js
+++ core/lively/ide/tests/SCBTests.js
@@ -440,7 +440,7 @@ TestCase.subclass('lively.ide.tests.ModuleWrapper',
         this.assertEquals('js', sut.type());
         var otherSrc = 'Foo + 3 + 2;';
         sut.setSource(otherSrc);
-        this.assertEquals(otherSrc, sut.getSource());
+        this.assertEqualState(otherSrc, sut.getSource());
     },
     testPipelineSetSourceRequests: function() {
         var called = 0, reqRevs = [];

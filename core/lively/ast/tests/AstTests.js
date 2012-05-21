20a8bd1ec5daa934bf6d145dcffd0d2c983caa8e
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-05-21 19:28:18 +0200

    [mirror commit] {   "rev": "165295",   "author": "cschuster",   "changes": "U   core/lively/ast/tests/AstTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/tests/AstTests.js\n" }

--- core/lively/ast/tests/AstTests.js
+++ core/lively/ast/tests/AstTests.js
@@ -1272,8 +1272,8 @@ TestCase.subclass('lively.ast.tests.AstTests.BreakpointTest',
         this.assertStep(frame,{a:6,i:3});
         this.assertStep(frame,{a:6,i:4});
         this.assertStep(frame,{a:6,i:4});
-        //this.assertStep(frame,{a:6,b:2,i:4});
-        //this.assertEquals(frame.resume(),6);
+        this.assertStep(frame,{a:6,b:2,i:4});
+        this.assertEquals(frame.resume(),6);
     },
     testSimpleRestart: function() {
         var that = this;

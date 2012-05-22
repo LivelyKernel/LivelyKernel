4604cf926d3a6b907708f9717a90d437c110aefd
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-05-23 01:27:05 +0200

    [mirror commit] {   "rev": "165871",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTML.js\n" }

--- core/lively/morphic/HTML.js
+++ core/lively/morphic/HTML.js
@@ -255,7 +255,7 @@ lively.morphic.Morph.addMethods(
         if (UserAgent.fireFoxVersion)
             ctx.morphNode['-moz-user-modify'] = 'read-only'
 
-        console.log(ctx.getShapeNode());
+        //console.log(ctx.getShapeNode());
     
         //this.setShapeClassNameHTML(ctx, this.constructor.name);
     },

78098231133760093cc3391d2da98122e624f4f2
cmanciero <lively-kernel@hpi.uni-potsdam.de>
2012-05-22 02:51:53 +0200

    [mirror commit] {   "rev": "165531",   "author": "cmanciero",   "changes": "U   core/lively/morphic/Chris.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Chris.js\n" }

--- core/lively/morphic/Chris.js
+++ core/lively/morphic/Chris.js
@@ -2,13 +2,14 @@ module('lively.morphic.Chris').requires().toRun(function() {
     lively.morphic.Morph.subclass('lively.morphic.labbook', 'events', {
         initialize: function($super){
             $super();
-            this.password = new lively.morphic.PasswordInput().openInWorld();
-            $morph('PasswordContainer').addMorph(this.password);
+            //this.password = new lively.morphic.PasswordInput().openInWorld();
+            //$morph('PasswordContainer').addMorph(this.password);
         },
         login: function(email, password){
             //alert(email);
             //alert(password);
-            
+            this.password = new lively.morphic.PasswordInput().openInWorld();
+            $morph('PasswordContainer').addMorph(this.password);
             $morph('Rectangle1').applyStyle({ opacity: .20});
         }
     });    

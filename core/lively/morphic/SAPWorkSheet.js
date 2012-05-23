5ae62727ceea7f7d7be071be8679ee4813b8d1d6
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-23 19:12:57 +0200

    [mirror commit] {   "rev": "166074",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -64,6 +64,43 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGrid',
     onMouseWheelEntry: function(evt) {
         debugger;
         console.log("onMouseWheelEntry");
+        var delta = 0;
+		       if (!event) /* For IE. */
+		            event = window.event;
+		        if (event.wheelDelta) { /* IE/Opera. */
+		            delta = event.wheelDelta / 120;
+		            /** In Opera 9, delta differs in sign as compared to IE. */
+		            if (window.opera)
+		                delta = -delta;
+		        } else if (event.detail) { /** Mozilla case. */
+		            /** In Mozilla, sign of delta is different than in IE.
+		            * Also, delta is multiple of 3.
+		            */
+		            delta = -event.detail / 3;
+		        }
+
+		        /** If delta is nonzero, handle it.
+		        * Basically, delta is now positive if wheel was scrolled up,
+		        * and negative, if wheel was scrolled down.
+		        */
+		        if (delta)
+		            //handle(delta);
+
+		        /** Prevent default actions caused by mouse wheel.
+		        * That might be ugly, but we handle scrolls somehow
+		        * anyway, so don't bother here..
+		        */
+		        if (event.preventDefault)
+		            event.preventDefault();
+
+		        event.returnValue = false;
+		          
+
+
+
+
+
+
     },
     onMouseWheel: function(evt) {
         console.log("onMouseWheel");

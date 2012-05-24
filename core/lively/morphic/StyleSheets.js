a9fcb3222bab0ee4491b2b6d8a6cafb8033fdc83
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-05-24 22:37:10 +0200

    [mirror commit] {   "rev": "166474",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheets.js\n" }

--- core/lively/morphic/StyleSheets.js
+++ core/lively/morphic/StyleSheets.js
@@ -3,41 +3,42 @@ module('lively.morphic.StyleSheets').requires().toRun(function() {
     lively.morphic.Morph.addMethods(
         'stylesheets', {
             applyCSS: function() {
-    var cssCode = this.get("CSSSource").textString;
-    var morphName = this.get("MorphName").textString;
+                var cssCode = this.get("CSSSource").textString;
+                var morphName = this.get("MorphName").textString;
     
-    var morph = $world.get(morphName);
-    console.log(morph);
-    if (morph){
-        morph.setNodeId();
-        morphId = morph.getNodeId();
+                var morph = $world.get(morphName);
+                console.log(morph);
+                if (morph){
+                    morph.setNodeId();
+                    morphId = morph.getNodeId();
         
-        var specificCss = "#"+morphId+" { "+cssCode+" }";
-        var styleTagId = "style-"+morphId;
+                    var specificCss = "#"+morphId+" { "+cssCode+" }";
+                    var styleTagId = "style-"+morphId;
         
-        var styleNode = document.getElementById(styleTagId);
-        console.log(styleNode);
-        if(styleNode) {
-            styleNode.parentNode.removeChild(styleNode);
-        }
+                    var styleNode = document.getElementById(styleTagId);
+                    console.log(styleNode);
+                    if(styleNode) {
+                        styleNode.parentNode.removeChild(styleNode);
+                    }
         
-        var head = document.head;  
-        var newStyleNode = document.createElement('style');
-        newStyleNode.type = 'text/css';
-        newStyleNode.id = styleTagId;
-        var newStyleContent = document.createTextNode(specificCss);
-        if (newStyleNode.styleSheet) {
-            newStyleNode.styleSheet.cssText = newStyleContent.nodeValue;
-        }
-        else {
-            newStyleNode.appendChild(newStyleContent);
-        }
-        head.appendChild(newStyleNode);
+                    var head = document.head;  
+                    var newStyleNode = document.createElement('style');
+                    newStyleNode.type = 'text/css';
+                    newStyleNode.id = styleTagId;
+                    var newStyleContent = document.createTextNode(specificCss);
+                    if (newStyleNode.styleSheet) {
+                        newStyleNode.styleSheet.cssText = newStyleContent.nodeValue;
+                    }
+                    else {
+                        newStyleNode.appendChild(newStyleContent);
+                    }
+                    head.appendChild(newStyleNode);
 
-    }
-    else {
-        alert("Could not find morph '"+morphName+"' ...");
-    }
+                }
+                else {
+                    alert("Could not find morph '"+morphName+"' ...");
+                }
+            }
         }
     )
 

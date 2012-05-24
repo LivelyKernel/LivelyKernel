6cd023c22065d12f07c580088c2639a6a1672528
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-05-24 23:21:11 +0200

    [mirror commit] {   "rev": "166480",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheets.js\n" }

--- core/lively/morphic/StyleSheets.js
+++ core/lively/morphic/StyleSheets.js
@@ -2,37 +2,34 @@ module('lively.morphic.StyleSheets').requires().toRun(function() {
 
     lively.morphic.Morph.addMethods(
         'stylesheets', {
-            applyStyleSheet: function(style) {
+	   applyStyleSheet: function(style) {
 
-                this.setNodeId();
-                var morphId = this.getNodeId();
-                
-                var specificCss = "#"+morphId+" { "+cssCode+" }";
-                var styleTagId = "style-"+morphId;
-        
-                var styleNode = document.getElementById(styleTagId);
-                console.log(styleNode);
-                if(styleNode) {
-                    styleNode.parentNode.removeChild(styleNode);
-                }
-        
-                var head = document.head;  
-                var newStyleNode = document.createElement('style');
-                newStyleNode.type = 'text/css';
-                newStyleNode.id = styleTagId;
-                var newStyleContent = document.createTextNode(specificCss);
-                if (newStyleNode.styleSheet) {
-                    newStyleNode.styleSheet.cssText = newStyleContent.nodeValue;
-                }
-                else {
-                    newStyleNode.appendChild(newStyleContent);
-                }
-                head.appendChild(newStyleNode);
-                
-                this.styleSheet = style;
+		this.setNodeId();
+		var morphId = this.getNodeId();
+		
+		var specificCss = "#"+morphId+" { "+style+" }";
+		var styleTagId = "style-"+morphId;
 
-            }
+		var styleNode = document.getElementById(styleTagId);
+		console.log(styleNode);
+		if(styleNode) {
+			styleNode.parentNode.removeChild(styleNode);
+		}
+
+		var head = document.head;
+		var newStyleNode = document.createElement('style');
+		newStyleNode.type = 'text/css';
+		newStyleNode.id = styleTagId;
+		var newStyleContent = document.createTextNode(specificCss);
+		if (newStyleNode.styleSheet) {
+			newStyleNode.styleSheet.cssText = newStyleContent.nodeValue;
+		}
+		else {
+			newStyleNode.appendChild(newStyleContent);
+		}
+		head.appendChild(newStyleNode);
+		this.styleSheet = style;
+	   }
         }
     )
-
-}) // end of module
+})

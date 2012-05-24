1f0dcfd8ac9bf6b496e332733bc25dd798c4dc07
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-05-24 22:35:22 +0200

    [mirror commit] {   "rev": "166473",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheets.js\n" }

--- core/lively/morphic/StyleSheets.js
+++ core/lively/morphic/StyleSheets.js
@@ -1,5 +1,44 @@
 module('lively.morphic.StyleSheets').requires().toRun(function() {
 
-// Enter your code here
+    lively.morphic.Morph.addMethods(
+        'stylesheets', {
+            applyCSS: function() {
+    var cssCode = this.get("CSSSource").textString;
+    var morphName = this.get("MorphName").textString;
+    
+    var morph = $world.get(morphName);
+    console.log(morph);
+    if (morph){
+        morph.setNodeId();
+        morphId = morph.getNodeId();
+        
+        var specificCss = "#"+morphId+" { "+cssCode+" }";
+        var styleTagId = "style-"+morphId;
+        
+        var styleNode = document.getElementById(styleTagId);
+        console.log(styleNode);
+        if(styleNode) {
+            styleNode.parentNode.removeChild(styleNode);
+        }
+        
+        var head = document.head;  
+        var newStyleNode = document.createElement('style');
+        newStyleNode.type = 'text/css';
+        newStyleNode.id = styleTagId;
+        var newStyleContent = document.createTextNode(specificCss);
+        if (newStyleNode.styleSheet) {
+            newStyleNode.styleSheet.cssText = newStyleContent.nodeValue;
+        }
+        else {
+            newStyleNode.appendChild(newStyleContent);
+        }
+        head.appendChild(newStyleNode);
+
+    }
+    else {
+        alert("Could not find morph '"+morphName+"' ...");
+    }
+        }
+    )
 
 }) // end of module

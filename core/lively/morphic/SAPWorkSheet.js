5950527346201996b9531b3ac195455a2804a642
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-24 05:25:53 +0200

    [mirror commit] {   "rev": "166198",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -2666,20 +2666,7 @@ lively.morphic.Morph.subclass('lively.morphic.SAPWorkBook',
         var nWidth= this.grid.defaultCellWidth * this.grid.numCols + 50;
         var nHeight = this.grid.defaultCellHeight * this.grid.numRows + 50;
         this.setExtent(lively.pt(nWidth,nHeight));
-        //this.setFill(Color.rgb(255, 255, 225));
-    },
-     onMouseDown: function($super, evt) {
-        
-        $super(evt);
-    },
-    onScroll: function(evt) {
-        console.log("SAPWorkBook: onScroll");
-    },
-    onMouseWheel: function($super,evt) {
-        console.log("SAPWorkBook: onMouseWheel");
-        $super(evt);
     }
-
 });
 lively.morphic.Text.subclass('lively.morphic.SAPGridHeadCell',
 'settings', {

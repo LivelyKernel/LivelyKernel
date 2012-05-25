ac61f6c24848fc22c5b21da63610c221b6341924
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-25 18:58:52 +0200

    [mirror commit] {   "rev": "166604",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheet.js\n" }

--- core/lively/morphic/SAPWorkSheet.js
+++ core/lively/morphic/SAPWorkSheet.js
@@ -2099,7 +2099,7 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGridToolBar',
         for (i= 0; i< this.grid.arrSelectedCells.length; i++) {
            arrDisplay.push({x:this.grid.arrSelectedCells[i].gridPos().x ,y:this.grid.arrSelectedCells[i].gridPos().y})
         }
-        arrDisplay.sortBy(this.x)
+        arrDisplay.sortBy(arrDisplay.x)
 
 
         //for data

9e6fce18951186577062a73c58829ab3afd1a588
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-05-31 23:56:50 +0200

    [mirror commit] {   "rev": "168052",   "author": "cschuster",   "changes": "U   core/lively/morphic/TextCore.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/TextCore.js\n" }

--- core/lively/morphic/TextCore.js
+++ core/lively/morphic/TextCore.js
@@ -1029,11 +1029,11 @@ doAutoIndent: function() {
     },
 
     onBackspacePressed: function(evt) {
-        if (this.textString === '') {
-            console.log("this should not happen!");
+        //if (this.textString === '') {
+          //  console.log("this should not happen!");
             //evt.stop();
             //return true;
-        }
+        //}
         if (this.mergeText()) {
             evt.stop();
             return true;

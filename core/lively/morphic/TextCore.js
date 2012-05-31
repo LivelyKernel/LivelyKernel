45e40e9224d58c650f8a575856b9f8ebf96758e5
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-05-31 23:31:23 +0200

    [mirror commit] {   "rev": "168035",   "author": "cschuster",   "changes": "U   core/lively/morphic/TextCore.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/TextCore.js\n" }

--- core/lively/morphic/TextCore.js
+++ core/lively/morphic/TextCore.js
@@ -1037,13 +1037,13 @@ doAutoIndent: function() {
             evt.stop();
             return true;
         }
-        //if (this.isTabBeforeCursor(true)) {
-            //this.insertAtCursor('', false, true)
-            //evt.stop();
-            //return true;
-        //}
-        if (this.charsTyped.length > 0)
-            this.charsTyped = this.charsTyped.substring(0, this.charsTyped.length-1);
+        if (this.isTabBeforeCursor(true)) {
+            this.insertAtCursor('', false, true)
+            evt.stop();
+            return true;
+        }
+        //if (this.charsTyped.length > 0)
+          //  this.charsTyped = this.charsTyped.substring(0, this.charsTyped.length-1);
         this.fixChunksDelayed();
         return true;
     },

601cc4f3f2cd09f587e26f5f769bc7d6a1152df9
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-31 15:32:35 +0200

    [mirror commit] {   "rev": "167929",   "author": "kimhw",   "changes": "U   core/lively/morphic/tests/SAPWorkBookTest.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/SAPWorkBookTest.js\n" }

--- core/lively/morphic/tests/SAPWorkBookTest.js
+++ core/lively/morphic/tests/SAPWorkBookTest.js
@@ -4,22 +4,22 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.SAPWorkBookTest.S
 'running', {
     setUp: function($super) {
         $super();
-        this.grid = new lively.morphic.DataGrid(5,5, {showColHeads: false});
-        this.grid.openInWorld();
+        this.oWorkBook = lively.morphic.SAPWorkBook(10,10);
+        this.oWorkBook.openInWorld();
     },
     tearDown: function($super) {
         $super();
-        this.grid.remove();
+        this.oWorkBook.remove();
     },
 },
 'testing', {
     test01activateCell: function() {
-        this.grid.at(2,2).activate();
-        this.assertEquals(this.grid.activeCell, this.grid.at(2,2), 'cell (2,2) should be active after activation');
+        this.oWorkBook.grid.at(2,2).activate();
+        this.assertEquals(this.oWorkBook.grid.activeCell, this.oWorkBook.grid.at(2,2), 'cell (2,2) should be active after activation');
     },
     test02TestEvaluate: function() {
-        this.grid.evaluateExpression("this.at(3,3).activate()");
-        this.assertEquals(this.grid.activeCell, this.grid.at(3,3), "could not activate cell (3,3) using evaluateExpression");
+        this.oWorkBook.grid.evaluateExpression("this.at(3,3).activate()");
+        this.oWorkBook.assertEquals(this.oWorkBook.grid.activeCell, this.oWorkBook.grid.at(3,3), "could not activate cell (3,3) using evaluateExpression");
     }
 
 });

5879469a88ed45a19f9215eca277777ba43f1b52
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-05-31 17:30:00 +0200

    [mirror commit] {   "rev": "167943",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheetTest.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheetTest.js\n" }

--- core/lively/morphic/SAPWorkSheetTest.js
+++ core/lively/morphic/SAPWorkSheetTest.js
@@ -1741,7 +1741,7 @@ lively.morphic.Text.subclass('lively.morphic.SAPGridCell',
         $super(arg);
         this.evalExpression = undefined;
 
-        //this.disableHalos();
+        this.disableHalos();
         //this.cellformula='';
         //this.annotation='';//maybe we need array object to save more than one
     },

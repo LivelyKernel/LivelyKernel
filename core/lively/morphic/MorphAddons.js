4540bd2edd639a50bb6dd186d4e0c8c5c0ca3bcd
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-01 01:50:31 +0200

    [mirror commit] {   "rev": "168094",   "author": "bgnauk",   "changes": "U   core/lively/morphic/MorphAddons.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/MorphAddons.js\n" }

--- core/lively/morphic/MorphAddons.js
+++ core/lively/morphic/MorphAddons.js
@@ -233,7 +233,7 @@ lively.morphic.Morph.addMethods(
 
     removeAndDropSubmorphs: function() {
         // Removes the morph and lets all its child morphs drop to its owner
-        this.withAllSubmorphsDo(function(submorph){
+        this.submorphs.each(function(submorph){
             console.log(submorph);
             this.owner.addMorph(submorph.copy());
             this.submorphs = this.submorphs.without(submorph);

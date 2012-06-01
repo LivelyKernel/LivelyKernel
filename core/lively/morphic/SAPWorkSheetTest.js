3e61af532a62372049eef8f2bd3ffe958fab6d62
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-06-01 22:19:14 +0200

    [mirror commit] {   "rev": "168439",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheetTest.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheetTest.js\n" }

--- core/lively/morphic/SAPWorkSheetTest.js
+++ core/lively/morphic/SAPWorkSheetTest.js
@@ -1543,6 +1543,7 @@ lively.morphic.Text.subclass('lively.morphic.SAPGridCell',
 'default category', {
     setExtent: function ($super,oPos) {
         console.log("Cell setExtent");
+        $super(oPos);
     },
     addToGrid: function(aGrid) {
         this.grid = aGrid;

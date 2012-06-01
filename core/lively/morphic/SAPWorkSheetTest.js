1da5cc74f136b95ebe65e59e17ee0f0d6a2149e6
kimhw <lively-kernel@hpi.uni-potsdam.de>
2012-06-01 05:51:12 +0200

    [mirror commit] {   "rev": "168133",   "author": "kimhw",   "changes": "U   core/lively/morphic/SAPWorkSheetTest.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPWorkSheetTest.js\n" }

--- core/lively/morphic/SAPWorkSheetTest.js
+++ core/lively/morphic/SAPWorkSheetTest.js
@@ -561,8 +561,8 @@ lively.morphic.Morph.subclass('lively.morphic.SAPGrid',
         var nNumofVisibleRow =  this.getNumofVisibleRow()+1;
         var nNumofVisibleColumn = this.getNumofVisibleColumn()+1;     
 
-        if (newX ==0 || newY==0 || newX >= nNumofVisibleColumn  || newY==nNumofVisibleRow ){
-        //if (newX ==0 || newY==0 || newX ==this.numCols || newY==this.numRows){
+        //if (newX ==0 || newY==0 || newX >= nNumofVisibleColumn  || newY==nNumofVisibleRow ){
+        if (newX ==0 || newY==0 || newX ==this.numCols || newY==this.numRows){
             return;
         }
        

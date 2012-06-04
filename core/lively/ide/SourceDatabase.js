a2448cee04c3bffc6e5cd6b43d0cc9434cd4a1ef
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-06-04 19:14:52 +0200

    [mirror commit] {   "rev": "168875",   "author": "cschuster",   "changes": "U   core/lively/ide/SourceDatabase.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ide/SourceDatabase.js\n" }

--- core/lively/ide/SourceDatabase.js
+++ core/lively/ide/SourceDatabase.js
@@ -244,13 +244,13 @@ Object.subclass('AnotherSourceDatabase', {
     findModuleWrapperForFileName: function(fileName) {
         // support for Config.modulePaths == [users/, projects/]
         // FIXME this is a hack
-        console.log("** in file name: " + fileName);
+        //console.log("** in file name: " + fileName);
         var m = fileName.match(/\.\.\/([A-Za-z0-9]+\/)(.*)/);
         if (m && Config.modulePaths.include(m[1])) {
             fileName = m[1] + m[2];
         }
         return this.allModules().detect(function(ea) {
-            console.log(ea.fileName()); 
+            //console.log(ea.fileName()); 
             return ea.fileName() == fileName 
         })
     },

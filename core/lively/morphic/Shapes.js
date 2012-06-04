34a7624b79f58f45e2d22ebc08703ff5a154209d
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-05 00:06:40 +0200

    [mirror commit] {   "rev": "168990",   "author": "bgnauk",   "changes": "U   core/lively/morphic/Shapes.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Shapes.js\n" }

--- core/lively/morphic/Shapes.js
+++ core/lively/morphic/Shapes.js
@@ -35,6 +35,7 @@ Object.subclass('lively.morphic.Shapes.Shape',
     },
     setBorderWidth: function(width) { return this.shapeSetter('BorderWidth', width) },
     getBorderWidth: function() {
+        console.log("This shape has a border styling mode of: "+this.getBorderStylingMode());
         return this.shapeGetter('BorderWidth')  || 0;
     },
     setBorderColor: function(fill) { return this.shapeSetter('BorderColor', fill) },

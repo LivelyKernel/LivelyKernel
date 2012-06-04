06a29ea01668ee7073626d7018c3ec202f28c7f1
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-04 20:36:14 +0200

    [mirror commit] {   "rev": "168902",   "author": "bgnauk",   "changes": "U   core/lively/morphic/MorphAddons.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/MorphAddons.js\n" }

--- core/lively/morphic/MorphAddons.js
+++ core/lively/morphic/MorphAddons.js
@@ -236,7 +236,7 @@ lively.morphic.Morph.addMethods(
         this.submorphs.each(function(submorph){
             console.log(submorph);
             
-            var supermorph = null; // ?
+            var supermorph = this.owner; // ?
             console.log(supermorph);
                supermorph.addMorph(submorph.copy());
             this.submorphs = this.submorphs.without(submorph);

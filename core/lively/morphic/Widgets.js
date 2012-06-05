97dc2b57956cf86027efa4767a77cedbf66a7dc2
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-05 22:47:56 +0200

    [mirror commit] {   "rev": "169384",   "author": "bgnauk",   "changes": "U   core/lively/morphic/Widgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Widgets.js\n" }

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -552,7 +552,7 @@ lively.morphic.Box.subclass('lively.morphic.Menu',
             if (item.isSubMenu) {
                 var arrowMorph = new lively.morphic.Text(
                     new Rectangle(this.getExtent().x-10, y, 10, itemHeight), "▶");
-                itemMorph.applyStyle({
+                arrowMorph.applyStyle({
                 clipMode: 'hidden',
                 fixedHeight: true,
                 fixedWidth: false,

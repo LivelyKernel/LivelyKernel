9f52205854a7c5b3b689f0cb18e5a3e106e5759d
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-12 23:56:47 +0200

    [mirror commit] {   "rev": "170745",   "author": "bgnauk",   "changes": "U   core/lively/morphic/Events.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Events.js\n" }

--- core/lively/morphic/Events.js
+++ core/lively/morphic/Events.js
@@ -674,7 +674,7 @@ handleOnCapture);
             var extent = this.getExtent();
             var scroll = this.getScroll();            
             
-            console.log("    offsetY: "+ evt.offsetX+ "      Y scroll: "+scroll[1]+ "   extent.y- scrollbarExtent.y: "+(extent.y- scrollbarExtent.y));
+            console.log("    offsetY: "+ evt.offsetY+ "      Y scroll: "+scroll[1]+ "   extent.y- scrollbarExtent.y: "+(extent.y- scrollbarExtent.y));
             
             
             if ((evt.offsetX> extent.x- scrollbarExtent.x + scroll[0]) || (evt.offsetY> extent.y- scrollbarExtent.y + scroll[1])) {

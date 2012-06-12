9b7b2b068c371d4fd4ae43bf523842b41322b8b6
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-06-13 01:05:32 +0200

    [mirror commit] {   "rev": "170768",   "author": "cschuster",   "changes": "U   core/lively/ast/tests/AstTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/tests/AstTests.js\n" }

--- core/lively/ast/tests/AstTests.js
+++ core/lively/ast/tests/AstTests.js
@@ -158,6 +158,7 @@ TestCase.subclass('lively.ast.tests.AstTests.ParserTest',
                 ['binop', [0, 3], '&', ['number', [0, 1], 1], ['number', [2, 3], 1]],
                 ['number', [4, 5], 1]],
             result = this.parseJS(src, 'expr');
+        inspect(result);
         this.assertMatches(expected, result);
     }
 });

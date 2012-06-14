af5ad51b15e62cfac9714080e9c005e8d9b1d72d
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-06-14 20:18:20 +0200

    [mirror commit] {   "rev": "171191",   "author": "cschuster",   "changes": "U   core/lively/lang/Object.js\n",   "msg": "added addScript method to all objects\n" }

--- core/lively/lang/Object.js
+++ core/lively/lang/Object.js
@@ -302,3 +302,15 @@ Properties = {
     }
 
 };
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Extensions to Object instances
+///////////////////////////////////////////////////////////////////////////////
+
+Object.extend(Object.prototype, {
+    addScript: function (funcOrString, optName) {
+        var func = Function.fromString(funcOrString);
+        return func.asScriptOf(this, optName);
+    }
+});

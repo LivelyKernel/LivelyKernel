aba63684e9e6ba798a3ce3c65ae1a9c7a64ee708
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-15 23:46:09 +0200

    [mirror commit] {   "rev": "171388",   "author": "bgnauk",   "changes": "U   core/lively/morphic/ModernWindow.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/ModernWindow.js\n" }

--- core/lively/morphic/ModernWindow.js
+++ core/lively/morphic/ModernWindow.js
@@ -269,7 +269,7 @@ lively.morphic.Morph.subclass('lively.morphic.Window',
 
     alignBottomReframeHandle: function() {
         if (this.bottomReframeHandle) {
-            console.log(this.bottomReframeHandle.getPosition());
+            console.log(this.bounds().bottomLeft());
             this.bottomReframeHandle.align(this.bounds().bottomLeft(), pt(0,this.bottomReframeHandle.owner.getExtent().height));
             console.log(this.bottomReframeHandle.getPosition());
         }

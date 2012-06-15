a300d72482aab8278dbc49255105db255aa07a58
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-16 00:28:47 +0200

    [mirror commit] {   "rev": "171407",   "author": "bgnauk",   "changes": "U   core/lively/morphic/ModernWindow.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/ModernWindow.js\n" }

--- core/lively/morphic/ModernWindow.js
+++ core/lively/morphic/ModernWindow.js
@@ -242,7 +242,7 @@ lively.morphic.Morph.subclass('lively.morphic.Window',
         var theExtent = this.getExtent();
         //var handle = lively.morphic.Morph.makeRectangle(0,0,theExtent.x, this.spacing);
         var handle = lively.morphic.Morph.makePolygon(
-            [pt(0, 0), pt(theExtent.x, 0), pt(theExtent.x, this.spacing), pt(0, this.spacing)], 0, null, Color.red);
+            [pt(0, 0), pt(theExtent.x, 0), pt(theExtent.x, this.spacing), pt(0, this.spacing)], 0, null, Color.purple);
         handle.applyStyle({fill: Color.purple}); 
         handle.addScript(function onDragStart(evt) {
             this.dragStartPoint = evt.mousePoint;

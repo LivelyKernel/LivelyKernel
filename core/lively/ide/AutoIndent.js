da640a2330283a5066748d0e824d7ce03ffe1e36
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-06-15 23:23:03 +0200

    [mirror commit] {   "rev": "171361",   "author": "cschuster",   "changes": "U   core/lively/ide/AutoIndent.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ide/AutoIndent.js\n" }

--- core/lively/ide/AutoIndent.js
+++ core/lively/ide/AutoIndent.js
@@ -1,5 +1,18 @@
-module('lively.ide.AutoIndent').requires().toRun(function() {
+module('lively.ide.AutoIndent').requires('lively.morphic.TextCore').toRun(function() {
 
-// Enter your code here
+cop.create('AutoIndentLayer').refineClass(lively.morphic.Text, {
+    onEnterPressed: function(evt) {
+        cop.proceed(evt);
+        if (!this.isInputLine && !evt.isCommandKey() && this.getSelectionRange()) {
+            var endOfLastLine = this.getSelectionRange()[0] - 2;
+            var beginOfLastLine = this.textString.lastIndexOf('\n', endOfLastLine);
+            var lastLine = this.textString.substring(beginOfLastLine + 1, endOfLastLine + 1);
+            var indent = lastLine.match(/^[\ \t]*/).join();
+            if (['{','[','('].include(this.textString[endOfLastLine])) indent += this.tab;
+            this.insertAtCursor(indent, false, false);
+        }
+        return true;
+    },
+})
 
 }) // end of module

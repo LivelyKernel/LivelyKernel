f30d26251f86385cefa723eb7bc5850e061ddaeb
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-15 02:08:03 +0200

    [mirror commit] {   "rev": "171220",   "author": "bgnauk",   "changes": "U   core/lively/morphic/ModernWindow.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/ModernWindow.js\n" }

--- core/lively/morphic/ModernWindow.js
+++ core/lively/morphic/ModernWindow.js
@@ -1,5 +1,13 @@
-module('lively.morphic.ModernWindow').requires().toRun(function() {
+module('lively.morphic.ModernWindow').requires('lively.morphic.Widgets').toRun(function() {
 
-// Enter your code here
+lively.morphic.Morph.subclass('lively.morphic.Window', 
+'documentation', {
+    documentation: "Full-fledged AND good looking windows with title bar, menus, etc",
+},
+'rest',
+{
+
+
+});
 
 }) // end of module

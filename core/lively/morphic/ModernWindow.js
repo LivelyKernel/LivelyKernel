fac935511eca3f39a4a3cee4fd52138babf0b550
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-15 19:54:55 +0200

    [mirror commit] {   "rev": "171317",   "author": "bgnauk",   "changes": "U   core/lively/morphic/ModernWindow.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/ModernWindow.js\n" }

--- core/lively/morphic/ModernWindow.js
+++ core/lively/morphic/ModernWindow.js
@@ -126,15 +126,15 @@ lively.morphic.WindowControl.subclass("lively.morphic.ModernWindowControl",
 },
 
 'style', {
-    style: "background: #3d9eff; background-image: linear-gradient(270deg, #3d9eff 0%, #0060bf 100%); border: 1px solid #0066cc;  border-radius: 4px;   box-shadow: inset 0px 1px 0px 0px rgba(255, 255, 255, 0.6), 0px 2px 4px 0px rgba(0, 0, 0, 0.8);"
+    style: "background: #3d9eff; background-image: linear-gradient(270deg, #3d9eff 0%, #0060bf 100%); border: 1px solid #0066cc;  border-radius: 4px;"
 },
 
 'initializing', {
     initialize: function($super, bnds, inset, labelString, labelOffset, color) {
         $super(bnds, inset, labelString, labelOffset);
-        this.setStyleSheet(this.style);
         this.setAppearanceStylingMode(true);
         this.setBorderStylingMode(true);
+        this.setStyleSheet(this.style);
     },
 });
 

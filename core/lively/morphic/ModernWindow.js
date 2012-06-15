b66107caab490cb506639e602d1430a5784b4a72
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-15 02:10:30 +0200

    [mirror commit] {   "rev": "171221",   "author": "bgnauk",   "changes": "U   core/lively/morphic/ModernWindow.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/ModernWindow.js\n" }

--- core/lively/morphic/ModernWindow.js
+++ core/lively/morphic/ModernWindow.js
@@ -4,10 +4,36 @@ lively.morphic.Morph.subclass('lively.morphic.Window',
 'documentation', {
     documentation: "Full-fledged AND good looking windows with title bar, menus, etc",
 },
-'rest',
+'initializing', {
+    initialize: function($super, targetMorph, titleString, optSuppressControls) {
+        $super(new lively.morphic.Shapes.Rectangle());
+        this.LK2 = true; // to enable workaround in WindowMorph trait.expand
+
+        var bounds      = targetMorph.bounds(),
+            titleBar    = this.makeTitleBar(titleString, bounds.width, optSuppressControls),
+            titleHeight = titleBar.bounds().height - titleBar.getBorderWidth();
+        this.setBounds(bounds.withHeight(bounds.height + titleHeight));
+        this.targetMorph = this.addMorph(targetMorph);
+        this.reframeHandle = this.addMorph(this.makeReframeHandle());
+        this.alignReframeHandle();
+        this.titleBar = this.addMorph(titleBar);
+        this.contentOffset = pt(0, titleHeight);
+        targetMorph.setPosition(this.contentOffset);
+        // this.closeAllToDnD();
+
+        this.collapsedTransform   = null;
+        this.collapsedExtent      = null;
+        this.expandedTransform    = null;
+        this.expandedExtent       = null;
+        this.ignoreEventsOnExpand = false;
+        this.disableDropping();
+        return this;
+    },
+
+},'rest',
 {
 
-
-});
+}
+);
 
 }) // end of module

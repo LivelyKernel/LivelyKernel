bb3734279dc13d39c1c20041fa267ad64f80e6b6
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-15 21:08:46 +0200

    [mirror commit] {   "rev": "171351",   "author": "bgnauk",   "changes": "U   core/lively/morphic/ModernWindow.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/ModernWindow.js\n" }

--- core/lively/morphic/ModernWindow.js
+++ core/lively/morphic/ModernWindow.js
@@ -138,7 +138,7 @@ lively.morphic.WindowControl.subclass("lively.morphic.ModernWindowControl",
         //this.setBorderStylingMode(true);
         this.applyStyle({borderWidth: 1, borderRadius: 3, borderColor: Color.rgbHex("888888")});
         this.label.applyStyle({emphasize: {fontWeight: 'bold'}});
-        this.label.setPosition(this.label.getPosition().addPt(labelOffset));
+        //this.label.setPosition(this.label.getPosition().addPt(labelOffset));
         this.setStyleSheet(this.style +"background: "+color+";");
     },
 });

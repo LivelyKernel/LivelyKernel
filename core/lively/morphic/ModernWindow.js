55b127e0386c4989624a0bf3c1d0eafbd344a43f
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-15 20:58:37 +0200

    [mirror commit] {   "rev": "171341",   "author": "bgnauk",   "changes": "U   core/lively/morphic/ModernWindow.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/ModernWindow.js\n" }

--- core/lively/morphic/ModernWindow.js
+++ core/lively/morphic/ModernWindow.js
@@ -89,7 +89,7 @@ lively.morphic.TitleBar.subclass("lively.morphic.ModernTitleBar",
             var cell = new Rectangle(0, 0, this.barHeight-5, this.barHeight-5);
 
             this.closeButton = this.addMorph(
-                new lively.morphic.ModernWindowControl(cell, this.controlSpacing, "X", pt(-4,-4), "#E73F22"));
+                new lively.morphic.ModernWindowControl(cell, this.controlSpacing, "X", pt(-3,-3), "#E73F22"));
             this.closeButton.applyStyle({moveHorizontal: true});
             this.closeButton.label.applyStyle({textColor: Color.white});
             //this.closeButton.linkToStyles('titleBar_closeButton');

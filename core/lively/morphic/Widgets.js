89d895f5b334cff4b38928a88648fbb2be345945
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-18 22:05:21 +0200

    [mirror commit] {   "rev": "171670",   "author": "bgnauk",   "changes": "U   core/lively/morphic/Widgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Widgets.js\n" }

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -165,7 +165,6 @@ lively.morphic.Button.subclass('lively.morphic.ImageButton',
 'initializing', {
     initialize: function($super, bounds, url) {
         if (bounds) this.setBounds(bounds);
-        // calling $super.$super ...
         $super(bounds, '');
         
         this.image = new lively.morphic.Image(this.getExtent().extentAsRectangle(), url, true);

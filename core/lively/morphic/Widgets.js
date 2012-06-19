62ebc241134f1165c2efbc4987022f92d687054b
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-19 23:18:02 +0200

    [mirror commit] {   "rev": "171936",   "author": "bgnauk",   "changes": "U   core/lively/morphic/Widgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Widgets.js\n" }

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -85,7 +85,7 @@ lively.morphic.Morph.subclass('lively.morphic.Button',
                     var labelPadding = pressed ? this.style.label.padding.withY(this.style.label.padding.y+1):this.style.label.padding;
             this.setPadding(labelPadding);
         }
-        this.setFill(this.generateFillWith(baseColor, shade, upperGradientCenter, lowerGradientCenter));
+        this.setFill(this.generateFillWith(baseColor, shade, upperGradientCenter, lowerGradientCenter, bottomShade ));
     },
     applyStyle: function($super, spec) {
         $super(spec);

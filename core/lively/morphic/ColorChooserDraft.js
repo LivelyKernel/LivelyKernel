4a1ec571effd9352f19cb24c355c3158f5d2de4e
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-19 23:14:29 +0200

    [mirror commit] {   "rev": "171932",   "author": "bgnauk",   "changes": "U   core/lively/morphic/ColorChooserDraft.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/ColorChooserDraft.js\n" }

--- core/lively/morphic/ColorChooserDraft.js
+++ core/lively/morphic/ColorChooserDraft.js
@@ -100,13 +100,7 @@ lively.morphic.Button.subclass('lively.morphic.SimpleColorField',
         this.colorDisplay.applyStyle({borderRadius: this.colorDisplayBorderRadius, resizeWidth: true, resizeHeight: true});
         this.applyStyle({adjustForNewBounds: true});
     },
-    
-    setValue: function(bool) {
-        this.value = bool;
-        this.changeAppearanceFor(bool);
         
-    },
-    
     
 }
 

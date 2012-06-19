6ac297f8bbc64641a6821b90045c9a4f4307d592
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-19 23:22:33 +0200

    [mirror commit] {   "rev": "171937",   "author": "bgnauk",   "changes": "U   core/lively/morphic/Widgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Widgets.js\n" }

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -4,7 +4,7 @@ lively.morphic.Morph.subclass('lively.morphic.Button',
 'settings', {
     isButton: true,
     
-    normalColor: Color.rgbHex('#CCCCCC'),
+    normalColor: Color.rgbHex('#DDDDDD'),
     toggleColor: Color.rgb(171,215,248),
     
     style: {

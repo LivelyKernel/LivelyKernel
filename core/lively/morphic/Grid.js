ad43e7c2c5e95831750936b68fb2ae9c47522b97
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-06-20 18:59:13 +0200

    [mirror commit] [ci skip] {   "rev": "172051",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/Grid.js\n",   "msg": "removing debug code\n\n" }

--- core/lively/morphic/Grid.js
+++ core/lively/morphic/Grid.js
@@ -446,10 +446,6 @@ lively.morphic.Text.subclass('lively.morphic.DataGridCell',
     }
 },
 'default category', {
-    setExtent: function ($super,oPos) {
-        console.log("Cell setExtent");
-        $super(oPos);
-    },
     addToGrid: function(aGrid) {
         this.grid = aGrid;
         this.grid.addMorph(this);

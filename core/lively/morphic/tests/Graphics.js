e46fd6516093b86308308bcf366ea03e5407f752
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-21 19:38:07 +0200

    [mirror commit] [ci skip] {   "rev": "172082",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/Graphics.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/Graphics.js\n" }

--- core/lively/morphic/tests/Graphics.js
+++ core/lively/morphic/tests/Graphics.js
@@ -1,6 +1,6 @@
 module('lively.morphic.tests.Graphics').requires().toRun(function() {
 
-lively.morphic.tests.TestCase.subclass('lively.morphic.tests.ColorTests',
+lively.morphic.tests.TestCase.subclass('lively.morphic.tests.Graphics.ColorTests',
 'testing', {
     testRgbHex: function() {
         this.assert(Color.rgbHex("#FFFFFF").equals(new Color(1,1,1)),"#FFFFFF should be white");

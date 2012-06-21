05404f5927f6652f53f0b78773153d4850fab43c
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-21 18:48:42 +0200

    [mirror commit] [ci skip] {   "rev": "172079",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/Graphics.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/Graphics.js\n" }

--- core/lively/morphic/tests/Graphics.js
+++ core/lively/morphic/tests/Graphics.js
@@ -1,5 +1,20 @@
 module('lively.morphic.tests.Graphics').requires().toRun(function() {
 
-// Enter your code here
+lively.morphic.tests.TestCase.subclass('lively.morphic.tests.ColorTests',
+'testing', {
+    testAddWorldToDoc: function() {
+        var canvasNode = document.body, bounds = new Rectangle(0, 0, 100, 100),
+            world = new lively.morphic.World();
+        try {
+            world.setBounds(bounds);
+            world.displayOnCanvas(canvasNode);
+
+            var expected = {tagName: 'div', parentNode: canvasNode, childNodes: [{tagName: 'div'}]};
+            this.assertNodeMatches(expected, world.renderContext().getMorphNode());
+        } finally {
+            world.remove();
+        }
+    },
+});
 
 }) // end of module

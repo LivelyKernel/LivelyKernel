8b48ff0556d5309ba2afb93dda3c8913abef08c3
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-23 00:15:25 +0200

    [mirror commit] [ci skip] {   "rev": "172325",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -1,6 +1,46 @@
 module('lively.morphic.SAPUI5').requires().toRun(function() {
 
+lively.morphic.Morph.subclass('lively.morphic.SAPUI5.Button',
+'properties', {
+    connections: {
+        setChecked: {}
+    }
+},
+'initializing', {
+    initialize: function($super, isChecked) {
+        $super(this.createShape());
+    },
+    createShape: function() {
+        var node = XHTMLNS.create('button');
+        node.type = 'checkbox';
+        return new lively.morphic.Shapes.External(node);
+    },
+},
+'accessing', {
+},
+'event handling', {
 
+    onClick: function(evt) {
+        // for halos/menus
+         if (evt.isCommandKey() || !evt.isLeftMouseButtonDown()) {
+            evt.stop()
+            return true;
+        }
+        // we do it ourselves
+
+        // evt.stop();
+         return true;
+     },
+
+},
+'serialization', {
+    prepareForNewRenderContext: function ($super, renderCtx) {
+        $super(renderCtx);
+        // FIXME what about connections to this.isChecked?
+        // they would be updated here...
+        this.setChecked(this.isChecked());
+    },
+});
 
 
 }) // end of module

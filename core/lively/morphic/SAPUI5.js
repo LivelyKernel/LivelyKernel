58a33dc7008e7570f4e656fe2a1df9944b4b93a0
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-23 00:42:00 +0200

    [mirror commit] [ci skip] {   "rev": "172333",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -2,7 +2,7 @@ module('lively.morphic.SAPUI5').requires().toRun(function() {
 
 lively.morphic.Morph.subclass('lively.morphic.SAPUI5.Button',
 'initializing', {
-    initialize: function($super, isChecked) {
+    initialize: function($super) {
         $super(this.createShape());
     },
     createShape: function() {

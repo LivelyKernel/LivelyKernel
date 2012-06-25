8c221f8940b2dff5ddb01f6edf6d13030ba25865
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-26 01:14:15 +0200

    [mirror commit] [ci skip] {   "rev": "172557",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTML.js\n" }

--- core/lively/morphic/HTML.js
+++ core/lively/morphic/HTML.js
@@ -1247,6 +1247,7 @@ lively.morphic.Shapes.Shape.addMethods(
 			var webRGet = webR.get();
 			if (webRGet.status.code() == 200) {
 			    // add resource path to all relative urls in the css
+			    /*
 			    var resPath = resourcePath;
 			    if (!resPath){
                                 resPath = absPath = absPath.substring(0, absPath.lastIndexOf('/') + 1);   
@@ -1255,7 +1256,7 @@ lively.morphic.Shapes.Shape.addMethods(
 			    var urlReplaceSingle = "url('"+resPath;
 			    var urlReplaceDouble = 'url("'+resPath;			    			    
 			    var css = webRGet.content.replace(/url\([\s]*\'(?![\s]*http)/g, urlReplaceSingle).replace(/url\([\s]*\"(?![\s]*http)/g, urlReplaceDouble ).replace(/url\((?![\s]*[\'|\"])(?![\s]*http)/g, urlReplace );
-
+                            */
                             // insert line breaks so the css is more legible
                             //css = css.replace(/\;(?![\s]*(\r\n|\n|\r))/g,";\n").replace(/\}(?![\s]*(\r\n|\n|\r))/g,"}\n").replace(/\{(?![\s]*(\r\n|\n|\r))/g,"{\n");
                             //console.log(css);

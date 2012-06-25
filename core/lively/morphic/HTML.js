06ce0610abd9bc2501c9286641504154ce655c3a
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-25 21:24:38 +0200

    [mirror commit] [ci skip] {   "rev": "172479",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTML.js\n" }

--- core/lively/morphic/HTML.js
+++ core/lively/morphic/HTML.js
@@ -1251,9 +1251,11 @@ lively.morphic.Shapes.Shape.addMethods(
 			    
 
 			    var urlReplace = "url("+resPath;
-			    var urlReplace = "url("+resPath;
-			    var urlReplace = "url("+resPath;			    			    
+			    var urlReplaceDouble = "url('"+resPath;
+			    var urlReplaceSingle = 'url("'+resPath;			    			    
 			    var css = webRGet.content.replace(/url\([\s]*\'[\s]*(?!http)/g, urlReplace );
+			    css = webRGet.content.replace(/url\([\s]*(?!http)/g, urlReplaceSingle );
+			    css = webRGet.content.replace(/url\([\s]*\"[\s]*(?!http)/g, urlReplaceDouble );
                             
                             // set the style sheet
                             this.setStyleSheet(css);

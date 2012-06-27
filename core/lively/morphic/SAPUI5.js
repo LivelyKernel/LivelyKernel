2668c99d70224d8fe1083098dcd16b45f27b6a57
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-27 02:15:04 +0200

    [mirror commit] [ci skip] {   "rev": "172929",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -588,5 +588,184 @@ lively.morphic.List.subclass('lively.morphic.SAPUI5.ListBox',
 
 
 
+lively.morphic.SAPUI5.CheckBox.subclass('lively.morphic.SAPUI5.RadioButton',
+
+'settings',{
+    baseClass:'sapUiCb',
+    activeClass: 'sapUiCbInteractive sapUiCbStd', 
+    checkedClass: 'sapUiCbChk',
+    disabledClass: 'sapUiCbDis',
+    readOnlyClass: 'sapUiCbRo',
+    label: "Checkbox"
+},
+'HTML render settings', {
+    htmlDispatchTable: {
+        updateLabel: 'updateLabelHTML',
+        resizeCheckBox: 'resizeCheckBoxHTML',
+        getCheckBoxExtent: 'getCheckBoxExtentHTML',
+        setWrapperNodeClass: 'setWrapperNodeClassHTML',
+        updateInputTag: 'updateInputTagHTML'
+    },
+},
+'initializing', {
+    initialize: function($super, bounds, optLabel) {
+        $super(bounds);
+        if (optLabel) this.setLabel(optLabel);
+        this.readOnly = false;
+        this.checked = false;
+        this.active = true;
+        this.updateAppearance();
+    }
+},
+
+'rendering', {
+    initHTML: function($super, ctx) {
+        if (!ctx.wrapperNode) ctx.wrapperNode= XHTMLNS.create('div');
+        if (!ctx.checkBoxNode) this.setupCheckBoxNodeHTML(ctx);
+        if (!ctx.labelNode) this.setupLabelNodeHTML(ctx);
+        
+        this.setWrapperNodeClass(this.active?this.classes:this.disabledClasses);
+        ctx.subNodes = [];
+        $super(ctx);
+        if (this.shape) this.updateLabel(this.label || "Button")
+    },
+    setupCheckBoxNodeHTML: function(ctx){
+        var c = XHTMLNS.create('input');
+        c.type = "checkbox";
+        c.id = 'checkbox-'+this.id;
+        ctx.checkBoxNode = c;
+    },    
+    setupLabelNodeHTML: function(ctx){
+        var l = XHTMLNS.create('label');
+        l.htmlFor = 'checkbox-'+this.id;
+        ctx.labelNode = l;
+    },
+    appendHTML: function($super, ctx, optMorphAfter) {
+        $super(ctx, optMorphAfter);
+        ctx.wrapperNode.appendChild(ctx.checkBoxNode);
+        ctx.wrapperNode.appendChild(ctx.labelNode);
+        ctx.shapeNode.appendChild(ctx.wrapperNode);
+        this.resizeCheckBoxHTML(ctx);
+    },
+    setClipModeHTML: function(ctx, clipMode) {
+        // FIXME duplication wiht super, delay logic
+        // can be extracted
+        if (!ctx.wrapperNode|| this.delayedClipMode) {
+            this.delayedClipMode = clipMode;
+            return;
+        }
+        this.setClipModeHTMLForNode(ctx, ctx.wrapperNode, clipMode);
+    },
+    resizeCheckBoxHTML: function(ctx) {
+        var borderWidth = this.getBorderWidth(),
+            extent = this.getExtent().subPt(pt(2*borderWidth, 2*borderWidth)),
+            wrapperNode= ctx.wrapperNode;
+        wrapperNode.style.left = this.shape.getPosition().x /*+ this.padding.left()*/ + 'px';
+        wrapperNode.style.top = this.shape.getPosition().y /*+ this.padding.top()*/ + 'px';
+        wrapperNode.style.width = extent.x /*- this.padding.right() - this.padding.left())*/ + 'px';
+        wrapperNode.style.height = extent.y /*- this.padding.bottom() - this.padding.top()*/ + 'px';
+    },
+    updateLabelHTML: function(ctx, label) {
+        ctx.labelNode.innerHTML = label;
+        ctx.checkBoxNode.title = label;
+    },
+    setWrapperNodeClassHTML: function(ctx, className) {
+        ctx.wrapperNode.className = className;
+    },
+    
+    
+    updateInputTagHTML: function(ctx) {
+        ctx.checkBoxNode.checked = (this.checked)?"checked":null;
+        ctx.checkBoxNode.disabled= (this.active)?null:"disabled";        
+        ctx.checkBoxNode.readOnly= (this.readOnly)?"readOnly":null;   
+    }
+    
+},
+
+'node creation', {
+
+    getWrapperExtentHTML: function(ctx) {
+        return ctx.wrapperNode.scrollHeight != 0 ? pt(ctx.wrapperNode.scrollWidth, ctx.wrapperNode.scrollHeight) : this.getExtent()
+    },
+},
+
+'accessing', {
+    setChecked: function(checked){ 
+        this.checked = checked;
+        this.updateAppearance();
+    },
+    
+    setActive: function(active) {
+        this.active = active;
+        this.updateAppearance();
+    },
+    setReadOnly: function(readOnly ) {
+        this.readOnly = readOnly ;
+        this.updateAppearance();
+    },
+    setExtent: function($super, extent) {
+        $super(extent);
+        this.resizeCheckBox();
+    },
+    updateInputTag: function(idx) {
+        return this.renderContextDispatch('updateInputTag');
+    },
+    resizeCheckBox: function(idx) {
+        return this.renderContextDispatch('resizeCheckBox');
+    },
+    getWrapperExtent: function() { return this.renderContextDispatch('getWrapperExtent') },
+    updateLabel: function(label) {
+        this.label = label;
+        this.renderContextDispatch('updateLabel', label);
+    },
+    setLabel: function(label) {
+        this.updateLabel(label);
+    },
+    getLabel: function() {
+        return this.label;    
+    },
+    morphMenuItems: function($super) {
+        var self = this, items = $super();
+        items.push([
+            'Set label', function(evt) {
+            $world.prompt('Set label', function(input) {
+                if (input !== null)
+                    self.setLabel(input || '');
+            }, self.getLabel());
+        }])
+        return items;
+    },
+    setWrapperNodeClass: function(className) {
+        this.renderContextDispatch('setWrapperNodeClass', className);     
+    }
+    
+},
+'event handling', {
+    updateAppearance: function() {
+
+        var classNames = this.baseClass;
+        
+        if (this.checked) { classNames+=' '+this.checkedClass}
+        if (this.readOnly) {classNames+=' '+this.readOnlyClass}
+            else if (this.active) {classNames+=' '+this.activeClass}
+            else {classNames+=' '+this.disabledClass}
+        this.setWrapperNodeClass(classNames);
+        this.updateInputTag();
+    },
+    
+    onChange: function(evt) {
+
+        if (this.active && !this.readOnly) {
+            
+            lively.bindings.signal(this, 'fire', true);
+            this.setChecked(!this.checked);
+        }
+         return true;
+     },
+
+}
+);
+
+
 
 }) // end of module

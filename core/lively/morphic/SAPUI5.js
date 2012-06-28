6bc24dec2484d94135ac6e31ba853217a5e2101b
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-29 01:43:42 +0200

    [mirror commit] [ci skip] {   "rev": "173267",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -1053,7 +1053,7 @@ lively.morphic.SAPUI5.Component.subclass('lively.morphic.SAPUI5.Slider',
     addSliderEventsHTML: function(ctx, sliderNode){
         sliderNode.onmousedown = this.onSliderMouseDown;    
         sliderNode.onmouseup = this.onSliderMouseUp;
-        sliderNode.onmousemove = this.onSliderMouseMove;                
+        //sliderNode.onmousemove = this.onSliderMouseMove;                
     },
 
     onSliderMouseDown: function(evt){

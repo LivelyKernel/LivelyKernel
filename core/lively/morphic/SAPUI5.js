fb9c1bc152b41dbcbc6a0e0c1538ea94108364ba
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-06-29 23:48:35 +0200

    [mirror commit] [ci skip] {   "rev": "173347",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -423,6 +423,7 @@ lively.morphic.SAPUI5.LabelComponent.subclass('lively.morphic.SAPUI5.CheckBox',
         this.updateInputTag();
     },
     
+    
     onChange: function(evt) {
 
         if (this.active && !this.readOnly) {

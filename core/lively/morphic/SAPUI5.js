1f8f37b72c211d101b2cb52034a7eaee7e4a78eb
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-02 23:14:26 +0200

    [mirror commit] [ci skip] {   "rev": "173568",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -1238,6 +1238,7 @@ lively.morphic.SAPUI5.ListBox.subclass('lively.morphic.SAPUI5.DropDownListBox',
     itemClass: 'sapUiLbxI',
     selectedItemClass: 'sapUiLbxISel',
     itemSpanClass: 'sapUiLbxITxt',
+    maxHeight: 350
 },
 'init', {
     
@@ -1248,8 +1249,16 @@ lively.morphic.SAPUI5.ListBox.subclass('lively.morphic.SAPUI5.DropDownListBox',
 
     remove: function($super){
         return $super();    
-    }
-    
+    },
+    resizeListHTML: function(ctx) {
+        var borderWidth = this.getBorderWidth(),
+            extent = this.getExtent().subPt(pt(2*borderWidth, 2*borderWidth)),
+            listNode = ctx.wrapperNode;
+        listNode.style.left = this.shape.getPosition().x /*+ this.padding.left()*/ + 'px';
+        listNode.style.top = this.shape.getPosition().y /*+ this.padding.top()*/ + 'px';
+        listNode.style.width = extent.x /*- this.padding.right() - this.padding.left())*/ + 'px';
+        listNode.style.maxHeight = this.maxHeight /*- this.padding.bottom() - this.padding.top()*/ + 'px';
+    },
 },
 
 

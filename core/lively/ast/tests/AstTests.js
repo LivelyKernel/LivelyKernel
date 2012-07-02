5b2697fa9dd1ae99fbb1101db1230aa290b0cf8f
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-07-02 22:26:15 +0200

    [mirror commit] [ci skip] {   "rev": "173549",   "author": "cschuster",   "changes": "U   core/lively/ast/tests/AstTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/tests/AstTests.js\n" }

--- core/lively/ast/tests/AstTests.js
+++ core/lively/ast/tests/AstTests.js
@@ -797,9 +797,9 @@ TestCase.subclass('lively.ast.tests.AstTests.VariableAnalyzerTest',
 },
 'testing', {
     test01FindFreeVariable: function() {
-        var src = 'function() { var x = 3; return x + y }',
+        var src = 'function f() { var x = 3; return x + y }',
             result = new lively.ast.VariableAnalyzer().findGlobalVariablesIn(src);
-        this.assertVarsFound(eval(src), [['y', 36, 37]], result);
+        this.assertVarsFound(eval(src), [['y', 38, 39]], result);
     },
     testFindSimpleGlobalRead: function() {
         var codeAndExpected = [

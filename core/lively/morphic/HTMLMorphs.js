82321ced9a06294543d81c101ff850bc8339efd4
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-05 19:32:26 +0200

    [mirror commit] [ci skip] {   "rev": "174098",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTMLMorphs.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTMLMorphs.js\n" }

--- core/lively/morphic/HTMLMorphs.js
+++ core/lively/morphic/HTMLMorphs.js
@@ -1,5 +1,12 @@
 module('lively.morphic.HTMLMorphs').requires().toRun(function() {
 
-// Enter your code here
+lively.morphic.Morph.subclass('lively.morphic.Table',
+'initializing', {
+    initialize: function($super, bounds) {
+        $super(this.defaultShape());
+
+    },
+}
+);
 
 }) // end of module

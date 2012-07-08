2e52f8395a25f7d3126064e59a168f07123e8b0d
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-07-08 11:09:15 +0200

    [mirror commit] [ci skip] {   "rev": "174173",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/Events.js\n",   "msg": "resizing selected morphs with shift + arrow keys\n\n" }

--- core/lively/morphic/Events.js
+++ core/lively/morphic/Events.js
@@ -889,48 +889,37 @@ handleOnCapture);
     onPageDownPressed: function(evt) { return false },
     onRightPressed: function(evt) {
         if (this.eventsAreIgnored) { return false; }
-        if (!this.showsHalos) return false;
-        evt.stop();
-        var dist = evt.isCommandKey() ? 10 : 1;
-        this.world().withSelectedMorphsDo(function(ea) {
-            ea.moveBy(pt(dist, 0));
-            ea.halos && ea.halos.invoke('alignAtTarget');
-        })
-        return true;
+        return this.interactiveMoveOrResize('right', evt);
     },
     onLeftPressed: function(evt) {
         if (this.eventsAreIgnored) { return false; }
-        if (!this.showsHalos) return false;
-        evt.stop();
-        var dist = evt.isCommandKey() ? 10 : 1;
-        this.world().withSelectedMorphsDo(function(ea) {
-            ea.moveBy(pt(-dist, 0));
-            ea.halos && ea.halos.invoke('alignAtTarget');
-        })
-        return true;
+        return this.interactiveMoveOrResize('left', evt);
     },
     onUpPressed: function(evt) {
         if (this.eventsAreIgnored) { return false; }
-        if (!this.showsHalos) return false;
-        evt.stop();
-        var dist = evt.isCommandKey() ? 10 : 1;
-        this.world().withSelectedMorphsDo(function(ea) {
-            ea.moveBy(pt(0, -dist));
-            ea.halos && ea.halos.invoke('alignAtTarget');
-        })
-        return true;
+        return this.interactiveMoveOrResize('up', evt);
     },
     onDownPressed: function(evt) {
         if (this.eventsAreIgnored) { return false; }
+        return this.interactiveMoveOrResize('down', evt);
+    },
+    interactiveMoveOrResize: function(keyPressed, evt) {
         if (!this.showsHalos) return false;
         evt.stop();
-        var dist = evt.isCommandKey() ? 10 : 1;
+        var dist = evt.isCommandKey() ? 10 : 1,
+            operation = evt.isShiftDown() ? 'resizeBy' : 'moveBy',
+            x = 0, y = 0;
+        if (keyPressed === 'left') x = -dist;
+        if (keyPressed === 'right') x = dist;
+        if (keyPressed === 'up') y = -dist;
+        if (keyPressed === 'down') y = dist;
         this.world().withSelectedMorphsDo(function(ea) {
-            ea.moveBy(pt(0, dist));
+            ea[operation](pt(x, y));
             ea.halos && ea.halos.invoke('alignAtTarget');
-        })
+        });
         return true;
-    },
+    }
+
 
 },
 'touch events', {

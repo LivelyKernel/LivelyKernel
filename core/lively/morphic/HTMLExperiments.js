40409c71ec8ac5ccd42c791d9db9ed43d705f193
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-10 01:09:31 +0200

    [mirror commit] [ci skip] {   "rev": "174399",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTMLExperiments.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTMLExperiments.js\n" }

--- core/lively/morphic/HTMLExperiments.js
+++ core/lively/morphic/HTMLExperiments.js
@@ -154,11 +154,15 @@ lively.morphic.Shapes.Shape.subclass('lively.morphic.Shapes.NullShape',
     },
     setExtentHTML: function(ctx, value) {
         if (!ctx.morphNode) return undefined;
-        this.extentOverride = value;
-        var outer = this.getExtent();
-        var inner = pt($(ctx.morphNode).width(), $(ctx.morphNode).height());
-        var delta = outer.subPt(inner);
-        ctx.domInterface.setExtent(ctx.morphNode, value.subPt(delta));
+        if (!value) {
+             ctx.morphNode.style.width = null;
+            ctx.morphNode.style.height = null;           
+        } else {
+            var outer = this.getExtent();
+            var inner = pt($(ctx.morphNode).width(), $(ctx.morphNode).height());
+            var delta = outer.subPt(inner);
+            ctx.domInterface.setExtent(ctx.morphNode, value.subPt(delta));
+        }
         return value;
     },
     getExtent: function(){

46c327b4515fea96b1f44d548de60ee5c61f7598
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-10 20:34:20 +0200

    [mirror commit] [ci skip] {   "rev": "174477",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTMLExperiments.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTMLExperiments.js\n" }

--- core/lively/morphic/HTMLExperiments.js
+++ core/lively/morphic/HTMLExperiments.js
@@ -14,14 +14,12 @@ cop.create('lively.morphic.TestLayer')
 
 cop.create('lively.morphic.RelativeLayer')
 .refineClass(lively.morphic.Morph, {
-    get htmlDispatchTable(){
-        var r = cop.proceed();
-        Object.extend(r, {
+    get renderContextTable(){
+        return Object.extend(cop.proceed(), {
             setContent: 'setContentHTML',
             /*setAttribute: 'setAttributeHTML',
             resetExtent: 'resetExtentHTML'*/
         });
-        return r;
     },
 
     setContent: function(content){

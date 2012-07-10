7b26d900a0e215ae5bc6f5f2993412e8819bd43a
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-11 01:59:56 +0200

    [mirror commit] [ci skip] {   "rev": "174552",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTMLExperiments.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTMLExperiments.js\n" }

--- core/lively/morphic/HTMLExperiments.js
+++ core/lively/morphic/HTMLExperiments.js
@@ -327,7 +327,7 @@ lively.morphic.Shapes.Shape.subclass('lively.morphic.Shapes.HTMLShape',
 },
 'updating', {
     resetExtent: function() {
-    this.renderContextDispatch('resetExtent');     
+        this.renderContextDispatch('resetExtent');     
 
     },
 
@@ -366,109 +366,16 @@ lively.morphic.Shapes.Shape.subclass('lively.morphic.Shapes.HTMLShape',
         return pt(w, h);
     },
 
-    
-    setFillHTML: function(ctx, value) {
-        
-    },
+    setFillHTML: function(ctx, value) {},
 
-    setBorderStyleHTML: function(ctx, value) {
+    setBorderStyleHTML: function(ctx, value) {},
+    setBorderWidthHTML: function(ctx, width) {},
+    setBorderHTML: function(ctx, width, fill, opacity) {},
 
-    },
-    setBorderWidthHTML: function(ctx, width) {
-        this.setBorderHTML(ctx, width, this.getBorderColor(), this.getStrokeOpacity());
-        // since border influences width/height in HTML, see this.setExtentHTML
-        this.setExtentHTML(ctx, this.getExtent());
-        return width;
-    },
+    setOpacityHTML: function(ctx, value) {},
+    setPaddingHTML: function(ctx, r) {},
 
 
-    setBorderHTML: function(ctx, width, fill, opacity) {
-
-    },
-    compensateShapeNode: function(ctx) {
-        // compensates the shapeNode's position for childmorphs,
-        // positions childmorphs against morphNodes (origin!)
-        ctx.originNode.style.setProperty('top', -this.getPosition().y + 'px', 'important');
-        ctx.originNode.style.setProperty('left', -this.getPosition().x + 'px', 'important');
-        ctx.originNode.style.setProperty('position', 'absolute', 'important');
-
-        // FIXME: hack, necessary until the style editor knows
-        // about stroke widths of svg lines instead of using borderWidth...
-        if (ctx.pathNode) return;
-
-        // compensates the shapeNode's borderWidth for childmorphs, borders don't affect submorphs
-        ctx.originNode.style.setProperty('margin-top', -this.getBorderWidth() + 'px', 'important');
-        ctx.originNode.style.setProperty('margin-left', -this.getBorderWidth() + 'px', 'important');
-    },
-    setOpacityHTML: function(ctx, value) {
-
-    },
-    setPaddingHTML: function(ctx, r) {
-
-    },
-    setNodeIdHTML: function(ctx, value) {
-        //console.log("HTML.js, setStyleIdHTML(): Ok, got it, setting shape HTML id to "+value);
-        ctx.morphNode.id = value;
-    },
-    setNodeClassHTML: function(ctx, value) {
-        var a = value;
-        if (value instanceof Array) {
-            a = value.join(" ");
-        }
-        ctx.morphNode.className = a;
-    },
-    setStyleSheetHTML: function(ctx, value) {
-        var morphId = ctx.morphNode.id;
-        if (!morphId) {
-            alert("Cannot set morph specific style sheet. Morph node was not assigned any id.");
-            return;
-        }
-
-        var styleTagId = "style-for-"+morphId;
-
-	    var css = $('#' + styleTagId);
-	    css.remove();
-
-        if (value && value.length > 1) {
-            
-    	    //console.log("Setting CSS for shape "+morphId+" to "+value);
-            var specificCss = "#"+morphId+" { "+value+" }";
-            
-            // syntax fixes for the sap gold reflection css
-            specificCss = specificCss.replace(/[\s]*=[\s]*/g,"=");
-            specificCss = specificCss.replace(/alpha[\s]*\([\s]*opacity[\s]*\:/g,"alpha(opacity=");
-            specificCss = specificCss.replace(".dev-datepicker/jQuery",".dev-datepicker.jQuery");
-                            
-
-            if (less) {
-                new less.Parser().parse(specificCss, function(e, tree) {
-                    console.log(e);
-                    specificCss = tree.toCSS();
-                });
-                console.log(specificCss);
-            }
-
-
-	        css = $('<style type="text/css" id="' + styleTagId + '"></style>');
-	        css.text(specificCss);
-	        css.appendTo(document.head);
-        }
-
-    },
-
-    setComputedStylesHTML: function(ctx) {
-        
-        if (!ctx.morphNode) return;
-        var style = window.getComputedStyle(ctx.morphNode),
-            borderWidth = parseInt(style["borderWidth"].replace("px",""));
-        this.shapeSetter('ComputedBorderWidth', borderWidth );
-
-        if (ctx.originNode) {
-            this.compensateShapeNode(ctx);
-        }
-        this.setExtentHTML(ctx, this.getExtent());
-        
-    },
     
 
 

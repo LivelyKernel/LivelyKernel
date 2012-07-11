afc8aa328fa5d8bbd96102b070cd2964b420aed4
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-12 00:06:43 +0200

    [mirror commit] [ci skip] {   "rev": "174650",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -225,7 +225,7 @@ lively.morphic.SAPUI5.LabelComponent.subclass('lively.morphic.SAPUI5.OldControls
 }
 );
 
-lively.morphic.SAPUI5.Component.subclass('lively.morphic.SAPUI5.TextField',
+lively.morphic.SAPUI5.Component.subclass('lively.morphic.SAPUI5.OldControls.TextField',
 
 'settings',{
     baseClasses: 'sapUiTf sapUiTfBrd',

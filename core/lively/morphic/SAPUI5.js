7adf171db72a0a3172ba682828c0bdb7c4090501
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-13 01:03:32 +0200

    [mirror commit] [ci skip] {   "rev": "174788",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -427,7 +427,7 @@ lively.morphic.SAPUI5.Control.subclass('lively.morphic.SAPUI5.MatrixLayout',
         for (var r = 0; r < this.rows; r++) {
             
             var row = new lively.morphic.SAPUI5.TableRow('tr');
-            row.addWithLayer(lively.morphic.HTMLInnerTablePositionFix);
+            row.addWithLayer(lively.morphic.HTMLTableRowPositionFix);
             this.tbodyMorph.addMorph(row);
             
             for (var c = 0; c < this.cols; c++) {

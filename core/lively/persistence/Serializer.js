af588a9045d3f3c01ee6fafcf37d974bb3f6d803
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-07-12 02:06:08 +0200

    [mirror commit] [ci skip] {   "rev": "174709",   "author": "cschuster",   "changes": "U   core/lively/persistence/Serializer.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/persistence/Serializer.js\n" }

--- core/lively/persistence/Serializer.js
+++ core/lively/persistence/Serializer.js
@@ -122,30 +122,24 @@ Object.subclass('ObjectGraphLinearizer',
     },
 },
 'object registry -- serialization', {
-    register: function(obj, optAdditionalPath) {
-        if (optAdditionalPath) this.path.push(optAdditionalPath);
-        try {
-            if (this.isValueObject(obj))
-                return obj;
-    
-            if (Object.isArray(obj)) {
-                var result = [];
-                for (var i = 0; i < obj.length; i++) {
-                    this.path.push(i); // for debugging
-                    var item = obj[i];
+    register: function(obj) {
+        if (this.isValueObject(obj))
+            return obj;
 
+        if (Object.isArray(obj)) {
+            var result = [];
+            for (var i = 0; i < obj.length; i++) {
+                this.path.push(i); // for debugging
+                var item = obj[i];
                     if (this.somePlugin('ignoreProp', [obj, i, item])) continue;
-                    result.push(this.register(item));
-                    this.path.pop();
-                }
-                return result;
+                result.push(this.register(item));
+                this.path.pop();
             }
-
-            var id = this.addIdAndAddToRegistryIfNecessary(obj);
-            return this.registry[id].ref;
-        } finally {
-            if (optAdditionalPath) this.path.pop();
+            return result;
         }
+        
+        var id = this.addIdAndAddToRegistryIfNecessary(obj);
+        return this.registry[id].ref;
     },
     addIdAndAddToRegistryIfNecessary: function(obj) {
         var id = this.getIdFromObject(obj);

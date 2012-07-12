61bc57cfa5f93874586aed05a2db46ab74081f09
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-12 02:27:31 +0200

    [mirror commit] [ci skip] {   "rev": "174720",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTML.js\n" }

--- core/lively/morphic/HTML.js
+++ core/lively/morphic/HTML.js
@@ -1322,10 +1322,10 @@ lively.morphic.Shapes.Shape.addMethods(
 
 	
 		updateComputedStyles: function() {
-		    if (!this.isRelative) {
+		    //if (!this.isRelative) {
 			this.shape.updateComputedStyles();
 			this.submorphs.each(function(m){m.updateComputedStyles();});
-    		  }	
+    		  //}	
 		},
 	}
 );

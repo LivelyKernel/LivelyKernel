17dc564b6f4045d8bcec86c4e36ba826c4397b1c
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-17 23:19:01 +0200

    [mirror commit] [ci skip] {   "rev": "175188",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -101,8 +101,8 @@ lively.morphic.SAPUI5.Control.subclass('lively.morphic.SAPUI5.Button',
 },
 
 'initializing', {
-    initialize: function($super, bounds, optLabel) {
-        $super(bounds, this.tagName);
+    initialize: function($super, optBounds, optLabel) {
+        $super(this.tagName, optBounds);
         if (optLabel) this.setLabel(optLabel);
         this.value = false;
         this.toggle = false;

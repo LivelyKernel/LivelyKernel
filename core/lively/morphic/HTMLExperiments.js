23e5466494eb278562afb5d5342e29abeef446ab
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-17 02:01:47 +0200

    [mirror commit] [ci skip] {   "rev": "175063",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTMLExperiments.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTMLExperiments.js\n" }

--- core/lively/morphic/HTMLExperiments.js
+++ core/lively/morphic/HTMLExperiments.js
@@ -317,7 +317,7 @@ lively.morphic.Shapes.Shape.subclass('lively.morphic.Shapes.HTMLShape',
             newExtent = newExtent.subPt(delta);
         }
         else if (n == "textarea") {
-            var inner = pt($(ctx.shapeNode).innerWidth(), $(ctx.shapeNode).innerHeight());
+            var outer= pt($(ctx.shapeNode).innerWidth(), $(ctx.shapeNode).innerHeight());
             var inner = pt($(ctx.shapeNode).width(), $(ctx.shapeNode).height());
             var delta = outer.subPt(inner);
             newExtent = newExtent.subPt(delta);

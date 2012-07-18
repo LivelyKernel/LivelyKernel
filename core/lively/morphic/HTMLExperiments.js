c297110d1c484dbe531b73426b669fce9e614749
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-18 19:32:52 +0200

    [mirror commit] [ci skip] {   "rev": "175338",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTMLExperiments.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTMLExperiments.js\n" }

--- core/lively/morphic/HTMLExperiments.js
+++ core/lively/morphic/HTMLExperiments.js
@@ -123,6 +123,7 @@ lively.morphic.Morph.subclass('lively.morphic.HTMLMorph',
 'accessing',{
     setExtent: function(value) {
       this.shape.setExtent(value);
+      debugger;
       if (this.layout && (this.layout.adjustForNewBounds || this.layout.layouter))
             this.adjustForNewBounds();
       if (this.owner && (typeof this.owner['submorphResized'] == 'function')) {

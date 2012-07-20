5d587365f49335c3828a7a9adeb5a26b7d8f2cef
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-07-20 19:39:49 +0200

    [mirror commit] [ci skip] {   "rev": "175571",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -142,12 +142,12 @@ lively.morphic.SAPUI5.Control.subclass('lively.morphic.SAPUI5.TextField',
 },
 
 'rendering', {
-    /*
+    
     initHTML: function($super, ctx) {
         $super(ctx);
-        if (this.shape) this.setValueHTML(ctx, (this.value || this.defaultValue));
+        if (this.value) this.setValue(this.value || this.defaultValue);
     },
-*/
+
     getValueHTML: function(ctx) {
         if (ctx.shapeNode) return ctx.shapeNode.value;
         else return "";  

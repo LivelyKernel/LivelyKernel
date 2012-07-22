5929ca150f09987506f92eb459ca8bc5584e7b14
Robert Krahn <robert.krahn@gmail.com>
2012-07-22 00:14:16 -0700

    faster syntax highlighting that just styles textchunks that need to be styled

--- core/lively/ide/SyntaxHighlighting.js
+++ core/lively/ide/SyntaxHighlighting.js
@@ -9,59 +9,59 @@ Object.extend(SyntaxHighlighter, {
         // based on http://code.google.com/p/jquery-chili-js/ regex and colors
         num: {
             match: /\b[+-]?(?:\d*\.?\d+|\d+\.?\d*)(?:[eE][+-]?\d+)?\b/g,
-            style: {color: Color.web.blue}
+            style: {color: Color.web.blue, backgroundColor: null}
         },
         reg_exp: {
             match: /\/[^\/\\\n]*(?:\\.[^\/\\\n]*)*\/[gim]*/g,
-            style: {color: Color.web.maroon}
+            style: {color: Color.web.maroon, backgroundColor: null}
         },
         brace: {
             match: /[\{\}]/g,
-            style: {color: Color.web.green}
+            style: {color: Color.web.green, backgroundColor: null}
         },
         statement: {
             match: /\b(with|while|var|try|throw|switch|return|if|for|finally|else|do|default|continue|const|catch|case|break)\b/g,
-            style: {color: Color.web.navy}
+            style: {color: Color.web.navy, backgroundColor: null}
         },
         object: {
             match: /\b(String|RegExp|Object|Number|Math|Function|Date|Boolean|Array)\b/g,
-            style: {color: Color.web.deeppink}
+            style: {color: Color.web.deeppink, backgroundColor: null}
         },
         superclassOrLayer: {
             match: /([A-Za-z.]+)(?=\.(subclass|refineClass|addMethods|extend))/g,
-            style: {color: Color.web.navy}
+            style: {color: Color.web.navy, backgroundColor: null}
         },
         methodName: {
             match: /([A-Za-z0-9_$]+:)/g,   // (?= function)
-            style: {color: Color.web.darkred}
+            style: {color: Color.web.darkred, backgroundColor: null}
         },
         lively: {
             match: /\b(subclass|refineClass|addMethods|extend)\b/g,
-            style: {color: Color.web.gray}
+            style: {color: Color.web.gray, backgroundColor: null}
         },
         error: {
             match: /\b(URIError|TypeError|SyntaxError|ReferenceError|RangeError|EvalError|Error)\b/g,
-            style: {color: Color.web.coral}
+            style: {color: Color.web.coral, backgroundColor: null}
         },
         property: {
             match: /\b(undefined|arguments|NaN|Infinity)\b/g,
-            style: {color: Color.web.purple}
+            style: {color: Color.web.purple, backgroundColor: null}
         },
         operator: {
             match: /\b(void|typeof|this|new|instanceof|in|function|delete)\b/g,
-            style: {color: Color.web.darkblue}
+            style: {color: Color.web.darkblue, backgroundColor: null}
         },
         string: {
             match: /(?:\'[^\'\\\n]*(?:\\.[^\'\\\n]*)*\')|(?:\"[^\"\\\n]*(?:\\.[^\"\\\n]*)*\")/g,
-            style: {color: Color.web.teal}
+            style: {color: Color.web.teal, backgroundColor: null}
         },
         ml_comment: {
             match: /\/\*[^*]*\*+(?:[^\/][^*]*\*+)*\//g,
-            style: {color: Color.web.gray}
+            style: {color: Color.web.gray, backgroundColor: null}
         },
         sl_comment: {
               match: /\/\/.*/g,
-              style: {color: Color.web.green}
+              style: {color: Color.web.green, backgroundColor: null}
         },
         trailingWhitespace: {
             match: /[ \t]+[\n\r]/g,
@@ -120,6 +120,10 @@ lively.morphic.Text.addMethods(
             self.highlightSyntaxFromTo(0, self.textString.length,
                                        SyntaxHighlighter.JavaScriptRules);
             self.lastSyntaxHighlightTime = Date.now() - start;
+
+            if (!self.lastSyntaxHighlightTimes) self.lastSyntaxHighlightTimes = new Array(10);
+            self.lastSyntaxHighlightTimes.pop();
+            self.lastSyntaxHighlightTimes.unshift(self.lastSyntaxHighlightTime);
         };
         if (this.lastSyntaxHighlightTime >= 0) {
             var time = this.lastSyntaxHighlightTime * 2;
@@ -132,19 +136,75 @@ lively.morphic.Text.addMethods(
         return true;
     },
 
-    applyHighlighterRules: function(target, highlighterRules) {
-        for (var ruleName in highlighterRules) {
-            if (!highlighterRules.hasOwnProperty(ruleName)) continue;
-            var rule = highlighterRules[ruleName];
-            target.emphasizeRegex(rule.match, rule.style)
+    howToStyleString: function(string, rules, defaultStyle) {
+        var slices = [];
+        for (var ruleName in rules) {
+            if (!rules.hasOwnProperty(ruleName)) continue;
+            var rule = rules[ruleName],
+                m, re = rule.match, counter = 0;
+            while ((m = re.exec(string))) {
+                if (++counter > 9999) throw new Error('endless loop?');
+                var from = m.index, to = m.index + m[0].length;
+                slices.push([from, to, rule.style]);
+            }
         }
+        return Interval.intervalsInRangeDo(0, string.length, slices, function(slice, isNew) {
+            if (isNew) { slice[2] = defaultStyle }; return slice;
+        });
+    },
+
+    applyHighlighterRules: function(target, highlighterRules) {
+        // take highlighter rules and apply them to my textString. As a result
+        // of that we get text ranges and styles that should be applied to them.
+        // This are then used to style my textChunks. textChunks are reused if
+        // they have the correct ranges already, otherwise they are newly
+        // created. Return true if the DOM tree has changed through applying the
+        // highlight rules, false otherwise
+        //
+        // Unoptimized version:
+        // var defaultStyle = {color: Color.black, backgroundColor: null};
+        // this.emphasizeAll(defaultStyle)
+        // for (var ruleName in highlighterRules) {
+        //     if (!highlighterRules.hasOwnProperty(ruleName)) continue;
+        //     var rule = highlighterRules[ruleName];
+        //     target.emphasizeRegex(rule.match, rule.style)
+        // }
+
+        var defaultStyle = {color: Color.black, backgroundColor: null},
+            chunks = this.getTextChunks(),
+            rulesForString = this.howToStyleString(
+                target.textString, highlighterRules, defaultStyle);
+
+        // 1. find text chunks that can be reused
+        var ranges = target.getChunkRanges(),
+            indexesForExistingChunks = Interval.mapToMatchingIndexes(ranges, rulesForString),
+            leftOverRules = [];
+        indexesForExistingChunks.forEach(function(chunkIndexes, indexOfRule) {
+            var rule = rulesForString[indexOfRule]
+            if (chunkIndexes.length === 0) { leftOverRules.push(rule); return; }
+            false && chunkIndexes.forEach(function(chunkIndex) {
+                chunks[chunkIndex].styleText(rulesForString[2]); })
+        });
+
+        // 2. if any highlighting rules could not be applied before because
+        // textChunks available haven't the correct ranges, then slice new
+        // textChunks here
+        var leftOversExist = leftOverRules.length > 0;
+        if (leftOversExist) {
+            leftOverRules.forEach(function(rule) {
+                var chunksToStyle = target.sliceTextChunks(rule[0], rule[1], ranges);
+                chunksToStyle.forEach(function(ea) { ea.styleText(rule[2]) });
+            });
+            // this.coalesceChunks();
+        }
+        return leftOversExist;
     },
 
     highlightSyntaxFromTo: function(from, to, highlighterRules) {
         var selRange = this.getSelectionRange(),
-            scroll = this.getScroll();
-        // this.emphasize({color: Color.black}, from, to);
-        this.applyHighlighterRules(this, highlighterRules);
+            scroll = this.getScroll(),
+            domTreeChanged = this.applyHighlighterRules(this, highlighterRules);
+        console.log(selRange);
         selRange && this.setSelectionRange(selRange[0], selRange[1]);
         scroll && this.setScroll(scroll[0], scroll[1]);
     },

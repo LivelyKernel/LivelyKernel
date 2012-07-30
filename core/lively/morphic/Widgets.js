363c54c413b2a5972ef91175fb63ad758c3022da
timfelgentreff <lively-kernel@hpi.uni-potsdam.de>
2012-07-30 13:33:06 +0200

    [mirror commit] [ci skip] {   "rev": "176381",   "author": "timfelgentreff",   "changes": "U   core/lively/morphic/Widgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Widgets.js\n" }

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -3476,6 +3476,7 @@ lively.morphic.Box.subclass('lively.morphic.Tree',
     },
     createNodeAfter: function(item, optOtherNode) {
         var node = new lively.morphic.Tree(item, this);
+        node.childrenPerPage = this.childrenPerPage
         if (optOtherNode) {
             node.setPosition(optOtherNode.getPosition().addXY(0,1));
         }

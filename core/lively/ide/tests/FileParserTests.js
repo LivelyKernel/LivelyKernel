ad198af86f67436cf2d1d9c5a731ccdc486a19a4
Robert Krahn <robert.krahn@gmail.com>
2012-08-01 16:56:10 -0700

    from ww: test for AdvancedSyntaxHighlighting is epxpected to throw error (only when enabled, requires a fix)

--- core/lively/ide/tests/FileParserTests.js
+++ core/lively/ide/tests/FileParserTests.js
@@ -1109,11 +1109,18 @@ lively.ide.tests.FileParserTests.JsParserTest.subclass('lively.ide.tests.FilePar
     },
 
     testSourceWithErrorsWillNotBeSaved: function() {
-        var fragment = this.fragmentNamed('ClassA');
-        var newName = 'ClassAEdited';
-        fragment.putSourceCode('Object.subclass(\'' + newName + '\', \{\n');
-
-        this.assert(!this.db.getCachedText('foo.js').include('ClassAEdited'))
+        var fragment = this.fragmentNamed('ClassA'),
+            newName = 'ClassAEdited',
+            // FIXME test behavior depending on additionally loaded layer / system Config
+            shouldRaiseError = Global.AdvancedSyntaxHighlighting && AdvancedSyntaxHighlighting.isGlobal();
+        if (shouldRaiseError) {
+        this.assertRaises(function() {
+            fragment.putSourceCode('Object.subclass(\'' + newName + '\', \{\n');
+        });
+        } else {
+            fragment.putSourceCode('Object.subclass(\'' + newName + '\', \{\n');
+        }
+        this.assert(!this.db.getCachedText('foo.js').include('ClassAEdited'));
     },
 
     testReparse: function() {

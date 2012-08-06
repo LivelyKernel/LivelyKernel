c367f0819472d20b0099c36eb08f7ffcf4af55d6
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-07 01:25:22 +0200

    [mirror commit] [ci skip] {   "rev": "176996",   "author": "bgnauk",   "changes": "U   core/lively/morphic/SAPUI5.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/SAPUI5.js\n" }

--- core/lively/morphic/SAPUI5.js
+++ core/lively/morphic/SAPUI5.js
@@ -327,7 +327,7 @@ lively.morphic.SAPUI5.Control.subclass('lively.morphic.SAPUI5.Label',
     setFor: function(morph) {
         if (morph && morph.getNodeId){
             var id = (morph.targetNodeId && morph.targetNodeId()) || morph.getNodeId();
-            this.htmlFor = id;
+            this.htmlFor = morph;
             return this.setAttribute('for', id);
         }
     },

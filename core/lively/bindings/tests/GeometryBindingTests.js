e949d168c3bf7e88a1bce014fec5676e3f3ca45a
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-08-07 19:54:49 +0200

    [mirror commit] [ci skip] {   "rev": "177032",   "author": "robertkrahn",   "changes": "U   core/lively/bindings/tests/GeometryBindingTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/bindings/tests/GeometryBindingTests.js\n" }

--- core/lively/bindings/tests/GeometryBindingTests.js
+++ core/lively/bindings/tests/GeometryBindingTests.js
@@ -118,7 +118,7 @@ lively.morphic.tests.MorphTests.subclass('lively.bindings.tests.GeometryBindingT
         morph.addMorph(morph2);
         var observer = {extentChanged: function(ext) { this.extent = ext }};
         lively.bindings.connect(morph2, 'extent', observer, 'extentChanged');
-        morph.setExtent(pt(50,50));
+        morph2.setExtent(pt(50,50));
         this.assertEquals(pt(50,50), observer.extent, '1');
         morph.addMorph(morph2); // adding morph to same owner
         morph.setExtent(pt(40,40));

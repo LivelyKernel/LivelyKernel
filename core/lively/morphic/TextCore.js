b349f27ca3da8efd6b11c86bdc988382c695360b
robertkrahn <lively-kernel@hpi.uni-potsdam.de>
2012-08-09 01:16:34 +0200

    [mirror commit] [ci skip] {   "rev": "177342",   "author": "robertkrahn",   "changes": "U   core/lively/morphic/TextCore.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/TextCore.js\n" }

--- core/lively/morphic/TextCore.js
+++ core/lively/morphic/TextCore.js
@@ -265,7 +265,7 @@ lively.morphic.Morph.subclass('lively.morphic.Text', Trait('ScrollableTrait'), T
         allowInput: true,
         clipMode: 'visible',
         fontFamily: 'Helvetica',
-        textColor: Color.rgb(32,32,32),
+        textColor: Config.get('textColor'),
         fontSize: 10,
         padding: Rectangle.inset(4, 2)
     },

4b07ce79835fcec61c6da12a2c73b652f1cfa2ba
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-09 23:37:43 +0200

    [mirror commit] [ci skip] {   "rev": "177472",   "author": "bgnauk",   "changes": "U   core/lively/morphic/Widgets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Widgets.js\n" }

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -2581,8 +2581,8 @@ cop.create('lively.morphic.ModalLayer').refineClass(lively.morphic.Morph, {
     createBackpanel: function(optColor) {
         var backPanel,
             backPanelExtent = this.owner.getExtent();
-        backPanel = new lively.morphic.Box();
-            
+        backPanel = new lively.morphic.Box(backPanelExtent.extentAsRectangle());
+        return backPanel;
     },
     removeBackPanel: function() {
         this.backPanel && this.backPanel.remove && this.backPanel.remove();

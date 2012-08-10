0e47fe8a4972eaf47647d379ef5ccb981a679e18
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-10 19:39:14 +0200

    [mirror commit] [ci skip] {   "rev": "177705",   "author": "bgnauk",   "changes": "U   core/lively/morphic/Graphics.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Graphics.js\n" }

--- core/lively/morphic/Graphics.js
+++ core/lively/morphic/Graphics.js
@@ -1062,7 +1062,7 @@ Object.extend(Color, {
     rgbHex: function(colorHexString) {
         var colorData = this.parseHex(colorHexString);
         if (colorData && colorData[0] && colorData[1] && colorData[2]) {
-            return new Color(colorData [0],colorData [1],colorData [2]);
+            return new Color(colorData[0], colorData[1], colorData[2]);
         } else {
             return null;    
         }

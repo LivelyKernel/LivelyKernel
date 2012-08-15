e000856b50201eb0329bb590881832ee70aa4c07
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-08-15 03:45:11 +0200

    [mirror commit] [ci skip] {   "rev": "178265",   "author": "cschuster",   "changes": "U   core/lively/bindings/tests/BindingTests.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/bindings/tests/BindingTests.js\n" }

--- core/lively/bindings/tests/BindingTests.js
+++ core/lively/bindings/tests/BindingTests.js
@@ -447,6 +447,7 @@ TestCase.subclass('lively.bindings.tests.BindingTests.ConnectionTest', {
     },
     
     test39ConnectingNonexistentPropertyDoesNotAssignNewProperty: function() {
+        debugger;
         var obj = {m: function() {}};
         connect(obj, 'x', obj, 'm');
         this.assert(!obj.hasOwnProperty('x'), 'connecting assigns non-existent property value');

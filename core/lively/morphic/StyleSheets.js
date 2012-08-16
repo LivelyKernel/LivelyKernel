1f5c9def8103653f635013d0c184dc0148b078be
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-16 22:53:01 +0200

    [mirror commit] [ci skip] {   "rev": "178547",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheets.js\n" }

--- core/lively/morphic/StyleSheets.js
+++ core/lively/morphic/StyleSheets.js
@@ -48,17 +48,11 @@ lively.morphic.Morph.addMethods(
         return true;
     }, 
     getClassNames: function() {
-        var classNames = this.classNames,
-            uniqCaseInsensitive = function(array) {
-                return this.inject([], function(array, value, index) {
-                    if (0 == index || (sorted ? array.last() != value : !array.include(value))) array.push(value);
-                return array;
-        });
-            };
+        var classNames = this.classNames;
         
         var type = this.constructor;
         while (type != Object) {
-            a.unshift(type.name);
+            classNames .unshift(type.name);
             type = type.superclass;
         }
     }

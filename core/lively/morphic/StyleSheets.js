1121d2cfe3c9b30690765155fa3cacfebce04c6f
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-16 19:55:08 +0200

    [mirror commit] [ci skip] {   "rev": "178502",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheets.js\n" }

--- core/lively/morphic/StyleSheets.js
+++ core/lively/morphic/StyleSheets.js
@@ -62,7 +62,88 @@ Object.subclass("Selector",
 	}
 
 	// All others
-	return select( selector, context, results, seed, xml );
+	return this.select( selector, context, results, seed, xml );
+    },
+    select: function( selector, context, results, seed, xml ) {
+	// Remove excessive whitespace
+	selector = selector.replace( rtrim, "$1" );
+	var elements, matcher, i, len, elem, token,
+		type, findContext, notTokens,
+		match = selector.match( rgroups ),
+		tokens = selector.match( rtokens ),
+		contextNodeType = context.nodeType;
+
+	// POS handling
+	if ( matchExpr["POS"].test(selector) ) {
+		return handlePOS( selector, context, results, seed, match );
+	}
+
+	if ( seed ) {
+		elements = slice.call( seed, 0 );
+
+	// To maintain document order, only narrow the
+	// set if there is one group
+	} else if ( match && match.length === 1 ) {
+
+		// Take a shortcut and set the context if the root selector is an ID
+		if ( tokens.length > 1 && contextNodeType === 9 && !xml &&
+				(match = matchExpr["ID"].exec( tokens[0] )) ) {
+
+			context = Expr.find["ID"]( match[1], context, xml )[0];
+			if ( !context ) {
+				return results;
+			}
+
+			selector = selector.slice( tokens.shift().length );
+		}
+
+		findContext = ( (match = rsibling.exec( tokens[0] )) && !match.index && context.parentNode ) || context;
+
+		// Get the last token, excluding :not
+		notTokens = tokens.pop();
+		token = notTokens.split(":not")[0];
+
+		for ( i = 0, len = Expr.order.length; i < len; i++ ) {
+			type = Expr.order[i];
+
+			if ( (match = matchExpr[ type ].exec( token )) ) {
+				elements = Expr.find[ type ]( (match[1] || "").replace( rbackslash, "" ), findContext, xml );
+
+				if ( elements == null ) {
+					continue;
+				}
+
+				if ( token === notTokens ) {
+					selector = selector.slice( 0, selector.length - notTokens.length ) +
+						token.replace( matchExpr[ type ], "" );
+
+					if ( !selector ) {
+						push.apply( results, slice.call(elements, 0) );
+					}
+				}
+				break;
+			}
+		}
+	}
+
+	// Only loop over the given elements once
+	// If selector is empty, we're already done
+	if ( selector ) {
+		matcher = compile( selector, context, xml );
+		dirruns = matcher.dirruns++;
+
+		if ( elements == null ) {
+			elements = Expr.find["TAG"]( "*", (rsibling.test( selector ) && context.parentNode) || context );
+		}
+		for ( i = 0; (elem = elements[i]); i++ ) {
+			cachedruns = matcher.runs++;
+			if ( matcher(elem, context) ) {
+				results.push( elem );
+			}
+		}
+	}
+
+	return results;
 }
 },
 'helpers',{
@@ -255,4 +336,4 @@ Object.subclass("Selector",
 )
 
 })
-// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module
+// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module// end of module

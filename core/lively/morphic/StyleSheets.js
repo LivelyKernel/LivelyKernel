eb2d7dc7b32cff1640bb3f405eeedd303cede4ec
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-16 19:07:37 +0200

    [mirror commit] [ci skip] {   "rev": "178467",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheets.js\n" }

--- core/lively/morphic/StyleSheets.js
+++ core/lively/morphic/StyleSheets.js
@@ -1,5 +1,20 @@
 module('lively.morphic.StyleSheets').requires().toRun(function() {
 
-// Enter your code here
+Object.subclass("Selector",
+'documentation', {
+    documentation: "Sizzle port for morphic."
+},
+'settings', {
+    isColor: true
+},
+'initializing', {
+    initialize: function(r, g, b, a) {
+        this.r = r || 0;
+        this.g = g || 0;
+        this.b = b || 0;
+        this.a = a || (a === 0 ? 0 : 1);
+    }
+}
+)
 
 }) // end of module

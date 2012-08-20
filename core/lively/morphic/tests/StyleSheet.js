e3fd71d094aebfb9a3db11f96e4d84699e867d80
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-20 19:44:52 +0200

    [mirror commit] [ci skip] {   "rev": "178883",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheet.js\n" }

--- core/lively/morphic/tests/StyleSheet.js
+++ core/lively/morphic/tests/StyleSheet.js
@@ -1,6 +1,11 @@
 module('lively.morphic.tests.StyleSheet').requires('lively.morphic.tests.Helper').toRun(function() {
 
 lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheet.CSSForMorphs',
+'running', {
+    setUp: function($super) {
+        $super();
+    }
+},
 'testing', {
     
     test01FindCSSRulesForMorph: function() {

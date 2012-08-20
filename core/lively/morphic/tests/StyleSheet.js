2b1882069978b1ba897b8e14a8a471d3ff4a4531
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-20 19:56:48 +0200

    [mirror commit] [ci skip] {   "rev": "178885",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheet.js\n" }

--- core/lively/morphic/tests/StyleSheet.js
+++ core/lively/morphic/tests/StyleSheet.js
@@ -5,7 +5,24 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheet.CSSFor
     setUp: function($super) {
         $super();
         this.createSomeMorphs();
-    }
+    },
+    createSomeMorphs: function() {
+        yellowRectangle = lively.morphic.Morph.makeRectangle(0,0, 300, 300);
+yellowRectangle.applyStyle({fill: Color.yellow});
+yellowRectangle.openInWorld();
+
+redRectangle = lively.morphic.Morph.makeRectangle(25, 25, 250, 250);
+redRectangle.applyStyle({fill: Color.red});
+yellowRectangle.addMorph(redRectangle);
+
+blueRectangle1 = lively.morphic.Morph.makeRectangle(10, 10, 150, 100);
+blueRectangle1 .applyStyle({fill: Color.blue});
+redRectangle.addMorph(blueRectangle1);
+
+blueRectangle2 = lively.morphic.Morph.makeRectangle(10, 160, 150, 80);
+blueRectangle2 .applyStyle({fill: Color.blue});
+redRectangle.addMorph(blueRectangle2);
+    },
 },
 'testing', {
     

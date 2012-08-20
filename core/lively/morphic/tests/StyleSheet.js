4d390a5016e86433eb128e4cf8780f0276eebc15
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-21 00:17:49 +0200

    [mirror commit] [ci skip] {   "rev": "179067",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheet.js\n" }

--- core/lively/morphic/tests/StyleSheet.js
+++ core/lively/morphic/tests/StyleSheet.js
@@ -99,8 +99,34 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheet.Sizzle
         $super();
         this.sizzle = new lively.morphic.Sizzle();
     },
-    newMethod: function() {
-        // enter comment here
+    createSomeMorphs: function() {
+        // this method creates 4 morphs: yellowRectange is the ouyter parent
+        // redRectangle its embedded submorph, blueRectangle1, blueRectangle1
+        // are its submorphs
+        var yellowRectangle = lively.morphic.Morph.makeRectangle(0,0, 300, 300);
+        yellowRectangle.applyStyle({fill: Color.yellow});
+        yellowRectangle.openInWorld();
+
+        var redRectangle = lively.morphic.Morph.makeRectangle(25, 25, 250, 250);
+        redRectangle.applyStyle({fill: Color.red});
+        redRectangle.addClassName('red');
+        yellowRectangle.addMorph(redRectangle);
+
+        var blueRectangle1 = lively.morphic.Morph.makeRectangle(10, 10, 150, 100);
+        blueRectangle1.applyStyle({fill: Color.blue});
+        blueRectangle1.addClassName('blue');
+        redRectangle.addMorph(blueRectangle1);
+
+        var blueRectangle2 = lively.morphic.Morph.makeRectangle(10, 160, 150, 80);
+        blueRectangle2.applyStyle({fill: Color.blue});
+        blueRectangle2.addClassName('blue');
+        redRectangle.addMorph(blueRectangle2);
+        
+        this.yellowRectangle = yellowRectangle;
+        this.redRectangle = redRectangle;
+        this.blueRectangle1 = blueRectangle1;
+        this.blueRectangle2 = blueRectangle2;
+        
     },
 
 },

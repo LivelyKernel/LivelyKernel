ba181ad0af85f24171625e7d21e82c732f476ce6
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-21 01:22:59 +0200

    [mirror commit] [ci skip] {   "rev": "179123",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheet.js\n" }

--- core/lively/morphic/tests/StyleSheet.js
+++ core/lively/morphic/tests/StyleSheet.js
@@ -174,8 +174,8 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheet.Sizzle
         selection = this.sizzle.select('[testAttribute^="the]', this.world);
         this.assertEqualState([this.yellowRectangle, this.redRectangle], selection, 'selection for attribute "testAttribute^=the" should return yellow and red rectangles');
         
-        selection = this.sizzle.select('[testAttribute$="the]', this.world);
-        this.assertEqualState([this.yellowRectangle, this.redRectangle], selection, 'selection for attribute "testAttribute^=the" should return yellow and red rectangles');
+        selection = this.sizzle.select('[testAttribute$="Rectangle]', this.world);
+        this.assertEqualState([this.yellowRectangle, this.redRectangle], selection, 'selection for attribute "testAttribute$=Rectangle" should return yellow and red rectangles');
     }});
 TestCase.subclass('lively.morphic.tests.StyleSheet.CSSRuleInterface',
 'testing', {

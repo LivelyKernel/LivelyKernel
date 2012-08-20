3cc4dea48066872b1c8d33f61fff46f13fa144cb
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-21 01:25:24 +0200

    [mirror commit] [ci skip] {   "rev": "179130",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheet.js\n" }

--- core/lively/morphic/tests/StyleSheet.js
+++ core/lively/morphic/tests/StyleSheet.js
@@ -176,7 +176,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheet.Sizzle
         
         selection = this.sizzle.select('[testAttribute$="Rectangle"]', this.world);
         this.assertEqualState([this.yellowRectangle, this.redRectangle], selection, 'selection for attribute "testAttribute$=Rectangle" should return yellow and red rectangles');
-    }});
+    },});
 TestCase.subclass('lively.morphic.tests.StyleSheet.CSSRuleInterface',
 'testing', {
     test01RuleOfCSSClassDef: function() {

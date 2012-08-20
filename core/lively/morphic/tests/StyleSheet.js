7c513826a34046f88ff7c8e4b7e97ca618222f73
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-21 01:24:15 +0200

    [mirror commit] [ci skip] {   "rev": "179125",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheet.js\n" }

--- core/lively/morphic/tests/StyleSheet.js
+++ core/lively/morphic/tests/StyleSheet.js
@@ -168,13 +168,13 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheet.Sizzle
         var selection = this.sizzle.select('[testAttribute]', this.world);
         this.assertEqualState([this.yellowRectangle, this.redRectangle], selection, 'selection for attribute "testAttribute" should return yellow rectangle and red rectangle');
         
-        selection = this.sizzle.select('[testAttribute="theYellowRectangle]', this.world);
+        selection = this.sizzle.select('[testAttribute="theYellowRectangle"]', this.world);
         this.assertEqualState([this.yellowRectangle], selection, 'selection for attribute "testAttribute=theYellowRectangle" should return yellow rectangle');
         
-        selection = this.sizzle.select('[testAttribute^="the]', this.world);
+        selection = this.sizzle.select('[testAttribute^="the"]', this.world);
         this.assertEqualState([this.yellowRectangle, this.redRectangle], selection, 'selection for attribute "testAttribute^=the" should return yellow and red rectangles');
         
-        selection = this.sizzle.select('[testAttribute$="Rectangle]', this.world);
+        selection = this.sizzle.select('[testAttribute$="Rectangle"]', this.world);
         this.assertEqualState([this.yellowRectangle, this.redRectangle], selection, 'selection for attribute "testAttribute$=Rectangle" should return yellow and red rectangles');
     }});
 TestCase.subclass('lively.morphic.tests.StyleSheet.CSSRuleInterface',

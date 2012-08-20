53a01930d7ce625cfede09edf909304133df80e9
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-20 21:01:21 +0200

    [mirror commit] [ci skip] {   "rev": "178931",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheet.js\n" }

--- core/lively/morphic/tests/StyleSheet.js
+++ core/lively/morphic/tests/StyleSheet.js
@@ -54,7 +54,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheet.CSSFor
         var css = ".some-class { color: red }";
         this.morph.addClassName('some-class');
         this.morph.processStyleSheet(css);
-
+inspect(this.morph.styleSheetRules)
         this.assertEquals(1, this.morph.styleSheetRules.length, 'no rule assigned');
         this.assertEquals('.some-class', this.morph.styleSheetRules[0].selectorText, 'Selector of first rule is not .blue');
     },    

8d589b6236565d8df5d234d5cde034f112b2729d
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-21 02:01:04 +0200

    [mirror commit] [ci skip] {   "rev": "179147",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheet.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheet.js\n" }

--- core/lively/morphic/tests/StyleSheet.js
+++ core/lively/morphic/tests/StyleSheet.js
@@ -115,7 +115,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheet.Sizzle
     
     testDoNotSelectMorphByClass: function() {
         var selection = this.sizzle.select(':not(.blue)', this.yellowRectangle);
-        this.assertEqualState([this.yellowRectangle, this.redRectangle], selection, 
+        this.assertEqualState([this.yellowRectangle, this.redRectangle], selection,
             'selection by not-class should return yellow and red rectangles');
     },
     

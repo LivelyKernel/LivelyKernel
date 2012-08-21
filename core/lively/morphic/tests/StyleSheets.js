ae72ad7b61beb74b9c96476b055c8efcbe46dc59
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-22 01:58:15 +0200

    [mirror commit] [ci skip] {   "rev": "179424",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -396,15 +396,20 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.CSSFo
         var blue1Styles = this.blueRectangle1.getStyleSheetDeclarations(),
             blueStyles1BackgroundColorValue =
                 blue1Styles['background-color'].values.first().value;
-        this.assertEquals('blue', blueStyles1BackgroundColorValue, 
+        this.assertEquals('blue', blueStyles1BackgroundColorValue,
             'background-color of blue1 should be blue');
             
         var blue2Styles = this.blueRectangle2.getStyleSheetDeclarations(),
             blueStyles2BackgroundColorValue =
                 blue2Styles['background-color'].values.first().value;
-        this.assertEquals('yellow', blueStyles2BackgroundColorValue, 
-            'background-color of blue2 should be yellow');        
+        this.assertEquals('yellow', blueStyles2BackgroundColorValue,
+            'background-color of blue2 should be yellow');
         
+        var redStyles = this.redRectangle.getStyleSheetDeclarations(),
+            redBackgroundColorValue =
+                blue1Styles['background-color'].values.first().value;
+        this.assertEquals('blue', blueStyles1BackgroundColorValue,
+            'background-color of blue1 should be blue');
     },
 
 

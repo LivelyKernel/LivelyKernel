bc0ece3d3093d812dcfa1ddf5d024ad70e1b1747
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-22 00:36:12 +0200

    [mirror commit] [ci skip] {   "rev": "179370",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -418,9 +418,28 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.CSSFo
         this.assert(!this.morph.isRuleMoreSpecific(classOnlyRule, classOnlyRule),
             '.some-class is not more specific than .some-class');
     },
-    newMethod: function() {
-        // enter comment here
-    }
+    test06GetStyleSheetDeclarations: function() {
+        var css = '.some-class { color: red; }'+
+            '#some-id.some-class { color: blue; }';
+        this.morph.addClassName('some-class');
+        this.morph.setNewId('some-id');
+        this.world.addMorph(this.morph);
+        this.world.processStyleSheet(css);
+
+        var classOnlyRule = this.morph.styleSheetRules.filter(function(rule){
+                return (rule.selectorText() === '.some-class');
+            }).first(),
+            classAndIdRule = this.morph.styleSheetRules.filter(function(rule){
+                return (rule.selectorText() === '#some-id.some-class');
+            }).first();
+
+        this.assert(this.morph.isRuleMoreSpecific(classAndIdRule, classOnlyRule),
+            '#some-id.some-class is more specific than .some-class');
+        this.assert(!this.morph.isRuleMoreSpecific(classOnlyRule, classAndIdRule),
+            '.some-class is not more specific than #some-id.some-class');
+        this.assert(!this.morph.isRuleMoreSpecific(classOnlyRule, classOnlyRule),
+            '.some-class is not more specific than .some-class');
+    },
 
 
 

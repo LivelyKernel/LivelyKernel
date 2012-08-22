a02b23a14d3429a31591da11cc0a869dcb9a84d1
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-22 22:42:12 +0200

    [mirror commit] [ci skip] {   "rev": "179547",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -460,13 +460,13 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.CSSFo
         this.createSomeMorphs(); // sets up a hierarchy of morphs
 
         this.world.processStyleSheet(css);
-        
+
         var blue1Styles = this.blueRectangle1.getStyleSheetDeclarations(),
             blueStyles1BackgroundColorValue =
                 blue1Styles['background-color'].values.first().value;
         this.assertEquals('blue', blueStyles1BackgroundColorValue,
             'background-color of blue1 should be blue');
-            
+
         var blue2Styles = this.blueRectangle2.getStyleSheetDeclarations(),
             blueStyles2BackgroundColorValue =
                 blue2Styles['background-color'].values.first().value;

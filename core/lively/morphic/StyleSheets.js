77008cd67808e3b49347c40ce383054612101273
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-22 20:06:32 +0200

    [mirror commit] [ci skip] {   "rev": "179481",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheets.js\n" }

--- core/lively/morphic/StyleSheets.js
+++ core/lively/morphic/StyleSheets.js
@@ -107,38 +107,37 @@ lively.morphic.Morph.addMethods(
             });
     },
     getStyleSheetRuleSpecificity: function(rule) {
-         // check if it is a grouped selector
-                    if (rule.selector.indexOf(",") != -1)
-                    {
-                        var selectors = rule.selector.split(",");
-                        var maxSpecificity = -1;
-                        var sel, spec, mostSpecificSelector;
-                        
-                        // loop over all selectors in the group
-                        for (var j, len = selectors.length; j < len; j++)
-                        {
-                            sel = selectors[j];
-                            
-                            // find if the selector matches the element
-                            if (Selector.matches(sel, arr).length == 1)
-                            {
-                                spec = getCSSRuleSpecificity(sel);
-                                
-                                // find the most specific selector that macthes the element
-                                if (spec > maxSpecificity)
-                                {
-                                    maxSpecificity = spec;
-                                    mostSpecificSelector = sel;
-                                }
-                            }
-                        }
-                        
-                        rule.specificity = maxSpecificity;
-                    }
+        // check if it is a grouped selector
+        if (rule.selectorText().indexOf(",") != -1) {
+            var selectors = rule.selectorText().split(","),
+                maxSpecificity = -1,
+                sizzle = new lively.morphic.Sizzle();
+                sel, spec, mostSpecificSelector;
+
+            // loop over all selectors in the group
+            for (var j, len = selectors.length; j < len; j++)
+            {
+                sel = selectors[j];
+                
+                
+                // find if the selector matches the element
+                if (Selector.matches(sel, arr).length == 1)
+                {
+                    spec = getCSSRuleSpecificity(sel);
                     
+                    // find the most specific selector that macthes the element
+                    if (spec > maxSpecificity)
+                    {
+                        maxSpecificity = spec;
+                        mostSpecificSelector = sel;
+                    }
+                }
+            }
             
-                    return (apps.cssParser.calculateCSSRuleSpecificity(a.selectorText()) > 
-                        apps.cssParser.calculateCSSRuleSpecificity(b.selectorText()));
+            return maxSpecificity;
+        } else {
+            return apps.cssParser.calculateCSSRuleSpecificity(rule.selectorText());
+        }
     }
 
 

bf3a289a93261b86e6b9519a5a2507e914f1c86c
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-22 21:15:07 +0200

    [mirror commit] [ci skip] {   "rev": "179535",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -392,17 +392,9 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.CSSFo
             worldCSS = "#the-red-rectangle { color: black; }";;
             
 
-        this.yellowRectangle.processStyleSheet(css);
+        this.yellowRectangle.processStyleSheet(firstCSS);
         
-        this.assert(this.blueRectangle1.styleSheetRules, 'Blue Rectangle 1 has no rule attribute');
-        var b1css = this.blueRectangle1.styleSheetRules;
-        this.assertEquals(1, b1css.length, 'Blue Rectangle 1 has not exactly 1 rule');
-        this.assertEquals('.blue:nth-child(1)', b1css [0].selectorText(), 'Selector of first rule in blueRectangle2 is not .blue:nth-child(1)');
-        
-        this.assert(this.blueRectangle2.styleSheetRules, 'Blue Rectangle 2 has no rule attribute');
-        var b2css = this.blueRectangle2.styleSheetRules;
-        this.assertEquals(1, b2css.length, 'Blue Rectangle 2 has not exactly 1 rule');
-        this.assertEquals('.blue:nth-child(2)', b2css[0].selectorText(), 'Selector of first rule in blueRectangle2 is not .blue:nth-child(2)');
+
 
         this.assert(this.redRectangle.styleSheetRules, 'Red Rectangle has no rule attribute');
         var rcss = this.redRectangle.styleSheetRules;

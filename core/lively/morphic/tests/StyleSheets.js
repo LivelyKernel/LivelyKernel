10cdcbdc0d7251f075917c0b9cb75927450f6306
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-22 20:49:58 +0200

    [mirror commit] [ci skip] {   "rev": "179526",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -385,7 +385,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.CSSFo
         this.assertEquals('.red', rcss[0].selectorText(), 'Selector of first rule in RedRectangle is not .red');
         
     },
-    test04GetSortedRules: function() {
+    test05GetSortedRules: function() {
 
         var worldCss = '.red { color: red;}'+ //1
                 '#the-red-rectangle.red {color: blue;}' + //3

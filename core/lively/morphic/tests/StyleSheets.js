10cbf2ec9705f8ee5b84fbae0621f1856d8edb68
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-22 20:45:18 +0200

    [mirror commit] [ci skip] {   "rev": "179519",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -199,12 +199,10 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.Sizzl
         */
     },
     testSelectMorphByClassName: function() {
-        
         this.assertSizzleSelect([this.blueRectangle1, this.blueRectangle2],
             '.blue',
             this.world,
             'selection by class should include both blue rectangle morphs');
-
     },
     testSelectMorphByClassWithSeed: function() {
         var selection = this.sizzle.select('.blue', this.world, null, [this.blueRectangle1]),

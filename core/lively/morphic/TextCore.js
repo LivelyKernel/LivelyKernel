40c112ae70025268455c88ea472fb3601c9d589a
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-08-23 03:50:57 +0200

    [mirror commit] [ci skip] {   "rev": "179640",   "author": "cschuster",   "changes": "U   core/lively/morphic/TextCore.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/TextCore.js\n" }

--- core/lively/morphic/TextCore.js
+++ core/lively/morphic/TextCore.js
@@ -2086,6 +2086,7 @@ lively.morphic.Morph.subclass('lively.morphic.Text', Trait('ScrollableTrait'), T
     },
 
     appendRichText: function(string, style) {
+        this.textChunks.last().removeNonChunkNodes();
         var newChunk = this.createChunk();
         this.textChunks.push(newChunk);
         newChunk.textString = string;

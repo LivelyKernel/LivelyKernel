e27ad989a8efcd22c63a80bdee4d528ade6b2438
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-25 00:31:33 +0200

    [mirror commit] [ci skip] {   "rev": "179907",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheetsHTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheetsHTML.js\n" }

--- core/lively/morphic/StyleSheetsHTML.js
+++ core/lively/morphic/StyleSheetsHTML.js
@@ -8,6 +8,49 @@ module('lively.morphic.StyleSheetsHTML').requires('lively.morphic.HTML').toRun(f
             setStyleSheet: function(value) {
                 this.shape.setStyleSheet(value);
             },
+        },
+        'HTML DOM', {
+            setStyleSheetHTML: function(ctx, styleSheet) {
+                var morphId = ctx.shapeNode && ctx.shapeNode.id;
+                if (!morphId) {
+                    //console.log('Warning, morph has no shape node or shape node was not given any id.');
+                    return;
+                }
+
+                var styleTagId = "style-for-"+morphId;
+
+        	if (ctx.styleNode) {
+        	   $(ctx.styleNode).remove();
+                }
+                if (value && value.length > 1) {
+
+            	    //console.log("Setting CSS for shape "+morphId+" to "+value);
+                    var specificCss = "#"+morphId+" { "+value+" }";
+
+                    // syntax fixes for the sap gold reflection css
+                    specificCss = specificCss.replace(/[\s]*=[\s]*/g,"=");
+                    specificCss = specificCss.replace(/alpha[\s]*\([\s]*opacity[\s]*\:/g,"alpha(opacity=");
+                    specificCss = specificCss.replace(".dev-datepicker/jQuery",".dev-datepicker.jQuery");
+
+
+                    if (less) {
+                        new less.Parser().parse(specificCss, function(e, tree) {
+                            //console.log(e);
+                            specificCss = tree.toCSS();
+                        });
+                        console.log(specificCss);
+                    }
+
+
+        	        ctx.styleNode = $('<style type="text/css" id="' + styleTagId + '"></style>');
+        	        ctx.styleNode.text(specificCss);
+        	        ctx.styleNode.appendTo(document.head);
+
+                } else {
+                    delete this._StyleSheet;
+                }
+                
+            }
         }
     )
 }) // end of module

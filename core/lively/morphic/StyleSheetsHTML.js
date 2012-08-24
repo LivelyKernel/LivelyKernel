ce4bbde453c4dc7f00e7be981b79cf3f288c874e
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-25 00:53:08 +0200

    [mirror commit] [ci skip] {   "rev": "179917",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheetsHTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheetsHTML.js\n" }

--- core/lively/morphic/StyleSheetsHTML.js
+++ core/lively/morphic/StyleSheetsHTML.js
@@ -14,7 +14,9 @@ module('lively.morphic.StyleSheetsHTML').requires('lively.morphic.HTML').toRun(f
                     var m = this;
                     while ((m = m.owner)) {
                         ancestorPrefix = '#'+m.getDomId()+
-                            ((ancestorPrefix.length > 0)?(' '+ancestorPrefix):'');
+                            ((ancestorPrefix.length > 0)?
+                            (' '+ancestorPrefix):
+                            '');
                     }
                 }
                 return

7f05cc662e10a093e5032b8f8d8d13dcc4e6b958
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-24 19:51:04 +0200

    [mirror commit] [ci skip] {   "rev": "179758",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -108,9 +108,112 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.Morph
 
     
 });
-Object.subclass('MyClass',
-'default category', {
-    m1: function() {},
+lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.MorphClassNames',
+'running', {
+    setUp: function($super) {
+        $super();
+        this.createSomeMorphs();
+    },
+    createSomeMorphs: function() {
+        // this method creates 4 morphs: yellowRectange is the ouyter parent
+        // redRectangle its embedded submorph, blueRectangle1, blueRectangle1
+        // are its submorphs
+        var yellowRectangle = lively.morphic.Morph.makeRectangle(0,0, 300, 300);
+        yellowRectangle.applyStyle({fill: Color.yellow});
+        yellowRectangle.tagName = 'YellowRectangle';
+        yellowRectangle.testAttribute = 'theYellowRectangle';
+        yellowRectangle.openInWorld();
+
+
+        var redRectangle = lively.morphic.Morph.makeRectangle(25, 25, 250, 250);
+        redRectangle.applyStyle({fill: Color.red});
+        redRectangle.addClassName('red');
+        redRectangle.setNewId('the-red-rectangle');
+        redRectangle.testAttribute = 'theRedRectangle';
+        yellowRectangle.addMorph(redRectangle);
+
+        var blueRectangle1 = lively.morphic.Morph.makeRectangle(10, 10, 150, 100);
+        blueRectangle1.applyStyle({fill: Color.blue});
+        blueRectangle1.addClassName('blue');
+        blueRectangle1.setNewId('b1');
+        redRectangle.addMorph(blueRectangle1);
+
+        var blueRectangle2 = lively.morphic.Morph.makeRectangle(10, 160, 150, 80);
+        blueRectangle2.applyStyle({fill: Color.blue});
+        blueRectangle2.addClassName('blue');
+        blueRectangle2.setNewId('b2');
+        blueRectangle2.tagName = 'blueRectangleTag';
+        redRectangle.addMorph(blueRectangle2);
+        
+        this.yellowRectangle = yellowRectangle;
+        this.redRectangle = redRectangle;
+        this.blueRectangle1 = blueRectangle1;
+        this.blueRectangle2 = blueRectangle2;
+        
+    },
+
+},
+'testing', {
+
+    
+    
+    testMorphSelectsItself: function() {
+
+        this.assertEqualState([this.redRectangle], 
+            this.redRectangle.getSubmorphsByClassName('red'),
+            'selection by class only should include Red Rectangle'
+        );
+        
+        this.assertEqualState(this.redRectangle, 
+            this.redRectangle.getSubmorphById('the-red-rectangle'),
+            'selection by id only should include Red Rectangle'
+        );
+
+
+    },
+
+
+    
+    
+    testSelectMorphById: function() {
+        this.assertEqualState(this.redRectangle, this.world.getSubmorphById('the-red-rectangle'),
+            'selection by id should only include red rectangle morph');
+
+    },
+    testSelectMorphByClassName: function() {
+
+        this.assertEqualState([this.blueRectangle1, this.blueRectangle2], 
+            this.world.getSubmorphsByClassName('blue'),
+            'selection by class should include both blue rectangle morphs');
+
+    },
+    testSelectMorphByTagName: function() {
+
+        this.assertEqualState([this.blueRectangle2], 
+            this.world.getSubmorphsByTagName('blueRectangleTag'),
+            'selection by tag should include the 2nd blue rectangle morph only');
+
+    },
+
+
+    testSelectMorphByAttributes: function() {
+        
+        this.assertEqualState([this.yellowRectangle, this.redRectangle], 
+            this.world.getSubmorphsByAttribute('testAttribute'),
+            'selection by attribute should include the yellow and the red morph');
+        
+        this.assertEqualState([this.yellowRectangle], 
+            this.world.getSubmorphsByAttribute('testAttribute', 'theYellowRectangle'),
+            'selection by attribute should include the yellow morph');
+        
+        this.assertEqualState([this.yellowRectangle], 
+            this.world.getSubmorphsByAttribute('testAttribute', 'tHeYellOwRectAnglE', true),
+            'selection by attribute should include the yellow morph (case insensitive)');
+
+    },
+    
+
+    
 });
 
 lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.SizzleMorphicSelection',

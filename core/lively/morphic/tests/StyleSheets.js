975c925122fb5953b444e26a2069b42b38c5ed97
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-24 20:44:59 +0200

    [mirror commit] [ci skip] {   "rev": "179832",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -216,7 +216,8 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.Morph
         this.assert(!this.morph.isOfClass('test'),
             'Morph should NOT be of class "test"');
         this.assert(this.morph.isOfClass('crazymorph'),
-            'Morph should be of class "test"');
+            'Morph should be of class "crazymorph"');
+        this.morph.removeClassName('morph');
     }
 
 

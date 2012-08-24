0eae115e11a85443d7678e304e1bc2ffd775ff04
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-24 20:21:49 +0200

    [mirror commit] [ci skip] {   "rev": "179788",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -134,6 +134,10 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.Morph
         
         this.morph.addClassName('test');
         
+        this.assertEquals(1, this.morph.classNames.length,
+            'Class names array in morph is not 1');
+        this.assertEquals('test', this.morph.classNames.first(),
+            'Class names array should contain "test"');
     }
 
     

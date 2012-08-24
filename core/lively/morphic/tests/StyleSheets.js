be0099405955c62a1b54832ea5357db7838106b1
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-24 19:55:04 +0200

    [mirror commit] [ci skip] {   "rev": "179760",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -125,62 +125,6 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.Morph
         
     },
     
-    testMorphSelectsItself: function() {
-
-        this.assertEqualState([this.redRectangle], 
-            this.redRectangle.getSubmorphsByClassName('red'),
-            'selection by class only should include Red Rectangle'
-        );
-        
-        this.assertEqualState(this.redRectangle, 
-            this.redRectangle.getSubmorphById('the-red-rectangle'),
-            'selection by id only should include Red Rectangle'
-        );
-
-
-    },
-
-
-    
-    
-    testSelectMorphById: function() {
-        this.assertEqualState(this.redRectangle, this.world.getSubmorphById('the-red-rectangle'),
-            'selection by id should only include red rectangle morph');
-
-    },
-    testSelectMorphByClassName: function() {
-
-        this.assertEqualState([this.blueRectangle1, this.blueRectangle2], 
-            this.world.getSubmorphsByClassName('blue'),
-            'selection by class should include both blue rectangle morphs');
-
-    },
-    testSelectMorphByTagName: function() {
-
-        this.assertEqualState([this.blueRectangle2], 
-            this.world.getSubmorphsByTagName('blueRectangleTag'),
-            'selection by tag should include the 2nd blue rectangle morph only');
-
-    },
-
-
-    testSelectMorphByAttributes: function() {
-        
-        this.assertEqualState([this.yellowRectangle, this.redRectangle], 
-            this.world.getSubmorphsByAttribute('testAttribute'),
-            'selection by attribute should include the yellow and the red morph');
-        
-        this.assertEqualState([this.yellowRectangle], 
-            this.world.getSubmorphsByAttribute('testAttribute', 'theYellowRectangle'),
-            'selection by attribute should include the yellow morph');
-        
-        this.assertEqualState([this.yellowRectangle], 
-            this.world.getSubmorphsByAttribute('testAttribute', 'tHeYellOwRectAnglE', true),
-            'selection by attribute should include the yellow morph (case insensitive)');
-
-    },
-    
-
     
 });
 

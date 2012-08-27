d5c8aeb0b4b32cf362d37f42aa5387b6c5bc3c4e
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-28 01:31:24 +0200

    [mirror commit] [ci skip] {   "rev": "180281",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheetsHTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheetsHTML.js\n" }

--- core/lively/morphic/tests/StyleSheetsHTML.js
+++ core/lively/morphic/tests/StyleSheetsHTML.js
@@ -4,46 +4,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.Morph
 'running', {
     setUp: function($super) {
         $super();
-        this.createSomeMorphs();
     },
-    createSomeMorphs: function() {
-        // this method creates 4 morphs: yellowRectange is the ouyter parent
-        // redRectangle its embedded submorph, blueRectangle1, blueRectangle1
-        // are its submorphs
-        var yellowRectangle = lively.morphic.Morph.makeRectangle(0,0, 300, 300);
-        yellowRectangle.applyStyle({fill: Color.yellow});
-        yellowRectangle.tagName = 'YellowRectangle';
-        yellowRectangle.testAttribute = 'theYellowRectangle';
-        yellowRectangle.openInWorld();
-
-
-        var redRectangle = lively.morphic.Morph.makeRectangle(25, 25, 250, 250);
-        redRectangle.applyStyle({fill: Color.red});
-        redRectangle.addStyleClassName('red');
-        redRectangle.setStyleId('the-red-rectangle');
-        redRectangle.testAttribute = 'theRedRectangle';
-        yellowRectangle.addMorph(redRectangle);
-
-        var blueRectangle1 = lively.morphic.Morph.makeRectangle(10, 10, 150, 100);
-        blueRectangle1.applyStyle({fill: Color.blue});
-        blueRectangle1.addStyleClassName('blue');
-        blueRectangle1.setStyleId('b1');
-        redRectangle.addMorph(blueRectangle1);
-
-        var blueRectangle2 = lively.morphic.Morph.makeRectangle(10, 160, 150, 80);
-        blueRectangle2.applyStyle({fill: Color.blue});
-        blueRectangle2.addStyleClassName('blue');
-        blueRectangle2.setStyleId('b2');
-        blueRectangle2.tagName = 'blueRectangleTag';
-        redRectangle.addMorph(blueRectangle2);
-        
-        this.yellowRectangle = yellowRectangle;
-        this.redRectangle = redRectangle;
-        this.blueRectangle1 = blueRectangle1;
-        this.blueRectangle2 = blueRectangle2;
-        
-    },
-
 },
 'testing', {
 

9d2f2d49b22a69206e23423374e26f890ffcee9f
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-27 23:49:31 +0200

    [mirror commit] [ci skip] {   "rev": "180201",   "author": "bgnauk",   "changes": "U   core/lively/morphic/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheets.js\n" }

--- core/lively/morphic/StyleSheets.js
+++ core/lively/morphic/StyleSheets.js
@@ -20,8 +20,14 @@ lively.morphic.Morph.addMethods(
         //return this.morphicSetter('StyleSheetRules', styleSheetRules);
     },
     getStyleSheetRules: function() {
-        var rules = this.processStyleSheet(this.morphicGetter('StyleSheet'));
-        return rules;
+        var styleSheet = this.morphicGetter('StyleSheet');
+        
+        if (styleSheet && styleSheet.length && styleSheet.length > 0) {
+            return this.processStyleSheet(styleSheet);
+        } else {
+            return [];
+        }
+        
     }
   
 },

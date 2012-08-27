ae51334285214c7fb320231cdad1b00550a76c66
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-28 00:36:45 +0200

    [mirror commit] [ci skip] {   "rev": "180238",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -256,14 +256,14 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.Sizzl
 
         var blueRectangle1 = lively.morphic.Morph.makeRectangle(10, 10, 150, 100);
         blueRectangle1.applyStyle({fill: Color.blue});
-        blueRectangle1.addClassName('blue');
-        blueRectangle1.setNewId('b1');
+        blueRectangle1.addStyleClassName('blue');
+        blueRectangle1.setStyleId('b1');
         redRectangle.addMorph(blueRectangle1);
 
         var blueRectangle2 = lively.morphic.Morph.makeRectangle(10, 160, 150, 80);
         blueRectangle2.applyStyle({fill: Color.blue});
-        blueRectangle2.addClassName('blue');
-        blueRectangle2.setNewId('b2');
+        blueRectangle2.addStyleClassName('blue');
+        blueRectangle2.setStyleId('b2');
         redRectangle.addMorph(blueRectangle2);
         
         this.yellowRectangle = yellowRectangle;

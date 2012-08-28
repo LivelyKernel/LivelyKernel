51fa41b79af261844e4e6bebdd0064e7bf6785a0
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-28 02:03:05 +0200

    [mirror commit] [ci skip] {   "rev": "180304",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheetsHTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheetsHTML.js\n" }

--- core/lively/morphic/tests/StyleSheetsHTML.js
+++ core/lively/morphic/tests/StyleSheetsHTML.js
@@ -29,42 +29,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheetsHTML.S
 
     
     
-    testSelectMorphById: function() {
-        this.assertEqualState(this.redRectangle, this.world.getSubmorphByStyleId('the-red-rectangle'),
-            'selection by id should only include red rectangle morph');
-
-    },
-    testSelectMorphByClassName: function() {
-
-        this.assertEqualState([this.blueRectangle1, this.blueRectangle2], 
-            this.world.getSubmorphsByStyleClassName('blue'),
-            'selection by class should include both blue rectangle morphs');
-
-    },
-    testSelectMorphByTagName: function() {
-
-        this.assertEqualState([this.blueRectangle2], 
-            this.world.getSubmorphsByTagName('blueRectangleTag'),
-            'selection by tag should include the 2nd blue rectangle morph only');
-
-    },
-
-
-    testSelectMorphByAttributes: function() {
-        
-        this.assertEqualState([this.yellowRectangle, this.redRectangle], 
-            this.world.getSubmorphsByAttribute('testAttribute'),
-            'selection by attribute should include the yellow and the red morph');
-        
-        this.assertEqualState([this.yellowRectangle], 
-            this.world.getSubmorphsByAttribute('testAttribute', 'theYellowRectangle'),
-            'selection by attribute should include the yellow morph');
-        
-        this.assertEqualState([this.yellowRectangle], 
-            this.world.getSubmorphsByAttribute('testAttribute', 'tHeYellOwRectAnglE', true),
-            'selection by attribute should include the yellow morph (case insensitive)');
-
-    },
+    
     
 
     

b2020c0fc6d85d3deb827510eda509371a271d87
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-28 20:21:59 +0200

    [mirror commit] [ci skip] {   "rev": "180401",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTML.js\n" }

--- core/lively/morphic/HTML.js
+++ core/lively/morphic/HTML.js
@@ -296,8 +296,8 @@ lively.morphic.Morph.addMethods(
         this.insertMorphNodeInHTML(ctx, ctx.morphNode, parentNode, afterNode, ctx.shapeNode);
         if (this.originClass) this.setOriginClassHTML(ctx, this.originClass);
         this.getShape().renderUsing(ctx);
-        this.setNodeId(this.id);
-        this.setStyleSheet(this.getStyleSheet());
+        //this.setNodeId(this.id);
+        //this.setStyleSheet(this.getStyleSheet());
         //this.getShape().setStyleSheetHTML(ctx, this.getShape().getStyleSheet());
     },
     insertMorphNodeInHTML: function(ctx, morphNode, parentNode, optAfterNode) {

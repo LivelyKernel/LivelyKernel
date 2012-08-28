23dcb3d6cc729182430fe9e48b5cf35d1bd30611
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-28 23:25:22 +0200

    [mirror commit] [ci skip] {   "rev": "180484",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheetsHTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheetsHTML.js\n" }

--- core/lively/morphic/tests/StyleSheetsHTML.js
+++ core/lively/morphic/tests/StyleSheetsHTML.js
@@ -56,7 +56,7 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheetsHTML.S
 
     assertDOMMorphNodeAttribute: function(targetValue, attributeName, morph, msg) {
         var morphNode = morph.renderContext().morphNode;
-        return this.assertEquals(targetValue, $(morphNode).attr(attributeName), msg);
+        return this.assertEquals(targetValue, $(shapeNode).attr(attributeName), msg);
     },
 
     test01SetStyleClassNames: function() {

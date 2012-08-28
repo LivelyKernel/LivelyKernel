4098222c7ee32308304888476122dbb0ba012bd2
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-29 01:11:26 +0200

    [mirror commit] [ci skip] {   "rev": "180508",   "author": "bgnauk",   "changes": "U   core/lively/morphic/HTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/HTML.js\n" }

--- core/lively/morphic/HTML.js
+++ core/lively/morphic/HTML.js
@@ -866,50 +866,7 @@ lively.morphic.Shapes.Shape.addMethods(
         return r;
     },
 
-    setStyleSheetHTML: function(ctx, value) {
-        
-        
-        var morphId = ctx.shapeNode && ctx.shapeNode.id;
-        if (!morphId) {
-            //console.log('Warning, morph has no shape node or shape node was not given any id.');
-            return;
-        }
 
-        var styleTagId = "style-for-"+morphId;
-
-	if (ctx.styleNode) {
-	   $(ctx.styleNode).remove();
-        }
-        
-        if (value && value.length > 1) {
-
-    	    //console.log("Setting CSS for shape "+morphId+" to "+value);
-            var specificCss = "#"+morphId+" { "+value+" }";
-
-            // syntax fixes for the sap gold reflection css
-            specificCss = specificCss.replace(/[\s]*=[\s]*/g,"=");
-            specificCss = specificCss.replace(/alpha[\s]*\([\s]*opacity[\s]*\:/g,"alpha(opacity=");
-            specificCss = specificCss.replace(".dev-datepicker/jQuery",".dev-datepicker.jQuery");
-
-
-            if (less) {
-                new less.Parser().parse(specificCss, function(e, tree) {
-                    //console.log(e);
-                    specificCss = tree.toCSS();
-                });
-                console.log(specificCss);
-            }
-
-
-	        ctx.styleNode = $('<style type="text/css" id="' + styleTagId + '"></style>');
-	        ctx.styleNode.text(specificCss);
-	        ctx.styleNode.appendTo(document.head);
-
-        } else {
-            delete this._StyleSheet;
-        }
-
-    },
 
     getComputedBorderWidthHTML: function(ctx) {
         var width = ($(ctx.shapeNode).outerWidth() - $(ctx.shapeNode).width()) / 2;

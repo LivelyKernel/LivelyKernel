f3627e8da4577a5b7ae3abde302ab2cb49579b6b
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-29 23:52:27 +0200

    [mirror commit] [ci skip] {   "rev": "180631",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheetsHTML.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheetsHTML.js\n" }

--- core/lively/morphic/tests/StyleSheetsHTML.js
+++ core/lively/morphic/tests/StyleSheetsHTML.js
@@ -89,16 +89,16 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheetsHTML.H
         morph2Level2.addMorph(morph3Level3);
 
         morph1Level1.openInWorld();
-        appendStyleNode(morph3Level3).bind(this);
-        appendStyleNode(morph1Level1).bind(this);
+        appendStyleNode(morph3Level3);
+        appendStyleNode(morph1Level1);
         assertStyleNode(morph1Level1, morph3Level3,
             'Style node of 11 is not before 33').bind(this);
-        appendStyleNode(morph2Level2).bind(this);
+        appendStyleNode(morph2Level2);
 
         assertStyleNode(morph2Level2, morph3Level3,
-             'Style node of 22 is not before 33').bind(this);
-         assertStyleNode(morph1Level1, morph2Level2,
-             'Style node of 11 is not before 22').bind(this);
+             'Style node of 22 is not before 33');
+        assertStyleNode(morph1Level1, morph2Level2,
+             'Style node of 11 is not before 22');
 
   
 

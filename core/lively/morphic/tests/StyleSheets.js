637a3aa92ca05fb37f724a3ac630599e691b9c1a
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-08-30 23:46:46 +0200

    [mirror commit] [ci skip] {   "rev": "180710",   "author": "bgnauk",   "changes": "U   core/lively/morphic/tests/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/tests/StyleSheets.js\n" }

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -581,8 +581,8 @@ lively.morphic.tests.MorphTests.subclass('lively.morphic.tests.StyleSheets.CSSFo
 
         this.assertEquals('red', getVal(sortedRules[0]),
             'rule 0 should have color red');
-        this.assertEquals('green', getVal(sortedRules[1]),
-            'rule 1 should have color green');
+        this.assertEquals('black', getVal(sortedRules[1]),
+            'rule 1 should have color black');
         this.assertEquals('blue', getVal(sortedRules[2]),
             'rule 2 should have color blue');
         this.assertEquals('black', getVal(sortedRules[3]),

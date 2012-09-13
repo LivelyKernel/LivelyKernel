8e0b8a43199d7d8072050a78abbce7a2ed231eec
bgnauk <lively-kernel@hpi.uni-potsdam.de>
2012-09-13 16:08:47 -0700

    from ww: cssParser changes

--- apps/cssParser.js
+++ apps/cssParser.js
@@ -2,7 +2,24 @@ module('apps.cssParser').requires('lively.Network').toRun(function(m) {
 
 (function loadLib() {
     // FIXME load async!
-    jQuery.ajax({url: URL.codeBase.withFilename('lib/cssParser.js'), async: false});
+    var url = URL.codeBase.withFilename('lib/cssParser.js'),
+        src = url.asWebResource().beSync().get().content,
+
+	// lib has to be adapted to suit our needs:
+	// - declarations shall not be multiplied with vendor prefixes
+	// - shorthands shall never be expanded
+        vendorRepl = "kCSS_VENDOR_PREFIXES = {properties:[]};",
+	expRepl = 'aExpandShorthands = false;',
+	rewritten = '(function (){'
+            + src.replace(/kCSS_VENDOR_PREFIXES([\s\S]*?)};/, vendorRepl)
+            .replace(/(?=this.preserveState\(\);[\x20\t\r\n\f]*var blocks = \[\];)/, expRepl)
+            + 'this.CSSParser = CSSParser;'
+            + '}).call(apps.cssParser);';
+    try {
+        eval(rewritten);
+    } catch(e) {
+        alert('CSS Parser lib failed to load because of:\n' + e);
+    }
 })();
 
 Object.extend(apps.cssParser, {
@@ -35,25 +52,31 @@ Object.extend(apps.cssParser, {
         // ".some-class { color: red; }"
         // returns the rules as javascript objects with the interface:
         // TODO define the interface
-        var parser = new CSSParser(),
+
+        var parser = new apps.cssParser.CSSParser(),
             parsedStyleSheet = parser.parse(cssString, false, true);
 
-        if (!parsedStyleSheet) return [];
+        if (parsedStyleSheet) {
 
-        console.log(parsedStyleSheet.cssRules);
-        return parsedStyleSheet.cssRules/*.collect(function(rule) {
-            return {
-                selectorText: rule.selectorText(),
-                declarations: rule.declarations.collect(function(decl){
-                    return {
-                        valueText: decl.valueText.trim(),
-                        property: decl.property
-                    }
-                })
+            parsedStyleSheet.cssRules.each(function(rule) {
+	           if (rule.type === 0) {
+	               console.warn('CSS Error '+rule.error+': '+rule.parsedCssText);
+	           } else if (rule.declarations) {
+	               rule.declarations.each(function(decl){
+	                   decl.rule = rule;
+	               });
+	           }
+                });
+            return parsedStyleSheet;
 
-            };
-        })*/;
-    }
+            console.log(parsedStyleSheet.cssRules);
+            return parsedStyleSheet.cssRules.select(function(rule) {
+					return rule.type === 1;
+            })
+        }
+        else return [];
+    },
+
 });
 
 }); // end of module

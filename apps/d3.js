6736719ee303f2c829430177e48c14110783a209
Benjamin Gnauk <bgnauk@gmail.com>
2012-09-18 16:05:12 -0700

    added d3 lib

--- apps/d3.js
+++ apps/d3.js
@@ -2,15 +2,7 @@ module('apps.d3').requires().toRun(function() {
 
 (function loadD3() {
     //FIXME load async!
-    var src = JSLoader.getSync('http://d3js.org/d3.v2.js'),
-    rewritten = '(function (){'+ src
-        + 'this.d3 = d3;'+'}).call(apps.d3);';
-    try {
-        eval(rewritten);
-    } catch(e) {
-        alert('D3 lib failed to load because of:\n' + e);
-    }
-
+	jQuery.ajax({url: URL.codeBase.withFilename('lib/d3.v2.js'), async: false});
 })();
 
 }) // end of module

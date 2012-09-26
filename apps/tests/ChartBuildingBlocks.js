0ac5bb84e17596ba46fe00042207f74ec55ef3c1
Benjamin Gnauk <bgnauk@gmail.com>
2012-09-25 17:30:32 -0700

    Refactored chart building blocks to allow multiple data views

--- apps/tests/ChartBuildingBlocks.js
+++ apps/tests/ChartBuildingBlocks.js
@@ -1,6 +1,6 @@
 module('apps.tests.ChartBuildingBlocks').requires('lively.morphic.tests.Helper','apps.ChartBuildingBlocks').toRun(function() {
 
-lively.morphic.tests.MorphTests.subclass('apps.tests.ChartBuildingBlocks.ChartRendererTests',
+lively.morphic.tests.MorphTests.subclass('apps.tests.ChartBuildingBlocks.ChartRendererHooks',
 'running', {
     setUp: function($super) {
         $super();
@@ -21,7 +21,7 @@ lively.morphic.tests.MorphTests.subclass('apps.tests.ChartBuildingBlocks.ChartRe
 },
 'testing', {
     test01AddSubmorphsToRenderer: function() {
-        
+
         this.chartRenderer.addMorph(this.chartSeriesRenderer);
         this.chartRenderer.addMorph(this.chartDimensionRenderer);
         this.assertEquals(5, this.chartRenderer.drawSeries(1),
@@ -30,5 +30,55 @@ lively.morphic.tests.MorphTests.subclass('apps.tests.ChartBuildingBlocks.ChartRe
             'dimensions hook is not set up correctly');
     },
 });
+
+lively.morphic.tests.MorphTests.subclass('apps.tests.ChartBuildingBlocks.ChartRendererDefaults',
+'running', {
+    setUp: function($super) {
+        $super();
+        var someRect = new Rectangle(100, 100, 100, 100);
+        this.chartRenderer = new apps.ChartBuildingBlocks.ChartRenderer(someRect)
+    }
+},
+'testing', {
+    test01prepareContextDefault: function() {
+        var shapeNode = this.morph.renderContext().shapeNode,
+            context = this.chartRenderer.prepareContextDefault(shapeNode);
+        this.assertEquals(1, context.length, 'There should be only one context');
+        this.assertEquals(1, context.selectAll("svg").length,
+            'Context is not an "SVGSVGElement"');
+        this.assertEquals(shapeNode, context.first().parentNode,
+            'Context parentnode is not the shapenode');
+    },
+    test02setupDrawingAreasDefault: function() {
+        var areaArray
+                = this.chartRenderer.setupDrawingAreasDefault(null, 100, 200, [10,5,10,5]);
+
+
+        this.assert(Array.isArray(areaArray), 'areaArray should be array');
+        this.assertEquals(1, areaArray.length, 'areaArray array should be of length 1');
+        this.assertEquals(5, areaArray.first().x, 'areaArray left is not correct');
+        this.assertEquals(10, areaArray.first().y, 'areaArray top is not correct');
+        this.assertEquals(90, areaArray.first().width, 'areaArray width is not correct');
+        this.assertEquals(180, areaArray.first().height, 'areaArray height is not correct');
+    },
+
+    test03setupScalesDefault: function() {
+        this.assertEqualState({}, this.chartRenderer.setupScalesDefault(),
+        'setupScalesDefault() does not return an empty object');
+    },
+
+    test04drawDimensionsDefault: function() {
+        this.assertEquals(1234, this.chartRenderer.drawDimensionsDefault(1234),
+            'drawDimensionsDefault() does not return its parameter');
+    },
+
+    test05drawSeriesDefault: function() {
+        // Nothing should happen here anyways ...
+    }
+
+});
+
+
+
 
 }) // end of module

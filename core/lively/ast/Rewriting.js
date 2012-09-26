732cbdc9389a618860e11ef88807ba08faa64fe3
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-09-26 22:53:04 +0200

    [mirror commit] [ci skip] {   "rev": "182076",   "author": "cschuster",   "changes": "U   core/lively/ast/Rewriting.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/Rewriting.js\n" }

--- core/lively/ast/Rewriting.js
+++ core/lively/ast/Rewriting.js
@@ -350,9 +350,10 @@ lively.ast.Rewriting.Transformation.subclass('lively.ast.Rewriting.Rewriter',
         return this.storeComputationResult($super(node));
     },
     visitCall: function($super, node) {
-        return this.storeComputationResult(new lively.ast.Call(node.pos,
-           this.storeComputationResult(this.visit(node.fn)),
-           this.visitNodes(node.args)));
+        return this.storeComputationResult($super());
+        //return this.storeComputationResult(new lively.ast.Call(node.pos,
+        //   this.storeComputationResult(this.visit(node.fn)),
+        //   this.visitNodes(node.args)));
     },
     visitSend: function($super, node) {
         return this.storeComputationResult($super(node));

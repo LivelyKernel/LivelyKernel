013ce49ced968eb99da4f8e2b16230a3a6206780
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-09-26 03:49:39 +0200

    [mirror commit] [ci skip] {   "rev": "181924",   "author": "cschuster",   "changes": "U   core/lively/ast/Rewriting.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/Rewriting.js\n" }

--- core/lively/ast/Rewriting.js
+++ core/lively/ast/Rewriting.js
@@ -305,8 +305,8 @@ lively.ast.Rewriting.Transformation.subclass('lively.ast.Rewriting.Rewriter',
 });
 
 Object.extend(Global, {
-    __createClosure: function(i, scope, f) {
-        f._cachedAst = lively.ast.Rewriting.table.table[i];
+    __createClosure: function(scope, f) {
+        f._cachedAst = lively.ast.Rewriting.table.table[scope[2]];
         f._cachedScope = scope;
         return f;
     },

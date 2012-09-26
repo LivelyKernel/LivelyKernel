b5c4ef97e8be5cfb012fab478022de7a821a5aeb
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-09-26 07:26:24 +0200

    [mirror commit] [ci skip] {   "rev": "181986",   "author": "cschuster",   "changes": "U   core/lively/ast/Morphic.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/Morphic.js\n" }

--- core/lively/ast/Morphic.js
+++ core/lively/ast/Morphic.js
@@ -70,7 +70,7 @@ cop.create('DebugMethodsLayer').refineObject(Function.prototype, {
         }
         return cop.proceed(categoryName, source);
     },
-}).beGlobal();
+});
 
 lively.morphic.Text.addMethods(
 'debugging', {

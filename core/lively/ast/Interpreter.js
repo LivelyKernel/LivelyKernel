c0d310a929d52de92de01b6ddd094afeca62e81e
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-09-26 09:30:26 +0200

    [mirror commit] [ci skip] {   "rev": "182029",   "author": "cschuster",   "changes": "U   core/lively/ast/Interpreter.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/ast/Interpreter.js\n" }

--- core/lively/ast/Interpreter.js
+++ core/lively/ast/Interpreter.js
@@ -222,12 +222,12 @@ Object.subclass('lively.ast.Interpreter.Frame',
             return (+fromTo[1]) << 23 - (+fromTo[0]);
         });
         // find the node corresponding to this value
-        var node = this.func.ast().nodesMatching(function(node) {
+        this.pc = this.func.ast().nodesMatching(function(node) {
             return last == node.position();
         })[0];
         // the pc should be the next node right after the last one
         // this.pc = this.func.ast().nodeForAstIndex(node.astIndex() + 1);
-        this.pc = node.nextStatement();
+        if (!this.pc.isDebugger) this.pc = this.pc.nextStatement();
     },
     setPC: function(node) {
         this.pc = node.isFunction ? node : node.firstStatement();

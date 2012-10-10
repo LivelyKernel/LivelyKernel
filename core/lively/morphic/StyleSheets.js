021691f917b13add682c40913ef777c41d411e25
cschuster <lively-kernel@hpi.uni-potsdam.de>
2012-10-10 02:28:19 +0200

    [mirror commit] [ci skip] {   "rev": "182179",   "author": "cschuster",   "changes": "U   core/lively/morphic/StyleSheets.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/StyleSheets.js\n" }

--- core/lively/morphic/StyleSheets.js
+++ core/lively/morphic/StyleSheets.js
@@ -163,54 +163,31 @@ lively.morphic.Morph.addMethods(
         }
     },
 
-	loadStyleSheetFromFile: function(file, resourcePath){
-		// use the resourcePath parameter if the resources addressed
-		// in the CSS file are in a different directory than the CSS'.
-		// (use "" to leave the urls untouched)
-
-		var absPath = file;
-		// is the filename absolute? if not then make it absolute.
-		if (absPath.search('http://')<0) {
-		  absPath = document.location.href.toString().split('?')[0];
-		  absPath = absPath.substring(0, absPath.lastIndexOf('/') + 1);
-		  absPath += "/"+file;
-		}
-		var url = new URL(absPath);
-
-		URL.proxy = null;
-
-		var webR = new WebResource(url);
-					webR.forceUncached();
-		var webRGet = webR.get();
-		if (webRGet.status.code() == 200) {
-			// add resource path to all relative urls in the css
-			var css = webRGet.content;
-
-			var resPath = resourcePath;
-			if (!resPath){
-							resPath = absPath = absPath.substring(0, absPath.lastIndexOf('/') + 1);
-			}
-			var urlReplace = "url("+resPath;
-			var urlReplaceSingle = "url('"+resPath;
-			var urlReplaceDouble = 'url("'+resPath;
-			css = css.replace(/url\([\s]*\'(?![\s]*http)/g, urlReplaceSingle).replace(/url\([\s]*\"(?![\s]*http)/g, urlReplaceDouble ).replace(/url\((?![\s]*[\'|\"])(?![\s]*http)/g, urlReplace );
-
-			// insert line breaks so the css is more legible
-			css = css.replace(/\;(?![\s]*(\r\n|\n|\r))/g,";\n").replace(/\}(?![\s]*(\r\n|\n|\r))/g,"}\n").replace(/\{(?![\s]*(\r\n|\n|\r))/g,"{\n");
-			//console.log(css);
-
-			// set the style sheet
-			this.setStyleSheet(css);
-			}
-			else {
-				throw new Error("Couldn't load stylesheet at "+absPath+" --> " +webRGet.status.code());
-			}
-
-			return {
-				status: webRGet.status.code(),
-				responseText: webRGet.content
-			};
-		},
+    loadStyleSheetFromFile: function(file, resourcePath){
+        //cs: refactored to make it usable with https
+        // use the resourcePath parameter if the resources addressed
+        // in the CSS file are in a different directory than the CSS'.
+        // (use "" to leave the urls untouched)
+        var url = URL.ensureAbsoluteURL(file);
+        var webR = url.asWebResource();
+        webR.forceUncached();
+        if (webR.get().isSuccess() == 200) {
+            // add resource path to all relative urls in the css
+            var css = webR.content;
+            var resPath = resourcePath || url.getDirectory();
+            css = css.replace(/url\([\s]*\'(?![\s]*http)/g, "url('" + resPath)
+                     .replace(/url\([\s]*\"(?![\s]*http)/g, 'url("' + resPath)
+                     .replace(/url\((?![\s]*[\'|\"])(?![\s]*http)/g, "url(" + resPath);
+            // insert line breaks so the css is more legible
+            css = css.replace(/\;(?![\s]*(\r\n|\n|\r))/g, ";\n")
+                     .replace(/\}(?![\s]*(\r\n|\n|\r))/g, "}\n")
+                     .replace(/\{(?![\s]*(\r\n|\n|\r))/g, "{\n");
+            this.setStyleSheet(css);
+        } else {
+            throw new Error("Couldn't load stylesheet at " + file + " --> " + webR.status.code());
+        }
+        return {status: webR.status.code(), responseText: webR.content};
+    },
 
     getStyleSheet: function() {
         return this.morphicGetter('StyleSheet');

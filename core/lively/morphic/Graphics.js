8e8cd482fb412b63ee3587b7b5acc98be6e497ca
jenslincke <lively-kernel@hpi.uni-potsdam.de>
2012-10-18 12:05:16 +0200

    [mirror commit] [ci skip] {   "rev": "182470",   "author": "jenslincke",   "changes": "U   core/lively/morphic/Graphics.js\n",   "msg": "Autoversioning commit:  a non-deltaV client made a change to\n/core/lively/morphic/Graphics.js\n" }

--- core/lively/morphic/Graphics.js
+++ core/lively/morphic/Graphics.js
@@ -1,5 +1,5 @@
-module('lively.morphic.Graphics').requires('apps.ColorParser').toRun(function() {
-// 
+module('lively.morphic.Graphics').requires().toRun(function() {
+// 'apps.ColorParser'
 
 Object.subclass("Point",
 'documentation', {

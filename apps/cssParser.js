24131a53bf1839355d37343bd0367fcdf31e695e
Benjamin Gnauk <bgnauk@gmail.com>
2012-10-19 13:57:57 -0700

    Added shorthand declarations and moved CSS prop list to apps.cssParser

--- apps/cssParser.js
+++ apps/cssParser.js
@@ -9,9 +9,12 @@ module('apps.cssParser').requires('lively.Network', 'lively.morphic.StyleSheetRe
 	// - declarations shall not be multiplied with vendor prefixes
 	// - shorthands shall never be expanded
         vendorRepl = "kCSS_VENDOR_PREFIXES = {properties:[]};",
-	expRepl = 'aExpandShorthands = false;',
+	expRepl = 'aExpandShorthands = this.expandShorthands;',
+        parseRepl = 'aTryToPreserveComments, expandShorthands) {\n'
+            +'this.expandShorthands = expandShorthands;',
 	rewritten = '(function (){'
             + src.replace(/kCSS_VENDOR_PREFIXES([\s\S]*?)};/, vendorRepl)
+                .replace('aTryToPreserveComments) {', parseRepl)
             .replace(/(?=this.preserveState\(\);[\x20\t\r\n\f]*var blocks = \[\];)/, expRepl)
             + 'this.CSSParser = CSSParser;'
             + '}).call(apps.cssParser);';
@@ -72,14 +75,15 @@ Object.extend(apps.cssParser, {
                             return new lively.morphic.StyleSheetRule(
                                     rule.selectorText(),
                                     rule.declarations.collect(function(decl) {
-                                            return new lively.morphic.StyleSheetDeclaration(
-                                                    decl.property,
-                                                    decl.values.collect(function(val) {
-                                                            return val.value;
-                                                        }),
-                                                    null,
-                                                    decl.priority
-                                                )
+                                            var vals = decl.values.collect(
+                                                    function(val) {return val.value});
+                                            if (apps.cssParser.isShorthand(decl.property)) {
+                                                return new lively.morphic.StyleSheetShorthandDeclaration(
+                                                    decl.property, vals, null, decl.priority);
+                                            } else {
+                                                return new lively.morphic.StyleSheetDeclaration(
+                                                    decl.property, vals, null, decl.priority);
+                                            }
                                         })
                                 );
                         case 2:
@@ -125,48 +129,237 @@ Object.extend(apps.cssParser, {
                 originMorph
             );
     },
+    isShorthand: function(property) {
+        var propList = apps.cssParser.getPropList();
+        if (propList[property] && propList[property].shorthandFor
+            && propList[property].shorthandFor.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    },
 
-    parse: function(cssString, originMorph) {
+
+    parseShorthand: function(shorthandDeclaration) {
+        var simStyleSheet = '* {' + shorthandDeclaration.getText() + ' }',
+            parsedSimStyleSheet = apps.cssParser.parse(simStyleSheet,
+                shorthandDeclaration.getOriginMorph(), true),
+            decls = parsedSimStyleSheet.getRules().first().getDeclarations();
+        if (decls && decls.length > 0) {
+            return decls.collect(function(decl) {
+                    decl.setRule(shorthandDeclaration.getRule());
+                    return decl;
+                });
+        } else {
+            return [shorthandDeclaration];
+        }
+    },
+
+    parse: function(cssString, originMorph, expandShorthands) {
         // cssString should specifiy the css rules, e.g.
         // ".some-class { color: red; }"
         // returns the rules as javascript objects with the interface:
         // TODO define the interface
 
         var parser = new apps.cssParser.CSSParser(),
-            parsedStyleSheet = parser.parse(cssString, false, true);
+            parsedStyleSheet = parser.parse(cssString, false, true, expandShorthands);
 
-        console.log(parsedStyleSheet);
-        return this.convertParsedStyleSheet(parsedStyleSheet, originMorph);
-
-        /*
-        debugger
-        console.log(this.convertParsedStyleSheet(parsedStyleSheet));
-
-        if (parsedStyleSheet) {
-
-            parsedStyleSheet.cssRules.each(function(rule) {
-	           if (rule.type === 0) {
-	               console.warn('CSS Error '+rule.error+': '+rule.parsedCssText);
-	           } else if (rule.declarations) {
-	               rule.declarations.each(function(decl){
-	                   decl.rule = rule;
-	               });
-	           }
-                });
-            return parsedStyleSheet;
-
-            console.log(parsedStyleSheet.cssRules);
-            return parsedStyleSheet.cssRules.select(function(rule) {
-					return rule.type === 1;
-            })
-        }
-        else return [];
-        */
-
+        //console.log(parsedStyleSheet);
+        return apps.cssParser.convertParsedStyleSheet(parsedStyleSheet, originMorph);
     },
+
+    enhancePropList: function(orgPropList) {
+        // Enhances the property list in lively.morphic.CSSProperties.props
+        // by adding conclusive shorthands and shorthandFor
+        // attributes to make it faster to use for parsing.
+        var propList = {},
+            markShorthands = function (property, properties) {
+                    var shorthand = orgPropList[property].shorthand;
+                    propList[property].shorthandFor =
+                        propList[property].shorthandFor.concat(properties || []);
+                    if (shorthand && orgPropList[shorthand]) {
+                        if (properties) {
+                            properties.push(property);
+                        } else {
+                            properties = [property];
+                        }
+                        // TODO: avoid running in circles
+                        var shorthandsFor = markShorthands(shorthand, properties || []);
+                        propList[property].shorthands =
+                            propList[property].shorthands.concat(shorthandsFor);
+                        return shorthandsFor.concat(property);
+                    } else {
+                        return [property];
+                    }
+                };
+
+        // Prepare proplist
+        for (var x in orgPropList) {
+            propList[x] = {};
+            propList[x].shorthands = [];
+            propList[x].shorthandFor = [];
+        }
+        // Mark shorthands
+        for (var x in orgPropList) {
+            markShorthands(x);
+        }
+        // Make sure there are no duplicates in the shorthand attrs
+        for (var x in propList) {
+            propList[x].shorthands = propList[x].shorthands.uniq();
+            propList[x].shorthandFor = propList[x].shorthandFor.uniq();
+        }
+        return propList;
+    },
+    getPropList: function() {
+        return apps.cssParser.enhancePropList(
+            apps.cssParser.props);
+    },
+    props: {
+        /*
+        Information to interpret and manipulate CSS properties.
+        Since there are a lot of CSS properties out there, this
+        is only a selection.
+        Feel free to add missing properties!
+        
+        A property can have several value counts (i.e. the value of
+        border-color could be 'black', but it could also be 'black black black blue').
+        
+        A value is of a certain type:
+        0: Plain text (i.e. font-family; edit through text field)
+        1: Number (i.e. width; edit through slider)
+        2: Option (i.e. border-style; edit with drop-down box)
+        3: Color (i.e. color; edit with color chooser)
+        4: Shadow (i.e. box-shadow; edit with shadow dialog)
+    
+        Additionally, a property can also have a shorthand.
+        I.e. 'border-top-color' is implicitly set by the shorthand 'border-color'
+        */
+        'background-color': {
+            shorthand: 'background',
+            values: [ // only one value for this property
+            [3]]
+        },
+        'border': {
+            values: [
+            // either one value ...
+            [3],
+            // ... or four
+            [3, 3, 3, 3]]
+        },
+        'border-color': {
+            shorthand: 'border',
+            values: [
+            // either one value ...
+            [3],
+            // ... or four
+            [3, 3, 3, 3]]
+        },
+        'border-top-color': {
+            shorthand: 'border-color',
+            values: [ // only one value for this property
+            [3]]
+        },
+        'border-bottom-color': {
+            shorthand: 'border-color',
+            values: [ // only one value for this property
+            [3]]
+        },
+        'border-left-color': {
+            shorthand: 'border-color',
+            values: [ // only one value for this property
+            [3]]
+        },
+        'border-right-color': {
+            shorthand: 'border-color',
+            values: [ // only one value for this property
+            [3]]
+        },
+        'border-radius': {
+            values: [
+            // either one value ...
+            [1],
+            // ... or four
+            [1, 1, 1, 1]]
+        },
+        'color': {
+            values: [ // only one value for this property
+            [3]]
+        }
+    }
+
 
 });
 
 }); // end of module
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

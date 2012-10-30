c50bb493e73acc2ea192f58974bbd98a7fb86a57
Benjamin Gnauk <bgnauk@gmail.com>
2012-10-29 18:13:01 -0700

    Fixed CSS inline comments parsing test (somehow got lost in the last merge)

--- core/lively/morphic/tests/StyleSheets.js
+++ core/lively/morphic/tests/StyleSheets.js
@@ -905,7 +905,26 @@ TestCase.subclass('lively.morphic.tests.StyleSheets.CSSRuleInterface',
                         ', style or width in their property string');
                 }
             }, this);
+    },
+    test07ParseInlineComment: function () {
+        var styleSheet = '.Morph {\n' + '/* test */\n' + '}\n\n' + '/* test */',
+            parsedStyleSheet = apps.cssParser.parse(styleSheet);
+        this.assert(parsedStyleSheet.isStyleSheet, 'Parsed style sheet is no lively style sheet object');
+        this.assertEquals(2, parsedStyleSheet.getRules().length,
+            'Parsed style sheet does not have exactly two rules');
+        this.assert(parsedStyleSheet.getRules().first().isStyleSheetRule,
+            'First rule is no lively style sheet rule');
+        this.assert(parsedStyleSheet.getRules().last().isStyleSheetRule,
+            'Last rule is no lively style sheet rule');
+        this.assert(parsedStyleSheet.getRules().last().isStyleSheetComment,
+            'Last rule is no lively style sheet comment');
+        this.assertEquals(1, parsedStyleSheet.getRules().first().getDeclarations().length,
+            'First style sheet rule does not have exactly two declarations');
+        this.assert(parsedStyleSheet.getRules().first().getDeclarations().first().isStyleSheetInlineComment,
+            'Declaration is no lively style sheet inline comment');
+        this.assertEquals(styleSheet, parsedStyleSheet.getText(), 'CSS text output not as expected');
     }
+
 });
 
 }) // end of module

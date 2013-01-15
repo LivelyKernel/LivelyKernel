7a01c87b2f6ce849a5d1ce0a17ba91e97242c4ad
Robert Krahn <robert.krahn@gmail.com>
2013-01-14 16:01:55 -0800

    some more lively.store work - towards storage access interface - added file storage

--- core/lively/store/Interface.js
+++ core/lively/store/Interface.js
@@ -1,14 +1,26 @@
 module('lively.store.Interface').requires().toRun(function() {
 
 Object.extend(lively.store, {
-    createDB: function(name) {
-        return new lively.store.DB(name);
+    cachedStorageAccessors: {},
+    get: function(spec) {
+        if (this.cachedStorageAccessors[spec.id]) {
+            return this.cachedStorageAccessors[spec.id];
+        }
+        if (!spec.type) {
+            throw new Error('Storage type must be specified!');
+        }
+        var storageClass = lively.store[spec.type];
+        if (!storageClass) {
+            throw new Error('No such storage type ' + spec.type);
+        }
+        var storage = new storageClass(spec);
+        return this.cachedStorageAccessors[spec.id] = storage;
     }
 });
 
-Object.subclass('lively.store.DB',
+Object.subclass('lively.store.ObjectStorage',
 'initializing', {
-    initialize: function(name) {
+    initialize: function(spec) {
         this.stored = {}
     }
 },
@@ -19,6 +31,26 @@ Object.subclass('lively.store.DB',
 
     get: function(key) {
         return this.stored[key];
+    }
+});
+
+Object.subclass('lively.store.FileStorage',
+'initializing', {
+    initialize: function(spec) {
+        this.url = spec.url;
+    }
+},
+'accessing', {
+    set: function(data) {
+        var webR = new WebResource(this.url);
+        webR.put(data);
+    },
+
+    get: function(optCb) {
+        var webR = new WebResource(this.url),
+            result = webR.get().content;
+        if (optCb) optCb(null, result);
+        return result;
     }
 });
 

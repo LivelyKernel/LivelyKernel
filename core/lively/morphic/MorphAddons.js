750f8939e3c762d588ca7990029677fc0c50e323
Robert Krahn <robert.krahn@gmail.com>
2013-04-08 16:20:59 -0700

    convenience: lively.log, lively.show

--- core/lively/morphic/MorphAddons.js
+++ core/lively/morphic/MorphAddons.js
@@ -164,6 +164,10 @@ Object.extend(lively.morphic, {
     }
 
 });
+Object.extend(lively, {
+    show:     lively.morphic.show,
+    log:      lively.morphic.log
+});
 
 Object.extend(lively, {
     show:     lively.morphic.show,

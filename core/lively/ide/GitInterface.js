bf8929f612a28a25ad3dd1af52e5c796a7866db1
Robert Krahn <robert.krahn@gmail.com>
2013-04-10 19:17:46 -0700

    git: getting started with a git interface this commit has: - world with basic tooling (git cmdline) - git subserver - gitinerface module for streaming reads from server

---
+++ core/lively/ide/GitInterface.js
@@ -0,0 +1,94 @@
+module('lively.ide.GitInterface').requires('lively.Network').toRun(function() {
+
+Object.extend(lively.ide.GitInterface, {
+
+    gitControlURL: URL.create(Config.nodeJSURL).asDirectory().withFilename('GitControl/'),
+
+    commandQueue: [],
+
+    scheduleCommand: function(cmd) {
+        show("scheduleCommand %o", cmd);
+        this.commandQueue.push(cmd);
+        lively.bindings.connect(this.commandInProgress, 'end', this, 'commandFromQueue');
+    },
+
+    commandFromQueue: function() {
+        var cmd = this.commandQueue.shift();
+        show("commandFromQueue %o", cmd);
+        cmd.startRequest();
+    },
+
+    command: function(commandString, thenDo) {
+        var gitInterface = this,
+            webR = this.gitControlURL.asWebResource().beAsync(),
+            esc = {'8': String.fromCharCode(8), '27': String.fromCharCode(27)},
+            ansiColorRegex = new RegExp(Strings.format('%s\[[0-9;]*m|.%s', esc[27], esc[8]), 'g'),
+            cmd = {
+                streamPos: 0,
+                interval: null,
+                _stdout: '',
+                _stderr: '',
+                _code: '',
+
+                readInterval: function() {
+                    var xhr = webR.status && webR.status.transport;
+                    if (!xhr) return;
+                    this.read(xhr.responseText);
+                },
+
+                startInterval: function() {
+                    this.endInterval();
+                    this.interval = Global.setInterval(this.readInterval.bind(this), 50);
+                },
+
+                endInterval: function() {
+                    if (this.interval) { Global.clearInterval(this.interval); }
+                },
+                
+                read: function(string) {
+                    var input = string.slice(this.streamPos, string.length),
+                        headerMatch = input.match(/^<GITCONTROL\$([A-Z]+)([0-9]+)>/);
+                    if (!headerMatch) return;
+                    // show('pos: %s, reading: %s', this.streamPos, string);
+                    var header = headerMatch[0]
+                        type = headerMatch[1].toLowerCase(),
+                        length = Number(headerMatch[2]),
+                        readContent = input.slice(header.length, header.length+length).replace(ansiColorRegex, ''),
+                        remaining = input.slice(header.length + length);
+                    // show('type: %s, length: %s, content: %s, remaining: %s',
+                    //     type, length, readContent, remaining);
+                    lively.bindings.signal(this, type, readContent);
+                    this['_' + type] += readContent;
+                    this.streamPos += header.length + length;
+                    if (this.streamPos < string.length) this.read(string);
+                },
+
+                startRequest: function() {
+                    gitInterface.commandInProgress = this;
+                    webR.post(JSON.stringify({command: commandString}), 'application/json');
+                    this.startInterval();
+                    lively.bindings.connect(webR, 'status', this, 'endRequest', {
+                        updater: function($upd, status) {
+                            if (status.isDone()) $upd.curry(status).delay(0.1); }});
+                },
+
+                endRequest: function(status) {
+                    gitInterface.commandInProgress = null;
+                    this.endInterval();
+                    this.read(status.transport.responseText);
+                    lively.bindings.signal(this, 'end', this);
+                    if (thenDo) thenDo(cmd);
+                },
+
+                getStdout: function() { return this._stdout; },
+                getStderr: function() { return this._stderr; },
+                getCode: function() { return Number(this._code); },
+            };
+        if (!gitInterface.commandInProgress) cmd.startRequest();
+        else gitInterface.scheduleCommand(cmd);
+        return cmd;
+    }
+
+});
+
+}) // end of module
---
+++ core/servers/GitControl.js
@@ -0,0 +1,83 @@
+spawn = require('child_process').spawn;
+dir = process.env.WORKSPACE_LK;
+
+git = {process: null, stdout: '', stderr: '', lastExitCode: null}
+defaultOptions = ['--no-pager']
+
+runGit = function(/*args, thenDo*/) {
+    var args = [], thenDo;
+    for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] === 'function') { thenDo = arguments[i]; break;}
+        args.push(arguments[i]);
+    }
+
+    git.process = spawn('git', defaultOptions.concat(args), {cwd: dir});
+    git.process.stdout.on('data', function (data) {
+        // console.log('stdout: ' + data);
+        git.stdout += data;
+    });
+
+    git.process.stderr.on('data', function (data) {
+        // console.log('stderr: ' + data);
+        git.stderr += data;
+    });
+
+    git.process.on('close', function (code) {
+        console.log('git process exited with code ' + code);
+        git.process = null;
+        git.lastExitCode = code;
+    });
+
+}
+
+runGit('status')
+
+module.exports = function(route, app) {
+    
+    app.post(route, function(req, res, next) {
+        var command = req.body && req.body.command;
+        if (!command) { res.status(400).end(); return; }
+        if (git.process) { res.status(400).end({error: 'Git process still running!'}); return; }
+        runGit.apply(null, command.split(' '));
+        if (!git.process) { res.status(400).end({error: 'Could not start git!'}); return; }
+res.removeHeader('Content-Length');
+res.set({
+  'Content-Type': 'text/plain',
+  'Transfer-Encoding': 'chunked',
+//   'Content-Length': "-1",
+//   'Accept-Ranges': 'bytes'
+//   // 'ETag': '12345'
+})
+// res.set(200, {
+//                        'Transfer-Encoding': 'chunked'
+//                      , 'Content-Type': 'application/json'
+//                      , 'Accept-Ranges': 'bytes'
+// });
+// console.dir(res);
+        stdoutCount = 0;
+        git.process.stdout.on('data', function (data) {
+            console.log('stdout: ' + data);
+            // console.log('stdoutCount: %s', ++stdoutCount);
+            // res.write(JSON.stringify({stdout: data.toString()}));
+            var s = data.toString();
+            res.write('<GITCONTROL$STDOUT' + s.length + '>' + s);
+        });
+
+        stderrCount = 0;
+        git.process.stderr.on('data', function (data) {
+            // console.log('stderr: ' + data);
+            // console.log('stderrcount: %s', ++stderrCount);
+            // res.write(data.toString());
+            var s = data.toString();
+            res.write('<GITCONTROL$STDERR' + s.length + '>' + s);
+        });
+
+        git.process.on('close', function (code) {
+            var s = String(git.lastExitCode);
+            res.write('<GITCONTROL$CODE' + s.length + '>' + s);
+            res.end();
+        });
+
+    });
+
+}

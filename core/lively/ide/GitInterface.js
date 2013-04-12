cb581df063f7b201fb6e672ff573cf3dfec4768d
Robert Krahn <robert.krahn@gmail.com>
2013-04-11 22:58:42 -0700

    git: starting to create patches from diff/selections

--- core/lively/ide/GitInterface.js
+++ core/lively/ide/GitInterface.js
@@ -50,7 +50,7 @@ Object.extend(lively.ide.GitInterface, {
                         headerMatch = input.match(/^<GITCONTROL\$([A-Z]+)([0-9]+)>/);
                     if (!headerMatch) return;
                     // show('pos: %s, reading: %s', this.streamPos, string);
-                    var header = headerMatch[0]
+                    var header = headerMatch[0],
                         type = headerMatch[1].toLowerCase(),
                         length = Number(headerMatch[2]),
                         readContent = input.slice(header.length, header.length+length).replace(ansiColorRegex, ''),
@@ -82,7 +82,7 @@ Object.extend(lively.ide.GitInterface, {
 
                 getStdout: function() { return this._stdout; },
                 getStderr: function() { return this._stderr; },
-                getCode: function() { return Number(this._code); },
+                getCode: function() { return Number(this._code); }
             };
         if (!gitInterface.commandInProgress) cmd.startRequest();
         else gitInterface.scheduleCommand(cmd);
@@ -96,6 +96,109 @@ Object.extend(lively.ide.GitInterface, {
         } catch(e) { return ''; }
     }
 
+});
+
+Object.subclass("lively.ide.FilePatch",
+'initializing', {
+    read: function(patchString) {
+        // simple parser for unified patch format;
+        var lines = patchString.split('\n'), line, hunks = this.hunks = [];
+
+        // 1: parse header
+        // line 0 like: "diff --git a/test.txt b/test.txt\n"
+        line = lines.shift();
+        if (line.match(/^diff/)) { this.command = line; line = lines.shift(); }
+        if (line.match('^index')) { line = lines.shift(); }
+
+        lively.assert(line.match(/^---/), 'patch parser line 2');
+        this.pathOriginal = line.split(' ').last();
+        line = lines.shift();
+
+        lively.assert(line.match(/^\+\+\+/), 'patch parser line 3');
+        this.pathChanged = line.split(' ').last();
+
+        while (lines.length > 0) {
+            var hunk = new lively.ide.FilePatchHunk();
+            hunks.push(hunk.read(lines));
+        }
+        return this;
+    }
+},
+'patch creation', {
+    createPatchString: function() {
+        var parts = [];
+        if (this.command) parts.push(this.command);
+        parts.push(this.pathOriginal);
+        parts.push(this.pathChanged);
+        parts.pushAll(this.hunks.invoke('createPatchString'));
+        return parts.join('\n');
+    },
+    createPatchStringFromRows: function(startRow, endRow) {
+        return ''
+    }
+});
+
+Object.extend(lively.ide.FilePatch, {
+    read: function(patchString) { return new this().read(patchString); }
+});
+
+Object.subclass("lively.ide.FilePatchHunk",
+'initialize', {
+    read: function(lines) {
+        // parse header
+        var header = lines.shift(),
+            headerMatch = header.match(/^@@\s*-([0-9]+),?([0-9]*)\s*\+([0-9]+),?([0-9]*)\s*@@/);
+        lively.assert(headerMatch, 'hunk header ' + header);
+        this.header = headerMatch[0];
+        this.originalLine = Number(headerMatch[1]);
+        this.originalLength = Number(headerMatch[2]);
+        this.changedLine = Number(headerMatch[3]);
+        this.changedLength = Number(headerMatch[4]);
+        // parse context/addition/deletions
+        this.lines = [];
+        while (lines[0] && !lines[0].match(/^@@/)) {
+            this.lines.push(lines.shift());
+        }
+        this.length = this.lines.length + 1; // for header
+        return this;
+    }
+},
+'patch creation', {
+    createPatchString: function() {
+        return [this.header].concat(this.lines).join('\n');
+    },
+    createPatchStringFromRows: function(startRow, endRow) {
+        // row 0 is the header
+        var origLine = this.originalLine,
+            changedLine = this.changedLine,
+            origLength = this.originalLength,
+            changedLength = this.changedLength,
+            lines = [], header;
+        this.lines.forEach(function(line, i) {
+            i++; // compensate for header
+            if (i < startRow) {
+                switch (line[0]) {
+                    case ' ': changedLine++; origLine++; changedLength--; origLength--; return;
+                    case '-': changedLine++; origLine++; origLength--; return;
+                    case '+': changedLength--; return;
+                    default: return;
+                }
+            }
+            if (i > endRow) {
+                switch (line[0]) {
+                    case ' ': changedLength--; origLength--; return;
+                    case '-': origLength--; return;
+                    case '+': changedLength--; return;
+                    default: return;
+                }
+            }
+            lines.push(line);
+
+        });
+        header = Strings.format('@@ -%s,%s +%s,%s @@',
+            origLine, origLength, changedLine, changedLength);
+        return [header].concat(lines).join('\n');
+    }
 });
 
 }) // end of module

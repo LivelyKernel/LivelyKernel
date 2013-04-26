b59d8de98efd911eecd468e2569192728a8c5267
Robert Krahn <robert.krahn@gmail.com>
2013-04-26 11:24:48 -0700

    ometa: trying to fix travis

--- core/ometa/lively.js
+++ core/ometa/lively.js
@@ -8,7 +8,7 @@ var dependencies = [
     {url: Config.codeBase + 'ometa/bs-js-compiler.js', loadTest: function() { return typeof BSJSParser !== 'undefined'; }},
     {url: Config.codeBase + 'ometa/bs-ometa-js-compiler.js', loadTest: function() { return typeof BSOMetaJSParser !== 'undefined'; }},
     {url: Config.codeBase + 'ometa/bs-ometa-optimizer.js', loadTest: function() { return typeof BSOMetaOptimizer !== 'undefined'; }},
-    {url: Config.codeBase + 'ometa/lk-parser-extensions.js', loadTest: function() { return typeof LKJSParser !== 'undefined'; }}
+    // {url: Config.codeBase + 'ometa/lk-parser-extensions.js', loadTest: function() { return typeof LKJSParser !== 'undefined'; }}
 ];
 
 dependencies.doAndContinue(function(next, lib) {
@@ -23,15 +23,6 @@ dependencies.doAndContinue(function(next, lib) {
 
 module('ometa.lively').requires()
 .requiresLib({loadTest: function() { return !!allOMetaDependenciesLoaded; }})
-// .requiresLib({url: Config.codeBase + 'ometa/lib.js', loadTest: function() { return typeof objectThatDelegatesTo !== 'undefined'; }})
-// .requiresLib({url: Config.codeBase + 'ometa/lib.js', loadTest: function() { return typeof objectThatDelegatesTo !== 'undefined'; }})
-// .requiresLib({url: Config.codeBase + 'ometa/ometa-base.js', loadTest: function() { return typeof objectThatDelegatesTo !== 'undefined'; }})
-// .requiresLib({url: Config.codeBase + 'ometa/bs-ometa-optimizer.js', loadTest: function() { return typeof objectThatDelegatesTo !== 'undefined'; }})
-// .requiresLib({url: Config.codeBase + 'ometa/bs-js-compiler.js', loadTest: function() { return typeof objectThatDelegatesTo !== 'undefined'; }})
-// .requiresLib({url: Config.codeBase + 'ometa/bs-ometa-compiler.js', loadTest: function() { return typeof objectThatDelegatesTo !== 'undefined'; }})
 .toRun(function() {
-
-// .requiresLib({url: Config.codeBase + (false && lively.useMinifiedLibs ? 'lib/ace/lively-ace.min.js' : 'lib/ace/lively-ace.js'), loadTest: function() { return typeof ace !== 'undefined';}})
-// ('ometa.parser','ometa.bs-ometa-optimizer','ometa.bs-js-compiler','ometa.bs-ometa-compiler')
 
 });

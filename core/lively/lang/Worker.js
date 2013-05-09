f55af71ab8583e092601f7c1d687e366e64d8d20
Robert Krahn <robert.krahn@gmail.com>
2013-05-08 19:40:21 -0700

    worker: cleanup of the worker code, test failure is fixed

--- core/lively/lang/Worker.js
+++ core/lively/lang/Worker.js
@@ -28,38 +28,69 @@ setTimeout(function() { worker.terminate(); }, 5000);
 lively.Worker = {
     isAvailable: !!window.Worker,
     errors: [],
-    create: function(taskFunc) {
+    create: function() {
         // This code is triggered in the UI process directly after the
         // creation of the worker and sends the setup message to the worker
-        // for initializing it. We also create an end point for the
-        // console.log function of the worker that is installed in
-        // #workerSetupCode below
-        function run(worker) {
-            worker.postMessage({command: 'setup'});
+        // for initializing it.
+        function init(worker) {
+            var bootstrapFiles = LivelyLoader.bootstrapFiles.map(function(url) {
+                return '/' + URL.create(url).relativePathFrom(URL.root); });
+            worker.postMessage({command: 'setup', options: {bootstrapFiles: bootstrapFiles}});
             worker.onmessage = function(evt) {
-                var args = evt.data;
-                show(evt.data);
-                console.log.apply(console, args);
+                if (evt.data.workerReady) { worker.ready = true; return; }
+                if (worker.onMessage) worker.onMessage(evt);
             }
             worker.errors = [];
             worker.onerror = function(evt) {
+                console.error(evt);
                 worker.errors.push(evt);
+                if (worker.onError) worker.onError(evt);
             }
         }
 
         // This code is run inside the worker and initializes it. It installs
         // a console.log method since since this is not available by default.
         function workerSetupCode() {
-            self.realonmessage = function(evt) {
+
+            // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+            // yoshiki and robert, 05/08/13: Inserted code that sets up the lively context
+            // and globals of Lively:
+            function initGlobals(options) {
+                // 1) establish required objects
+                Global = this;
+                Global.window = Global;
+                Global.lively={};
+                Global.console = {
+                    log: function() {}, error: function() {}, warn: function() {}
+                }
+                Global.JSLoader = {
+                    loadJs: function(url, callback) {
+                        var match = url.match(/http:\/\/[^\/]+(\/?.*)/);
+                        if (match && match[1]) url = match[1];
+                        importScripts(url);
+                    },
+                    currentDir: function () {
+                        return this.dirOfURL(location.href.toString());
+                    },
+                    dirOfURL: function (url) {
+                        return url.substring(0, url.lastIndexOf('/') + 1);
+                    }
+                }
+                Global.LivelyMigrationSupport = {
+                    fixModuleName: function(n) { return n; }
+                }
+                // 2) Load bootstrap files
+                importScripts.apply(this, options.bootstrapFiles || []);
+            }
+
+            self.onmessage = function(evt) {
                 if (evt.data.command == "eval") {
                     postMessage({response: "response", value: String(eval(evt.data.source))});
                     return;
                 }
-            }
-
-            self.onmessage = function(evt) {
                 if (evt.data.command !== "setup") return;
-                self.onmessage = self.realonmessage; // delete setup handler
+                var options = evt.data.options || {};
+                initGlobals(options);
                 self.httpRequest = function (options) {
                     if (!options.url) {
                         console.log("Error, httpRequest needs url");
@@ -78,71 +109,20 @@ lively.Worker = {
                     req.send();
                 }
 
-                __CODE_PLACEHOLDER__
+                postMessage({workerReady: true});
             }
         }
 
-        // this might be too brittle...
-        // in order to not pass around strings we create functions and
-        // stringify them
-        function extractCodeOfFunction(func) {
-            var code = func.toString();
-            code = code.slice(code.indexOf('{') + 1);
-            code = code.slice(0, code.lastIndexOf('}'));
-            return code;
-        }
-        
-        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-        // yoshiki and robert, May 8 2013: Inserted code that sets up the lively context
-        // and globals of Lively:
-        function initGlobals(bootstrapFiles) {
-            Global = this;
-            Global.window = Global;
-            Global.lively={};
-            Global.console = {
-                log: function() {}, error: function() {}, warn: function() {}
-            }
-            Global.JSLoader = {
-                loadJs: function(url, callback) {
-                    var match = url.match(/http:\/\/[^\/]+(\/?.*)/);
-                    if (match && match[1]) url = match[1];
-                    importScripts(url);
-                },
-                currentDir: function () {
-                    return this.dirOfURL(location.href.toString());
-                },
-                dirOfURL: function (url) {
-                    return url.substring(0, url.lastIndexOf('/') + 1);
-                }
-            }
-            Global.LivelyMigrationSupport = {
-                fixModuleName: function(n) { return n}
-            }
-            importScripts.apply(this, bootstrapFiles);
-        }
-
-        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-        // combine the task code extracted from taskFunc with the setup code
-        var workerCode = extractCodeOfFunction(workerSetupCode),
-            workerTaskCode = extractCodeOfFunction(taskFunc);
-        workerCode = workerCode.replace("__CODE_PLACEHOLDER__", workerTaskCode);
-
-        // FIXME... It would be great if we could use data URIs...
-        // unfortunately data URIs are not recognized as same origin by a few
-        // browsers, among them Chrome. Since workers have to observe the same
-        // origin policy we have to create a temp file
+        // temporarily create a file with the worker setup code
+        // rk 05/08/13: tried out the blob object in Chrome as described in
+        // http://www.html5rocks.com/en/tutorials/workers/basics/
+        // but with no success
+        var workerCode = Strings.format('(%s)();', String(workerSetupCode));
         var codeFile = URL.source.getDirectory().withFilename('temp-worker-code.js').asWebResource();
         codeFile.put(workerCode);
         var worker = new Worker(codeFile.getURL().toString());
-        run(worker);
-        codeFile.del();
-        Strings.quote('foo')
-        var bootstrapFiles = LivelyLoader.bootstrapFiles.map(function(url) {
-            return Strings.quote('/' + URL.create(url).relativePathFrom(URL.root)); });
-        worker.postMessage({
-            command: "eval",
-            source: Strings.format('(%s).call(this, %s);',
-                    initGlobals, bootstrapFiles)});
+        lively.bindings.connect(worker, 'ready', codeFile, 'del');        
+        init(worker);
         return worker;
     }
 }
--- core/lively/lang/tests/WorkerTests.js
+++ core/lively/lang/tests/WorkerTests.js
@@ -4,46 +4,45 @@ AsyncTestCase.subclass('lively.lang.tests.WorkerTests.WorkerCreation',
 'testing', {
 
     testCreateAndRunWorker: function() {
-        var messageFromWorker = '',
-            worker = lively.Worker.create(function() {
-                self.onmessage = function(evt) {
-                    self.postMessage('Worker got "' + evt.data + '"');
-                }
+        var messageFromWorker = null,
+            worker = lively.Worker.create(),
+            workerCode = "this.onmessage = function(evt) {\n"
+                       + "    self.postMessage('Worker got \"' + evt.data + '\"');\n"
+                       + "}"
+        this.waitFor(function() { return worker.ready; }, 100, function() {
+            worker.postMessage({command: "eval", source: workerCode});
+            worker.onMessage = function(evt) { messageFromWorker = evt.data; }
+            worker.postMessage('message to worker');
+            this.waitFor(function() { return !!messageFromWorker }, 20, function() {
+                this.assertEquals('Worker got "message to worker"', messageFromWorker);
+                this.done();
             });
-        worker.onmessage = function(evt) {
-            messageFromWorker = evt.data;
-        }
-        worker.postMessage('message to worker');
-        this.delay(function() {
-            this.assertEquals('Worker got "message to worker"', messageFromWorker);
-            this.done();
-        }, 600);
+        });
     },
 
     testLoadBootstrapFiles: function() {
-        var messageFromWorker = '',
-            worker = lively.Worker.create(function() {});
-         worker.onmessage = function(evt) {
-             messageFromWorker = evt.data;
-         }
-
-        // simply eval some code in the worker scope that requires the bootstrap
-        // files to be loaded
-        var src = "try {\n"
-                + " module('lively.foo').uri()\n"
-                + " } catch(e) {\n"
-                + " String(e.stack);\n"
-                + " }"
-        worker.postMessage({command: "eval", source: src});
-
-        this.delay(function() {
-            if (worker.errors.length > 0) console.log(show(worker.errors[0].message));
-            this.assertEquals(0, worker.errors.length, 'worker got errors');
-            this.assertEquals(URL.root.withFilename('lively/foo.js'), messageFromWorker.value);
-            this.done();
-        }, 900);
+        this.setMaxWaitDelay(5000);
+        var messageFromWorker = null,
+            worker = lively.Worker.create();
+        this.waitFor(function() { return worker.ready; }, 100, function() {
+            worker.onMessage = function(evt) { messageFromWorker = evt.data; }
+            // simply eval some code in the worker scope that requires the bootstrap
+            // files to be loaded
+            var src = "(function() { try {\n"
+                    + "  return module('lively.foo').uri();\n"
+                    + "} catch(e) {\n"
+                    + "  return e + String(e.stack);\n"
+                    + "}"
+                    + "}).call()";
+            worker.postMessage({command: "eval", source: src});
+            
+            this.waitFor(function() { return !!messageFromWorker; }, 120, function() {
+                // this.assertEquals(false, worker.errors.length > 0 && worker.errors[0], 'worker got errors');
+                this.assertEquals(URL.root.withFilename('lively/foo.js'), messageFromWorker.value);
+                this.done();
+            });
+        });
     }
-
 });
 
 }); // end of module

19e82a2cbe4a0650b6c9787ca5af9ccc26bda2aa
Robert Krahn <robert.krahn@gmail.com>
2013-06-14 01:08:25 -0700

    ide: auto load commandline bindings

--- core/lively/ide.js
+++ core/lively/ide.js
@@ -1,4 +1,4 @@
-module('lively.ide').requires('lively.Helper','lively.ide.SystemCodeBrowser', 'lively.ide.VersionTools', 'lively.ide.ErrorViewer').toRun(function() {
+module('lively.ide').requires('lively.Helper','lively.ide.SystemCodeBrowser', 'lively.ide.VersionTools', 'lively.ide.ErrorViewer', 'lively.ide.CommandLineInterface').toRun(function() {
 
 if (Config.get("advancedSyntaxHighlighting")) {
     module("lively.ast.IDESupport").load();

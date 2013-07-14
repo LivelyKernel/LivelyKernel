3fcc430fcf5915c2b490a285cdbf0f98ded6cb5a
Robert Krahn <robert.krahn@gmail.com>
2013-07-14 04:12:02 -0700

    Config: defaultTextEditorExent

--- core/lively/defaultconfig.js
+++ core/lively/defaultconfig.js
@@ -491,6 +491,7 @@ Config.addOptions(
 
 "lively.ide.tools", [
     ["defaultSCBExtent", [830,515], "Size of SCB"],
+    ["defaultTextEditorExtent", [840,650], "Size of TextEditor"],
     ["defaultSCBSourcePaneToListPaneRatio", 0.525, "Ratio how much vertical space the sourcePane vs. the list panes get by default in SCB."]
 ],
 

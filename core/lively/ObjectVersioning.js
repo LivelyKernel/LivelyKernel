43dd4e57f08b105df52e9f681f9559df4d80e688
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2013-07-25 18:43:54 +0200

    Object Versioning: object table instead of property table
    not all property access via proxy redirected to global property table,
    but instead objects with properties, where properties might be primitive
    objects or alias objects and access to alias objects is redirected to a
    global object table

--- core/lively/ObjectVersioning.js
+++ core/lively/ObjectVersioning.js
@@ -1,98 +1,126 @@
 module('lively.ObjectVersioning').requires().toRun(function() {
         
-Object.subclass('lively.ObjectVersioning.ObjectVersioner', 
-'initialization', {
-    initialize: function() {
-        this.currentVersion = {};
-        this.versionHistory = []; // linear versionHistory, for now
-        this.versionHistory.push(this.currentVersion);
-    }
-},
-'version handling', {
-    nextVersion: function() {
-        this.currentVersion = Object.create(this.currentVersion);
-        this.versionHistory.push(this.currentVersion);
-    },
-    write: function(key, value) {
-        this.nextVersion();
-        this.currentVersion[key] = value;
-    },
-    read: function(key) {
-        return this.currentVersion[key];
-    },
-    previousVersion: function() {
-        var index = this.versionHistory.indexOf(this.currentVersion) - 1;
-        if (index < 0) {
-            return undefined;
-        }
-        return this.versionHistory[index];
-    },
-    followingVersion: function() {
-        var index = this.versionHistory.indexOf(this.currentVersion) + 1;
-        if (index === this.versionHistory.size()) {
-            return undefined;
-        }
-        return this.versionHistory[index];
-    },
-    undo: function() {
-        var previousVersion = this.previousVersion();
-        if (!previousVersion) {
-            throw new TypeError("No changes can't be undone");
-        }
-        this.currentVersion = previousVersion;
-    },
-    redo: function() {
-        var followingVersion = this.followingVersion();
-        if (!followingVersion) {
-            throw new TypeError("No changes can't be redone");
-        }
-        this.currentVersion = followingVersion;
-    },
-});
+// Object.subclass('lively.ObjectVersioning.ObjectVersioner', 
+// 'initialization', {
+//     initialize: function() {
+//         this.currentVersion = {};
+//         this.versionHistory = []; // linear versionHistory, for now
+//         this.versionHistory.push(this.currentVersion);
+//     }
+// },
+// 'version handling', {
+//     nextVersion: function() {
+//         this.currentVersion = Object.create(this.currentVersion);
+//         this.versionHistory.push(this.currentVersion);
+//     },
+//     write: function(key, value) {
+//         this.nextVersion();
+//         this.currentVersion[key] = value;
+//     },
+//     read: function(key) {
+//         return this.currentVersion[key];
+//     },
+//     previousVersion: function() {
+//         var index = this.versionHistory.indexOf(this.currentVersion) - 1;
+//         if (index < 0) {
+//             return undefined;
+//         }
+//         return this.versionHistory[index];
+//     },
+//     followingVersion: function() {
+//         var index = this.versionHistory.indexOf(this.currentVersion) + 1;
+//         if (index === this.versionHistory.size()) {
+//             return undefined;
+//         }
+//         return this.versionHistory[index];
+//     },
+//     undo: function() {
+//         var previousVersion = this.previousVersion();
+//         if (!previousVersion) {
+//             throw new TypeError("No changes can't be undone");
+//         }
+//         this.currentVersion = previousVersion;
+//     },
+//     redo: function() {
+//         var followingVersion = this.followingVersion();
+//         if (!followingVersion) {
+//             throw new TypeError("No changes can't be redone");
+//         }
+//         this.currentVersion = followingVersion;
+//     },
+// });
 
 Object.extend(lively.ObjectVersioning, {
+    proxyMetaProperties: ['__alias', '__target'],
     init: function() {
-        if (!this.activeVersioner) {
-            this.activeVersioner = new lively.ObjectVersioning.ObjectVersioner();
+        if (!lively.currentObjectTable) {
+            lively.currentObjectTable = [];
         }
     },
     reset: function() {
-        delete this.activeVersioner;
+        delete lively.currentObjectTable;
         this.init();
     },
-    undo: function() {
-        this.activeVersioner.undo();
+    wrap: function(target) {
+        var proxy = Proxy(target, this.versioningProxyHandler());
+        lively.currentObjectTable.push(proxy);
+        proxy.__target = target;
+        proxy.__alias = {
+            isAlias: true,
+            id: lively.currentObjectTable.length - 1
+        };        
+        return proxy;
     },
-    redo: function() {
-        this.activeVersioner.redo();
-    },
-    versioningProxyFor: function(target) {
-        return Proxy(target, this.versioningProxyHandler());
-    },
-    write: function(key, value) {
-        this.activeVersioner.write(key, value);
-    },
-    read: function(key) {
-        return this.activeVersioner.read(key);
+    unwrap: function(proxy){
+        return proxy.__target;
     },
     versioningProxyHandler: function() {
         return {
             set: function(target, name, value, receiver) {
-                if (!this[name]) {
-                    this[name] = new UUID().id;
+                
+                if (lively.ObjectVersioning.proxyMetaProperties.include(name)) {
+                    this[name] = value;
+                    return true;
                 }
-                lively.ObjectVersioning.write(this[name], value);
+                                                
+                if (value.__alias) {
+                    // proxied object
+                    target[name] = value.__alias;
+                } else {
+                    // primitive object
+                    target[name] = value;
+                }
                 return true;
             },
             get: function(target, name, receiver) {
-                if (this[name]) {
-                    return lively.ObjectVersioning.read(this[name]);
+                
+                if (lively.ObjectVersioning.proxyMetaProperties.include(name)) {
+                    return this[name];
+                }
+                                
+                if (target[name].isAlias) {
+                    // alias property
+                    return lively.currentObjectTable[target[name].id];
                 } else {
-                    return undefined;
+                    // primitive object
+                    return target[name];
                 }
             },
         };
     },
+
+    // undo: function() {
+    //     this.activeVersioner.undo();
+    // },
+    // redo: function() {
+    //     this.activeVersioner.redo();
+    // },
+    // write: function(key, value) {
+    //     this.activeVersioner.write(key, value);
+    // },
+    // read: function(key) {
+    //     return this.activeVersioner.read(key);
+    // },
 });
 
 lively.ObjectVersioning.init();
--- core/lively/tests/ObjectVersioningTests.js
+++ core/lively/tests/ObjectVersioningTests.js
@@ -1,57 +1,37 @@
 module('lively.tests.ObjectVersioningTests').requires('lively.TestFramework', 'lively.ObjectVersioning').toRun(function() {
     
-TestCase.subclass('lively.tests.ObjectVersioningTests.PropertyProxyTest',
+TestCase.subclass('lively.tests.ObjectVersioningTests.VersioningTests',
 'testing', {
     setUp: function() {
-        // global reset on each test for now
+        // global reset on each test (for now)
         lively.ObjectVersioning.reset();
     },
-    test01PropertyRetrievable: function() {
-        var person = lively.ObjectVersioning.versioningProxyFor({});
+    test01proxyCreation: function() {
+        var target = {},
+            proxy = lively.ObjectVersioning.wrap(target);
+        this.assertEquals(lively.ObjectVersioning.unwrap(proxy), target);
+    },
+    test02PrimitivePropertyRetrievable: function() {
+        var person = lively.ObjectVersioning.wrap({});
         person.age = 24;
         this.assertEquals(person.age, 24);
     },
-    test02PropertyCanBeOverwritten: function() {
-        var app = lively.ObjectVersioning.versioningProxyFor({});
+    test03PropertyCanBeOverwritten: function() {
+        var app = lively.ObjectVersioning.wrap({});
         app.counter = 1;
         app.counter = 2;
         app.counter = 3;
         this.assertEquals(app.counter, 3);
     },
-    test03ChangesCanBeUndone: function() {
-        var app = lively.ObjectVersioning.versioningProxyFor({});
-        app.counter = 1;
-        app.counter = 2;
-        lively.ObjectVersioning.undo();
-        this.assertEquals(app.counter, 1);
+    test04ObjectPropertyRetrievable: function() {
+        var person = lively.ObjectVersioning.wrap({});
+        var address = lively.ObjectVersioning.wrap({street: 'Friedrichstraße', number: '112b'});
+        person.address = address;
+        this.assertEquals(person.address, address);
     },
-    test04PropertyCreationCanBeUndone: function() {
-        var obj = lively.ObjectVersioning.versioningProxyFor({});
-        obj.isPropertyDefined = true;
-        lively.ObjectVersioning.undo();
-        this.assert(true, obj.isPropertyDefined === undefined);
-    },
-    test05UndoneChangesCanBeRedone: function() {
-        var address = lively.ObjectVersioning.versioningProxyFor({});
-        address.street = 'Meanstreet';
-        address.city = 'Chicago';
-        lively.ObjectVersioning.undo();
-        lively.ObjectVersioning.redo();
-        this.assertEquals(address.city, 'Chicago');
-    },
-    test06UndonePropertyAdditionCanBeRedone: function() {
-        var address = lively.ObjectVersioning.versioningProxyFor({});
-        address.street = 'Meanstreet';
-        address.city = 'Chicago';
-        lively.ObjectVersioning.undo();
-        lively.ObjectVersioning.undo();
-        lively.ObjectVersioning.redo();
-        this.assertEquals(address.street, 'Meanstreet');
-        this.assert(true, address.city === undefined);
-    },
-    test07CompoundPropertyRetrievable: function() {
-        var graphic = lively.ObjectVersioning.versioningProxyFor({});
-        graphic.position = lively.ObjectVersioning.versioningProxyFor({});
+    test05CompoundPropertyRetrievable: function() {
+        var graphic = lively.ObjectVersioning.wrap({});
+        graphic.position = lively.ObjectVersioning.wrap({});
         graphic.position.x = 13;
         graphic.position.y = 25;
         graphic.position.x = 0;
@@ -59,27 +39,59 @@ TestCase.subclass('lively.tests.ObjectVersioningTests.PropertyProxyTest',
         this.assertEquals(graphic.position.x, 0);
         this.assertEquals(graphic.position.y, 0);
     },
-    test08ChangesToCompoundPropertyCanBeUndone: function() {
-        var app = lively.ObjectVersioning.versioningProxyFor({});
-        app.view = lively.ObjectVersioning.versioningProxyFor({});
-        app.view.color = Color.red;
-        app.view.color = Color.green;
-        lively.ObjectVersioning.undo();
-        this.assertEquals(app.view.color, Color.red);
-    },
-    test09ReferenceSharedAmongObjects: function() {
-        var client1 = lively.ObjectVersioning.versioningProxyFor({}),
-            client2 = lively.ObjectVersioning.versioningProxyFor({});
-        client1.sharedServer = lively.ObjectVersioning.versioningProxyFor({});
-        client1.sharedServer.color = Color.red;
+    // test03ChangesCanBeUndone: function() {
+    //     var app = lively.ObjectVersioning.wrap({});
+    //     app.counter = 1;
+    //     app.counter = 2;
+    //     lively.ObjectVersioning.undo();
+    //     this.assertEquals(app.counter, 1);
+    // },
+    // test04PropertyCreationCanBeUndone: function() {
+    //     var obj = lively.ObjectVersioning.wrap({});
+    //     obj.isPropertyDefined = true;
+    //     lively.ObjectVersioning.undo();
+    //     this.assert(true, obj.isPropertyDefined === undefined);
+    // },
+    // test05UndoneChangesCanBeRedone: function() {
+    //     var address = lively.ObjectVersioning.wrap({});
+    //     address.street = 'Meanstreet';
+    //     address.city = 'Chicago';
+    //     lively.ObjectVersioning.undo();
+    //     lively.ObjectVersioning.redo();
+    //     this.assertEquals(address.city, 'Chicago');
+    // },
+    // test06UndonePropertyAdditionCanBeRedone: function() {
+    //     var address = lively.ObjectVersioning.wrap({});
+    //     address.street = 'Meanstreet';
+    //     address.city = 'Chicago';
+    //     lively.ObjectVersioning.undo();
+    //     lively.ObjectVersioning.undo();
+    //     lively.ObjectVersioning.redo();
+    //     this.assertEquals(address.street, 'Meanstreet');
+    //     this.assert(true, address.city === undefined);
+    // },
+
+    // test08ChangesToCompoundPropertyCanBeUndone: function() {
+    //     var app = lively.ObjectVersioning.wrap({});
+    //     app.view = lively.ObjectVersioning.wrap({});
+    //     app.view.color = Color.red;
+    //     app.view.color = Color.green;
+    //     lively.ObjectVersioning.undo();
+    //     this.assertEquals(app.view.color, Color.red);
+    // },
+    // test09ReferenceSharedAmongObjects: function() {
+    //     var client1 = lively.ObjectVersioning.wrap({}),
+    //         client2 = lively.ObjectVersioning.wrap({});
+    //     client1.sharedServer = lively.ObjectVersioning.wrap({});
+    //     client1.sharedServer.color = Color.red;
         
-        client2.sharedServer = client1.sharedServer;
-        client2.sharedServer.color = Color.green;
+    //     client2.sharedServer = client1.sharedServer;
+    //     client2.sharedServer.color = Color.green;
         
-        this.assertEquals(client1.sharedServer.color, Color.green);
-    }
+    //     this.assertEquals(client1.sharedServer.color, Color.green);
+    // }
     // test10VersionedObjectHasItsProperties: function() {
-    //     var versionedObject = lively.ObjectVersioning.versioningProxyFor({});
+    //     var versionedObject = lively.ObjectVersioning.wrap({});
     //     versionedObject.firstProperty = 'erstesMerkmal';
     //     this.assertEquals(true, 'firstProperty' in versionedObject);
     // }

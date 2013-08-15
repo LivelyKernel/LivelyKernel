5863a5bd53bf47d17e6e0e8660f2bdadfd83caf6
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2013-08-15 13:46:05 +0200

    Object Versioning: improved source code transformations
    building all replacement nodes using constructors (instead of assembling
    stmt-strings and parsing those to create the necessary AST trees)

--- core/lively/versions/UglifyTransformer.js
+++ core/lively/versions/UglifyTransformer.js
@@ -5,9 +5,12 @@ Global.MOZ_SourceMap = Global.sourceMap;
         
 Object.extend(lively.versions.UglifyTransformer, {
     
-    // FIXME: instead of creating new nodes by parsing (assembled) strings
-    // it's probably faster and definitely cleaner to just use ordinary constructors
-    // to build the necessary sub-trees with which original nodes should be replaced
+    // FIXME: function declarations get hoisted in JavaScript, so we need to move the func 
+    // decs we transform to variable assignments (of func exprs) to the beginning of each 
+    // scope (function scope), while preserving the lexical order of function declarations 
+    // (that is, the order of parsing, not the order of execution)
+    // javascriptweblog.wordpress.com/2010/07/06/function-declarations-vs-function-expressions/
+    
     transformSource: function (originalSource, optCodeGeneratorOptions) {
         var originalAst,
             transformedAst,
@@ -15,18 +18,41 @@ Object.extend(lively.versions.UglifyTransformer, {
             outputStream,
             codeGeneratorOptions = optCodeGeneratorOptions || {};
         
-        var wrapIntoProxyFunction = function(node) {
-            var exprNode = UglifyJS.parse('lively.versions.ObjectVersioning.proxy()'),
-                callNode = exprNode.body[0].body;
-                
-            callNode.args.push(node);
-            
-            // FIXME: is the following necessary?
-            callNode.start = node.start;
-            callNode.end = node.end;
-            
-            return callNode;
+        var exchangeLiteralExpression = function(node) {
+            // returns AST for: lively.versions.ObjectVersioning.proxy(node)
+           return new UglifyJS.AST_Call({
+                expression: new UglifyJS.AST_Dot({
+                    expression: new UglifyJS.AST_Dot({
+                        expression: new UglifyJS.AST_Dot({
+                            expression: new UglifyJS.AST_SymbolRef({
+                                name: 'lively'
+                            }),
+                            property: 'versions'
+                        }),
+                        property: 'ObjectVersioning'
+                    }),
+                    property: 'proxy'
+                }),
+                args: [node],
+                start: node.start,
+                end: node.end
+            });
         };
+        
+        var exchangeFunctionDeclaration = function(functionDeclaration) {
+            // takes function declaration: function fName() {...}
+            // returns AST for: var fName = lively.versions.ObjectVersioning.proxy(function fName() {...})
+            return new UglifyJS.AST_Var({
+                definitions: [new UglifyJS.AST_VarDef({
+                    name: new UglifyJS.AST_SymbolVar({
+                        name: functionDeclaration.name.name
+                    }),
+                    value: exchangeLiteralExpression(functionDeclaration)
+                })],
+                start: functionDeclaration.start,
+                end: functionDeclaration.end
+            }); 
+        }
         
         var wrapLiterals = new UglifyJS.TreeTransformer(null, function(node) {
             var result;
@@ -34,9 +60,11 @@ Object.extend(lively.versions.UglifyTransformer, {
             if (node instanceof UglifyJS.AST_Object || 
                 node instanceof UglifyJS.AST_Array ||
                 node instanceof UglifyJS.AST_Function) {
-                // AST_Function is a function expression, not a function declaration, but
+                // AST_Function is a function expression, not a function declaration.
+                // function expressions aren't accessible by name outside of the function body,
+                // so there's no need to expose the function name via a variable
                                 
-                result = wrapIntoProxyFunction(node);
+                result = exchangeLiteralExpression(node);
             } else if (node instanceof UglifyJS.AST_Defun) {
                 // AST_Defun is a function declaration, not a function expression, but wrapping the
                 // function literal with the proxy function, meaning supplying the literal as an argument
@@ -44,34 +72,15 @@ Object.extend(lively.versions.UglifyTransformer, {
                 // which isn't automatically accessible by its name in the current scope. therefore, we
                 // need to assign all function declarations to variables matching the function names.
                 // btw. it's syntactically legal to declare variables multiple times, their declaration
-                // gets hoisted anyway (only their declaration, not their assignment(s)...)
-                
-                // FIXME: function declarations get hoisted in JavaScript, so we need to move the func 
-                // decs we transform to variable assignments (of func exprs) to the beginning of each 
-                // scope (function scope), while preserving the lexical order of function declarations 
-                // (that is, the order of parsing, not the order of execution)
-                
-                // for more info see:
-                // javascriptweblog.wordpress.com/2010/07/06/function-declarations-vs-function-expressions/
+                // gets hoisted anyway (only their declaration, not their assignment(s)...
                 
                 // FIXME: is this really a problem? can this really happen?
                 if (!node.name.name || !Object.isString(node.name.name)) 
                     throw new Error('Source Transformations encountered function declaration without name');
-                                
-                var newStmt = 'var ' + node.name.name + ' = ' + '"standin"';
                 
-                var newStmtAst = UglifyJS.parse(newStmt);
-                var newVarStmtAst = newStmtAst.body[0];
-                
-                newVarStmtAst.start = node.start;
-                newVarStmtAst.end = node.end;
-                
-                // exchange the AST node of the 'standin' string with the wrapped original node
-                newVarStmtAst.definitions[0].value = wrapIntoProxyFunction(node);
-                
-                return newVarStmtAst;
+                result = exchangeFunctionDeclaration(node);
             } else if (node instanceof UglifyJS.AST_Accessor) {
-                // FIXME: what the heck is this?
+                // FIXME: what is an AST_Accessor node?
                 throw new Error('Transformations of AST_Accessor not yet implemented');
             } else {
                 result = node;

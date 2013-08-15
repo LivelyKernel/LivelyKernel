c51337c4ec1457344604918ce08a46b6ee60ae75
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2013-08-15 16:51:17 +0200

    Object Versioning: micro benchmark: applying proxied constructors

---
+++ core/lively/versions/tests/MicroBenchmarks.js
@@ -0,0 +1,20 @@
+module('lively.versions.tests.MicroBenchmarks').
+requires('lively.TestFramework', 'lively.versions.ObjectVersioning').
+toRun(function() {
+    
+// run micro benchmarks locally: 
+// lk test -m 'lively.versions.tests.MicroBenchmarks' -f 'MicroBenchmarks|.*|.*'
+    
+TestCase.subclass('lively.versions.tests.MicroBenchmarks.ProxiedConstructorApplication', {
+    test01CreateLotsOfObjectsFromProxiedConstructor: function() {
+        var NewType = lively.versions.ObjectVersioning.proxy(function C() {
+        });
+        
+        for (var i = 0; i < 200000; i++) {
+            // two arguments, because argument application is suspected to be slow
+            new NewType(1, 2);
+        }
+    }
+})
+
+});

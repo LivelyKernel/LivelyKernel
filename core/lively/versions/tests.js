f2c35674ed50ea6413508f1af6b2e4daec8d82fd
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2013-08-21 18:42:00 +0200

    Object Versioning: lively.versions.tests to load tests at once

---
+++ core/lively/versions/tests.js
@@ -0,0 +1,5 @@
+module('lively.versions.tests').requires('lively.versions.tests.ObjectVersioningTests', 'lively.versions.tests.Benchmarks', 'lively.versions.tests.MicroBenchmarks').toRun(function() {
+
+// Enter your code here
+
+}) // end of module

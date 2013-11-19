024f92e0ccaef0a415f1844a8d0330f072d2b32a
Jaqueline Pollak <Jaqueline.pollak@student.hpi.uni-potsdam.de>
2013-11-19 15:09:58 +0100

    start with the barChart

--- core/lively/morphic/Charts.js
+++ core/lively/morphic/Charts.js
@@ -1,11 +1,21 @@
 module('lively.morphic.Charts').requires().toRun(function() {
 
-lively.morphic.Morph.subclass("lively.morphic.Charts.BarChart", {
+lively.morphic.Morph.subclass("lively.morphic.BarChart", {
     initialize: function($super) {
         $super();
-        this.setExtent(pt(100, 100));
+        this.setExtent(pt(500, 500));
         this.setFill(Color.black);
-        
+        data = [4,6,2,7,1];
+        this.draw(data);
+    },
+    
+    draw: function(data){
+        WIDTH = 50; SPACE = 8;
+        for (var i = 0; i < data.length; i++) {
+            rect = new lively.morphic.Morph.makeRectangle((WIDTH+SPACE)*i,0,WIDTH,100*data[i]);
+            rect.setFill(Color.grey);
+            this.addMorph(rect);
+        }
     },
     
     

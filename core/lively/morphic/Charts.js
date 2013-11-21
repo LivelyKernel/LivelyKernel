e1ff9ca9c75c6f1331491f7f9cd87529fe74337a
Philipp Otto <philipp@scm.io>
2013-11-21 17:18:59 +0100

    implement DataFlowComponent with getMorphAbove() functionality

--- core/lively/morphic/Charts.js
+++ core/lively/morphic/Charts.js
@@ -21,4 +21,55 @@ lively.morphic.Morph.subclass("lively.morphic.BarChart", {
     
 } )
 
+lively.morphic.Morph.subclass("lively.morphic.DataFlowComponent", {
+    initialize: function($super) {
+        $super();
+        this.setExtent(pt(100, 100));
+        this.setFill(Color.black);
+        
+        // TODO
+        this.dataInput = null;
+        this.dataOutput = null;
+    },
+    
+    update: function() {
+        var morphAbove = this.getMorphAbove();
+        
+        if (morphAbove) {
+            this.dataInput = morphAbove.dataOutput;
+            this.updateComponent();
+        }
+    },
+    
+    updateComponent: function() {
+        // subclass responsibility
+    },
+    
+    getMorphAbove: function() {
+        var parentMorph = this.owner;
+        
+        var allDFComponents = parentMorph.withAllSubmorphsSelect(function(el) {
+            return el instanceof lively.morphic.DataFlowComponent;
+        });
+        
+        var closestAbove = null;
+        console.log(this);
+        var myPosition = this.getPosition();
+        console.log("test");
+        allDFComponents.forEach(function(el) {
+            if (el == this)
+                return;
+            
+            var elPosition = el.getPosition();
+            
+            if (elPosition.y < myPosition.y)
+                if (closestAbove == null || elPosition.y > closestAbove.getPosition().y)
+                    closestAbove = el;
+        });
+
+        return closestAbove;
+        
+    }
+    
+});
 }) // end of module

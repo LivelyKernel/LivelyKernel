f3ba3782523f44422049d05efd2004e09c565eda
Robert Krahn <robert.krahn@gmail.com>
2013-12-02 16:18:56 -0800

    shell runner: fix shortcuts in input line

--- core/lively/ide/tools/ShellCommandRunner.js
+++ core/lively/ide/tools/ShellCommandRunner.js
@@ -358,11 +358,19 @@ lively.BuildSpec('lively.ide.tools.ShellCommandRunner', {
         switch(sig) {
             case 'Alt-Up': case 'F1': this.get('output').focus(); evt.stop(); return true;
             case 'Alt-Down': case 'F2': this.get('commandLine').focus(); evt.stop(); return true;
-            case 'Control-C': this.killCommand('SIGINT'); evt.stop(); return true;
-            case 'Control-D': this.killCommand('SIGQUIT'); evt.stop(); return true;
-            case 'Esc': this.killCommand('SIGKILL'); evt.stop(); return true;
-            default: return $super(evt);        
         }
+        if (this.isRunning()) {
+            switch(sig) {
+                case 'Control-C': this.killCommand('SIGINT'); evt.stop(); return true;
+                case 'Control-D': this.killCommand('SIGQUIT'); evt.stop(); return true;
+                case 'Esc': this.killCommand('SIGKILL'); evt.stop(); return true;
+            }
+        }
+        return $super(evt);
+    },
+        
+        isRunning: function isRunning() {
+        return this.currentCommand && this.currentCommand.isRunning();
     },
         reset: function reset() {
         this.doNotSerialize = ['currentCommand'];

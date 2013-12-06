12a857e36d2f812d43fc8f3ca5e8a3cfd7950f4b
Robert Krahn <robert.krahn@gmail.com>
2013-12-05 17:24:42 -0800

    adding missing file for lk-test

--- README.md
+++ README.md
@@ -4,23 +4,33 @@ Lively Kernel is a web-based runtime and development environment that makes crea
 
 For feedback, announcements and discussions, please feel invited to subscribe to our [mailing list](http://lively-kernel.org/list/index.html).
 
-This repository is a fork of the Lively Kernel [Webwerkstatt wiki](http://www.lively-kernel.org/repository/webwerkstatt/) at HPI. See this project's [wiki](https://github.com/LivelyKernel/LivelyKernel/wiki/Repository-Purpose) to learn about the motivation and long-term vision for this repository.
-
 Integration status of the master branch using [Travis-CI](http://www.travis-ci.org): [![Build Status](https://secure.travis-ci.org/LivelyKernel/LivelyKernel.png?branch=master)](http://travis-ci.org/LivelyKernel/LivelyKernel)
 
 ## Installation
 
-To use Lively you do not have to install anything. The Lively Kernel 
-[Webwerkstatt wiki](http://lively-kernel.org/webwerkstatt) is an
-online wiki and development environment that can be used by everyone who wants
+To use Lively you do not have to install anything. The [Lively Web environment](http://lively-web.org/) is an
+online wiki and development platform that can be used by everyone who wants
 to experiment and develop with Lively.
 
 If you want to run your own Lively Kernel server or contribute to the core
-development, please see the [Installation Guide](https://github.com/LivelyKernel/LivelyKernel/wiki/Install-Guide).
+development then make sure you have [node.js installed](http://nodejs.org/download/) and follow these steps:
+
+1. Checkout this repository,
+2. install the node dependencies,
+3. start the server:
+
+```sh
+$ git clone -b master https://github.com/LivelyKernel/LivelyKernel
+$ cd LivelyKernel
+$ npm install
+$ npm start
+```
+
+Lively should now be running at [localhost:9001](http://localhost:9001/welcome.html).
 
 ## Running the tests
 
-Please see the [Testing wiki page](https://github.com/LivelyKernel/LivelyKernel/wiki/Testing).
+Start the server then run `$ npm test`.
 
 ## Contributing
 
---
+++ bin/test-config.js
@@ -0,0 +1,66 @@
+/*globals */
+var path = require('path'),
+    env = require('./env');
+
+// see http://peter.sh/experiments/chromium-command-line-switches/
+var chromeTmpDir = path.join(env.TEMP_DIR, 'chrome-for-lively/'),
+    chromeArgs   = [// "--enable-logging",
+                    // "--v=1",
+                    // "--enable-logging=stderr",
+                    "--no-sandbox", // for chrome/ubuntu bug
+                    "--no-process-singleton-dialog",
+                    "--user-data-dir=" + chromeTmpDir,
+                    "--no-first-run",
+                    "--js-flags=--harmony",
+                    "--disable-default-apps",
+                    "--disable-history-quick-provider",
+                    "--disable-history-url-provider",
+                    "--disable-breakpad",
+                    "--disable-restore-session-state",
+                    "--disable-restore-background-contents",
+                    "--disable-tab-closeable-state-watcher",
+                    "--disable-background-mode",
+                    "--disable-background-networking",
+                    "--disable-preconnect",
+                    "--disabled"],
+    firefoxArgs =  [];
+
+var Config = {
+    platformConfigs: {
+        "darwin": {
+            "chrome": {
+                path: env.CHROME_BIN,
+                args: chromeArgs,
+                tmpDir: chromeTmpDir
+            },
+            "firefox": {
+                path: env.FIREFOX_BIN,
+                args: firefoxArgs
+            }
+        },
+        "linux": {
+            "chrome": {
+                path: env.CHROME_BIN,
+                args: chromeArgs,
+                tmpDir: chromeTmpDir
+            },
+            "firefox": {
+                path: env.FIREFOX_BIN,
+                args: firefoxArgs
+            }
+        },
+	"win32": {
+	    "chrome": {
+		path: env.CHROME_BIN,
+		args: chromeArgs,
+		tmpDir: chromeTmpDir
+	    },
+            "firefox": {
+                path: env.FIREFOX_BIN,
+                args: firefoxArgs
+            }
+	}
+    }
+};
+
+module.exports = Config;

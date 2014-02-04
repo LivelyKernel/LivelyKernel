6094a764cf7e241d6efea7070c0861a88d070621
Robert Krahn <robert.krahn@gmail.com>
2014-02-03 17:15:26 -0800

    bootstrap: more verbose partsbin download

--- bin/helper/download-partsbin.js
+++ bin/helper/download-partsbin.js
@@ -1,16 +1,16 @@
-var http = require("http"),
-    fs = require("fs"),
-    path = require("path"),
-    util = require("util"),
-    async = require("async"),
-    exec = require("child_process").exec,
+var http    = require("http"),
+    fs      = require("fs"),
+    path    = require("path"),
+    util    = require("util"),
+    async   = require("async"),
+    exec    = require("child_process").exec,
     shelljs = require('shelljs');
 
-var lkDir = process.env.WORKSPACE_LK;
-var partsBinCopyURL = "http://lively-web.org/nodejs/PartsBinCopy/";
-var zipFile = path.join(lkDir, 'PartsBinCopy.zip');
-var partsBinFolder = path.join(lkDir, 'PartsBin')
-var partsBinExtractionFolder = path.join(lkDir, 'PartsBin_extracted')
+var lkDir = process.env.WORKSPACE_LK,
+    partsBinCopyURL = "http://lively-web.org/nodejs/PartsBinCopy/",
+    zipFile = path.join(lkDir, 'PartsBinCopy.zip'),
+    partsBinFolder = path.join(lkDir, 'PartsBin'),
+    partsBinExtractionFolder = path.join(lkDir, 'PartsBin_extracted');
 
 function partsBinExists() {
     return fs.existsSync(partsBinFolder);
@@ -18,33 +18,38 @@ function partsBinExists() {
 
 function downloadPartsBin(next) {
     http.get(partsBinCopyURL, function(res) {
-        console.log('Please wait, downloading PartsBin...');
         var writeS = fs.createWriteStream(zipFile);
         res.pipe(writeS);
         writeS.on('close', function() { next(null); });
-        writeS.on('error', function(error) { next(error); });
+        writeS.on('error', function(e) { console.error(e); next(e); });
     }).on('error', function(e) { next(e); });
 }
 
 function extractPartsBin(next) {
     var unzipDir = partsBinExtractionFolder;
-    var proc = exec(util.format("unzip -q %s -d %s", zipFile, unzipDir),
+    var proc = exec(util.format("unzip -o -q %s -d %s", zipFile, unzipDir),
         {cwd: lkDir, stdio: 'inherit'},
         function(code) { console.log('PartsBin extracted'); next(code); })
     proc.stdout.pipe(process.stdout);
+    proc.stderr.pipe(process.stdout);
 }
 
 function movePartsBinExtracted(next) {
-    console.log('moving partsbin into place');
     shelljs.mv(path.join(partsBinExtractionFolder, 'PartsBin'), partsBinFolder);
     next();
 }
 
 function cleanup(next) {
-    console.log('cleaning up PartsBin download files');
     shelljs.rm('-f', zipFile);
     shelljs.rm('-rf', partsBinExtractionFolder);
     next();
+}
+
+function msg(/*args*/) {
+    return function(next) {
+        console.log.apply(console, arguments);
+        next();
+    }
 }
 
 function downloadPartsBinIfNecessary(thenDo) {
@@ -53,10 +58,14 @@ function downloadPartsBinIfNecessary(thenDo) {
     console.log('...' + (exists ? 'yes' : 'no'));
     if (exists) { thenDo && thenDo(null); return; }
     async.series([
+        msg('Please wait, downloading PartsBin...'),
         downloadPartsBin,
+        msg('Extracting PartsBin...'),
         extractPartsBin,
         movePartsBinExtracted,
-        cleanup
+        msg('Cleaning up PartsBin download files...'),
+        cleanup,
+        msg('PartsBin installed into %s', partsBinFolder)
     ], function(err) {
         err && console.error(err);
         thenDo && thenDo(err);

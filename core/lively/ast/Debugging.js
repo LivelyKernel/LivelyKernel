02110675627030e3f52504dcc3329c798f84295c
Marko Röder <m.roeder@photon-software.de>
2014-02-28 12:54:09 +0100

    debugging: Preparing new Debugging module (to replace lively.ast.Morphic)

--- core/lively/ast/Debugging.js
+++ core/lively/ast/Debugging.js
@@ -1,4 +1,21 @@
-module('lively.ast.Debugging').requires('lively.ast.Rewriting').toRun(function() {
+module('lively.ast.Debugging').requires('lively.ast.Rewriting', 'lively.ide.tools.Debugger').toRun(function() {
+
+Object.extend(lively.ast, {
+
+    halt: function(frame) {
+        (function() {
+            lively.ast.openDebugger(frame, "Debugger");
+        }).delay(0);
+        return true;
+    },
+
+    openDebugger: function(frame, title) {
+        var m = lively.BuildSpec("lively.ide.tools.Debugger").createMorph();
+        m.targetMorph.setTopFrame(frame);
+        if (title) m.setTitle(title);
+        m.openInWorldCenter().comeForward();
+    }
+});
 
 cop.create('DebugGlobalErrorHandlerLayer')
 .beGlobal()

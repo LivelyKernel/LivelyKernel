77c229e770079d5546093e62a6635f11b3155d51
Robert Krahn <robert.krahn@gmail.com>
2014-04-24 01:27:46 +0000

    sqlite support: lively.store.SQLiteInterface

--- core/lively/ide/codeeditor/modes/SQL.js
+++ core/lively/ide/codeeditor/modes/SQL.js
@@ -1,55 +1,58 @@
-module('lively.ide.codeeditor.modes.SQL').requires('lively.ide.codeeditor.ace').toRun(function() {
+module('lively.ide.codeeditor.modes.SQL').requires('lively.ide.codeeditor.ace', 'lively.store.SQLiteInterface').toRun(function() {
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // SQL-mode, for the server database
 lively.ide.ace.require('ace/mode/sql').Mode.addMethods({
+
+    commands: {
+        changeDBAccessor: {
+            exec: function(ed) {
+                var mode = lively.ide.ace.require('ace/mode/sql').Mode;
+                var m = ed.$morph, defaultAccessor = mode.prototype.defaultDBAccessorCode;
+                $world.editPrompt('customize the DB access function', function(input) {
+                    if (!input) { show('aborting'); return; }
+                    m.sqlDBAccessorCode = String(input);
+                }, m.sqlDBAccessorCode || defaultAccessor)
+            }
+        },
+    },
+
+    attach: function(ed) {
+        ed.commands.addCommands(this.commands);
+    },
+
+    detach: function(ed) {
+        ed.commands.removeCommands(this.commands);
+    },
+
+    defaultDBAccessorCode: "lively.repository.fs.storage.db",
+
+    morphMenuItems: function(items, editor) {
+        var mode = this;
+        items.push(['SQL', [["change DB accessor", function() {
+            mode.commands.changeDBAccessor.exec(editor);
+        }]]]);
+        return items;
+    },
+
     doEval: function(codeEditor, insertResult) {
         var doQuery = !!insertResult,
             sourceString = codeEditor.getSelectionOrLineString();
-        function sendToServer(code, thenDo) {
-            var server = new WebResource(Config.nodeJSURL+'/NodeJSEvalServer/async');
-            var data = JSON.stringify({callback: 'sendSQLResult', code: code, timeout: 20*1000});
-            return server.beAsync().post(data, 'application/json')
-            .whenDone(function(content, status) {
-                // if (!status.isSuccess()) { show(status); return; }
-                var result = content;
-                try {
-                    result = JSON.parse(content);
-                    result = doQuery ? Grid.tableFromObjects(result) : result;
-                } catch (e) {}
-                thenDo(status.isSuccess() ? null : result || status, result);
-            });
-        }
-        // is is a query or a modify statement?
-        var template = doQuery ?
-            function queryTemplate() {
-                lively.repository.fs.storage.db.all("__STMT__", function(err, rows) {
-                    try {
-                        sendSQLResult(err ? String(err) : JSON.stringify(rows));
-                    } catch (e) {
-                        console.warn('SQL query from browser. Timed out?: %s', e);
-                    }
-                });
-            } : 
-            function runTemplate() {
-                lively.repository.fs.storage.db.run("__STMT__", function(err, rows) {
-                    try {
-                        sendSQLResult(err ? String(err) : JSON.stringify({lastID: this.lastID, change: this.changes}));
-                    } catch (e) {
-                        console.warn('SQL query from browser. Timed out?: %s', e);
-                    }
-                });
+        lively.store.SQLiteInterface._sendToServer(
+            {
+                dbAccessor: codeEditor.sqlDBAccessorCode || this.defaultDBAccessorCode,
+                statements: [{action: doQuery ? 'all' : 'run', sql: sourceString}]
             },
-            sql = Strings.format('(%s)();', template).replace(/__STMT__/, sourceString.replace(/\n/g, '\\n'));
-        sendToServer(sql, function(err, result) {
-            if (doQuery) {
-                var objToPrint = Object.isArray(result) ? Strings.printTable(result) : Objects.inspect(result);
-                codeEditor.printObject(codeEditor.aceEditor, objToPrint);
-            } else {
-                if (err) alert(Objects.inspect(err))
-                else alertOK(Objects.inspect(result));
-            }
-        })
+            function(err, result) {
+                if (doQuery) {
+                    
+                    var objToPrint = Object.isArray(result) ? result.map(Strings.printTable) : Objects.inspect(result);
+                    codeEditor.printObject(codeEditor.aceEditor, objToPrint);
+                } else {
+                    if (err) alert(Objects.inspect(err))
+                    else alertOK(Objects.inspect(result));
+                }
+            });
     }
 });
 
---
+++ core/lively/store/SQLiteInterface.js
@@ -0,0 +1,60 @@
+module('lively.store.SQLiteInterface').requires('lively.Network').toRun(function() {
+
+Object.extend(lively.store.SQLiteInterface, {
+
+    ensureDB: function(name, thenDo) {
+        URL.nodejsBase.withFilename('SQLiteServer/ensureDB').asWebResource()
+            .beAsync().post(JSON.stringify({ dbAccessor: name }), 'application/json')
+            .whenDone(function(content, status) {
+                thenDo(status.isSuccess() ? null : content || status, content);
+            });
+    },
+
+    removeDB: function(name, thenDo) {
+        URL.nodejsBase.withFilename('SQLiteServer/removeDB').asWebResource()
+            .beAsync().post(JSON.stringify({dbAccessor: name}), 'application/json')
+            .whenDone(function(content, status) {
+                thenDo(status.isSuccess() ? null : content || status, content);
+            });
+    },
+
+    _sendToServer: function(data, thenDo) {
+        URL.nodejsBase.withFilename('SQLiteServer/').asWebResource()
+            .beAsync().post(JSON.stringify(data), 'application/json')
+            .whenDone(function(content, status) {
+                var result = content;
+                try {
+                    result = JSON.parse(content);
+                    result = Object.isArray(result) ? result.map(Grid.tableFromObjects) : result;
+                } catch (e) {}
+                thenDo(status.isSuccess() ? null : result || status, result);
+            });
+    },
+
+    query: function(dbAccessorCode, sqlStatements, thenDo) {
+        this._sendToServer({
+            statements: sqlStatements.map(function(ea) { return {action: 'all', sql: ea}; }),
+            dbAccessor: dbAccessorCode
+        }, thenDo);
+    },
+
+    run: function(dbAccessorCode, sqlStatements, thenDo) {
+        this._sendToServer({
+            statements: sqlStatements.map(function(ea) { return {action: 'run', sql: ea}; }),
+            dbAccessor: dbAccessorCode
+        }, thenDo);
+    },
+
+    insert: function(dbAccessorCode, statementsAndContent, thenDo) {
+        // statementsAndContent = [{sql: STRING, args: ARRAY of content insert}]
+        this._sendToServer({
+            statements: statementsAndContent.map(function(ea) {
+                return Object.extend({action: 'run'}, ea);
+            }),
+            dbAccessor: dbAccessorCode
+        }, thenDo);
+    }
+    
+});
+
+}) // end of module

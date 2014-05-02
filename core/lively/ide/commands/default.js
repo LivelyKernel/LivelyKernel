12c30482b194388d6b315240b4705d4b956487e9
Robert Krahn <robert.krahn@gmail.com>
2014-05-02 04:13:01 +0000

    ide: refined lively.ide.openSubserverViewer command to allow browsing subservers programmatically

--- core/lively/ide/commands/default.js
+++ core/lively/ide/commands/default.js
@@ -803,7 +803,19 @@ Object.extend(lively.ide.commands.byName, {
     'lively.ide.openTextEditor': {description: 'open TextEditor', isActive: lively.ide.commands.helper.noCodeEditorActive, exec: function() { lively.ide.openFile(URL.source.toString()); return true; }},
     'lively.ide.openSystemConsole': {description: 'open SystemConsole', isActive: lively.ide.commands.helper.noCodeEditorActive, exec: function() { $world.openSystemConsole(); return true; }},
     'lively.ide.openOMetaWorkspace': {description: 'open OMetaWorkspace', isActive: lively.ide.commands.helper.noCodeEditorActive, exec: function() { $world.openOMetaWorkspace(); return true; }},
-    'lively.ide.openSubserverViewer': {description: 'open SubserverViewer', isActive: lively.ide.commands.helper.noCodeEditorActive, exec: function() { $world.openSubserverViewer(); return true; }},
+    'lively.ide.openSubserverViewer': {
+        description: 'open SubserverViewer',
+        exec: function(options) {
+            options = options || {};
+            var serverName = options.serverName,
+                line = options.line;
+            $world.openSubserverViewer(function(err, subserver) {
+                serverName && subserver.targetMorph.selectServer(serverName);
+                line && subserver.get("ServerSourceCode").selectAndCenterLine(line);
+            });
+            return true;
+        }
+    },
     'lively.ide.openServerWorkspace': {description: 'open ServerWorkspace', isActive: lively.ide.commands.helper.noCodeEditorActive, exec: function() { $world.openServerWorkspace(); return true; }},
     'lively.ide.openLively2LivelyWorkspace': {description: 'open Lively2LivelyWorkspace', isActive: lively.ide.commands.helper.noCodeEditorActive, exec: function() { lively.require('lively.net.tools.Lively2Lively').toRun(function() { lively.BuildSpec("lively.net.tools.Lively2LivelyWorkspace").createMorph().openInWorldCenter().comeForward(); }); return true; }},
     'lively.ide.openShellWorkspace': {description: 'open ShellWorkspace', isActive: lively.ide.commands.helper.noCodeEditorActive, exec: function() { var codeEditor = $world.addCodeEditor({textMode: 'sh', theme: 'pastel_on_dark', title: 'Shell Workspace', content: "# You can evaluate shell commands in here\nls $PWD"}).getWindow().comeForward(); return true; }},

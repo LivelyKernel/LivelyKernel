01243ecb04a4c542a8f44709756b6b9971ad08b5
Marko Röder <m.roeder@photon-software.de>
2014-06-06 22:44:45 +0200

    debugging: Break in try-block in interpreter should not execute catch block

--- core/lively/ast/AcornInterpreter.js
+++ core/lively/ast/AcornInterpreter.js
@@ -480,6 +480,8 @@ Object.subclass('lively.ast.AcornInterpreter.Interpreter',
         try {
             this.accept(node.block, state);
         } catch (e) {
+            if (lively.Config.get('loadRewrittenCode') && e.unwindException && e.toString() == 'Break')
+                throw e.unwindException;
             hasError = true;
             state.error = err = e;
         }

c516147b36408dd3066cfd1721e460d1f063bd43
Robin Schreiber <robin.schreiber@me.com>
2014-06-16 17:11:10 -0700

    add Entanglements.js

---
+++ core/lively/persistence/Entanglement.js
@@ -0,0 +1,5 @@
+module('lively.persistence.Entanglement').requires('lively.persistence.BuildSpec', 'lively.persistence.Serializer', 'lively.morphic.Core').toRun(function() {
+
+// Enter your code here
+
+}) // end of module

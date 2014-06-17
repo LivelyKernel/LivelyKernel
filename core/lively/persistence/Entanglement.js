e97e53af5a7b8f0f39015ab37bd928e13062b63f
Robin Schreiber <robin.schreiber@me.com>
2014-06-16 17:16:10 -0700

    add Entanglement initializer

--- core/lively/persistence/Entanglement.js
+++ core/lively/persistence/Entanglement.js
@@ -1,5 +1,11 @@
 module('lively.persistence.Entanglement').requires('lively.persistence.BuildSpec', 'lively.persistence.Serializer', 'lively.morphic.Core').toRun(function() {
-
-// Enter your code here
-
+Object.subclass("lively.persistence.Entanglement.Morph", 
+    "initializing", {
+        initialize: function() {
+            this.isEntanglement = true;
+            this.entangledAttributes = {};
+            this.subEntanglements = [];
+            this.entangledMorphs = [];
+        }
+    })
 }) // end of module

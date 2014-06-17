04a86312425528a43e549d47873e004925df4eb3
Robin Schreiber <robin.schreiber@me.com>
2014-06-16 17:35:31 -0700

    add entangling methods to Entanglements

--- core/lively/persistence/Entanglement.js
+++ core/lively/persistence/Entanglement.js
@@ -109,7 +109,141 @@ Object.subclass("lively.persistence.Entanglement.Morph",
                 propName = propName.replace('set', '_');
             return propName in props;
         }
-});
+},
+    "entangling", {
+        entangleWith: function(morph, options) {
+            var self = this;
+            // make sure the classes are the same
+            if(!this.entangledAttributes.className === morph.constructor) 
+                throw TypeError("Can not entangle to a different kind of Morph!");
+            
+            this.entangledMorphs.push(morph);
+            var props = Object.mergePropertyInHierarchy(morph, "buildSpecProperties");
+            
+            var excludes = function(key) {
+                if(key.startsWith('_')){
+                    // we also exlude when the user supplies us
+                    // only with the corresponding setter
+                    return options.include(key) || 
+                           options.include('set' + key.replace(/^_/, '').capitalize())
+                }
+                return options.include(key)
+            }
+            
+            morph.getBuildSpecProperties(props).forEach(function(key) {
+                // if this property is excluded or a subentanglement, skip
+                if(options && excludes(key)) {
+                    return;
+                }
+                
+                if(props[key])
+                    var getter = props[key].getter || morph['get' + key.replace(/^_/, '').capitalize()]
+                if(!key.startsWith('_') || !getter) {
+                    self.entanglePublicProperty(morph, key);
+                } else {
+                    self.entanglePrivateProperty(morph, key, getter);
+                }
+            });
+    },
+    createEntangledMorph: function(options) {
+        // we first augment out buildSpec, by wrapping each
+        // subbuildspec.createMorph with an entangleWith call
+        // that also makes sure that the correct exclusions are applied
+        // we could also: 1 -> wrap all buildSpec objects inside Entanglement Objects.
+        // Or just 2 -> completely do the creation of morphs by ourselves...
+        options = options || []
+        options = options.excludes || options; // is this really necessary..? clarity maybe
+        this.augmentBuildSpec(options);
+        var morph = this.baseSpec.createMorph();
+        return morph;
+    },
+    entanglePrivateProperty: function(morph, key, getter) {
+        // the mapping has been modified by the buildspec (_Attributes)
+        // find the corresponding setter in the instance
+        var setter = 'set' + key.replace(/^_/, '').capitalize();
+
+        if(!this.entangledAttributes[key]) {
+            //also make sure to use the getter method here
+            getter = getter.bind(morph);
+            this.entangledAttributes[key] = getter(morph);
+        } else {
+            //force the values in the entanglement onto the morph
+            morph[setter]((this.entangledAttributes[key]));
+        }
+        // FIXME: setter arguments not caught by connection
+        // HACK: return value of getter inside converter
+        connect(morph, setter, this, 'set', 
+                    {updater: function ($proceed, newValue, oldValue) {
+                                this.targetObj.currentUpdater = this.sourceObj;
+                                var res = $proceed([key, this.sourceObj[setter.replace('set', 'get')]()], oldValue);
+                                this.targetObj.currentUpdater = null;
+                                return res;
+                                }, 
+                     varMapping: {key: key, setter: setter}});
+        connect(this.entangledAttributes, key, morph, setter, 
+                    {updater: function($proceed, newValue, oldValue) {
+                        var res;
+                        if (this.sourceObj.currentUpdater != this.targetObj) {
+                            // to make sure that the updated does not cause a backfire,
+                            // temporarily disable the connection pointing back
+                            var reverseConn = this.targetObj.attributeConnections.find(
+                                function(conn) {
+                                    return conn.targetObj === self && conn.sourceAttrName === setter; 
+                                });
+                            lively.bindings.noUpdate({sourceObj: reverseConn && reverseConn.sourceObj, 
+                                                      sourceAttribute: reverseConn && reverseConn.sourceAttrName }, 
+                                                      function() {res = $proceed(newValue, oldValue);});
+                        }
+                        return res;
+                    }, varMapping: {setter: setter, self: this}});
+    },
+    disconnectMorph: function(morph) {
+        morph.attributeConnections && morph.attributeConnections.forEach(function(conn) {
+           conn.disconnect(); 
+        });
+        this.entangledAttributes.attributeConnections 
+        && this.entangledAttributes.attributeConnections.forEach(
+            function(conn) { if(conn.targetObj == morph) conn.disconnect(); });
+    },
+    clearAllEntanglements: function() {
+        var self = this;
+        var toRemove = this.entangledMorphs.pop();
+        while(toRemove) {
+            this.disconnectMorph(toRemove);
+            toRemove = this.entangledMorphs.pop();
+        }
+    },
+    entanglePublicProperty: function(morph, key) {
+        if (morph[key] && Object.isFunction(morph[key]))
+            return;
+        if(!this.entangledAttributes[key])
+            this.entangledAttributes[key] = morph[key]; //default value has already been set
+        // connect changes on morph to entanglement
+        connect(morph, key, this, 'set', 
+                {updater: function($proceed, newValue, oldValue) { 
+                    window.currentUpdater = this.sourceObj;
+                    var res = $proceed([key, newValue], oldValue); 
+                    window.currentUpdater = null;
+                    return res;
+                }, varMapping: {key: key }});
+        // connect changes received in entanglement to entangled morphs
+        connect(this.entangledAttributes, key, morph, key,
+                {updater: function($proceed, newValue, oldValue) {
+                    if (window.currentUpdater != this.targetObj) {
+                        // to make sure that the updated does not cause a backfire,
+                        // temporarily disable the connection pointing back
+                        var reverseConn = this.targetObj.attributeConnections.find(
+                            function(conn) {
+                                return conn.targetObj === self && conn.sourceAttrName === key; 
+                            });
+                        lively.bindings.noUpdate({sourceObj: reverseConn.sourceObj, 
+                                                  sourceAttribute: reverseConn.sourceAttrName },
+                            function() {$proceed(newValue, oldValue);});
+                        }
+                    }, 
+                varMapping: {key: key, self: this}});
+    }
+    });
 
 Object.extend(lively.persistence.Entanglement.Morph, {
     fromMorph: function(morph) { return new this().fromMorph(morph); },

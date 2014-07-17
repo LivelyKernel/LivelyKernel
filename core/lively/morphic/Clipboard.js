5b03a726ef435284a5600faac3835a158cc6f816
Robert Krahn <robert.krahn@gmail.com>
2014-07-17 02:54:09 +0000

    fixing cmd-v (paste) of morphs in safari

--- core/lively/morphic/Clipboard.js
+++ core/lively/morphic/Clipboard.js
@@ -16,7 +16,7 @@ Object.extend(lively.morphic.Clipboard, {
 
     handleKeyPaste: function(evt, data, withExtractedMorphsDo) {
         try {
-            if (data.types.any(function(type) { return type.toLowerCase() === 'files'})) {
+            if (Array.from(data.types).any(function(type) { return type.toLowerCase() === 'files'})) {
                 evt.getPosition = function() { return $world.firstHand().getPosition(); };
                 var items = Array.from(data.items);
                 lively.data.FileUpload.handleDroppedFiles(items.invoke('getAsFile'), evt);

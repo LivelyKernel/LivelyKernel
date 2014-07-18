f943e3e78666d1783bf17370f6c19507f265555d
Marko Röder <m.roeder@photon-software.de>
2014-07-18 11:11:39 -0700

    debugging: Adding change indicator Permitting to continue execution except for when changes were save

--- core/lively/ide/tools/Debugger.js
+++ core/lively/ide/tools/Debugger.js
@@ -233,163 +233,211 @@ lively.BuildSpec('lively.ide.tools.Debugger', {
             name: 'FrameInfo',
             sourceModule: "lively.morphic.Core",
             submorphs: [{
-                _AutocompletionEnabled: true,
                 _BorderColor: Color.rgb(230,230,230),
-                _BorderRadius: 0,
                 _BorderWidth: 1,
                 _Extent: lively.pt(479.0,302.0),
-                _FontSize: 12,
-                _LineWrapping: true,
-                _Position: lively.pt(1.0,1.1),
-                _ShowActiveLine: false,
-                _ShowErrors: false,
-                _ShowGutter: true,
-                _ShowIndents: true,
-                _ShowInvisibles: false,
-                _ShowPrintMargin: false,
-                _ShowWarnings: true,
-                _SoftTabs: true,
-                _StyleSheet: ".ace_debugging {\n\
-                	position: absolute;\n\
-                	background: #ff9999;\n\
-                	border: 1px rgb(200, 200, 250);\n\
-                }",
-                _Theme: lively.Config.get("aceWorkspaceTheme"),
-                _aceInitialized: true,
-                accessibleInInactiveWindow: true,
-                allowInput: true,
-                className: "lively.morphic.CodeEditor",
-                doNotSerialize: ["debugMarker"],
-                evalEnabled: false,
-                fixedHeight: true,
-                name: "FrameSource",
-                lastSaveSource: '',
+                _Fill: Color.rgb(242,242,242),
+                _Position: lively.pt(4.7,145.7),
+                className: "lively.morphic.Box",
+                droppingEnabled: true,
                 layout: {
-                    resizeWidth: true,
-                    resizeHeight: true
+                    adjustForNewBounds: true,
+                    borderSize: 0,
+                    resizeHeight: true,
+                    resizeWidth: true
                 },
-                sourceModule: "lively.ide.CodeEditor",
-                textMode: "javascript",
-                boundEval: function boundEval(__evalStatement) {
-                var frame = this.get("Debugger").currentFrame;
-                if (!frame) return;
+                sourceModule: "lively.morphic.Core",
+                submorphs: [{
+                    _AutocompletionEnabled: true,
+                    _BorderColor: Color.rgb(230,230,230),
+                    _BorderRadius: 0,
+                    _BorderWidth: 0,
+                    _Extent: lively.pt(477,300.0),
+                    _FontSize: 12,
+                    _LineWrapping: true,
+                    _Position: lively.pt(1.0,1.1),
+                    _ShowActiveLine: false,
+                    _ShowErrors: false,
+                    _ShowGutter: true,
+                    _ShowIndents: true,
+                    _ShowInvisibles: false,
+                    _ShowPrintMargin: false,
+                    _ShowWarnings: true,
+                    _SoftTabs: true,
+                    _StyleSheet: ".ace_debugging {\n\
+                    	position: absolute;\n\
+                    	background: #ff9999;\n\
+                    	border: 1px rgb(200, 200, 250);\n\
+                    }",
+                    _Theme: lively.Config.get("aceWorkspaceTheme"),
+                    _aceInitialized: true,
+                    accessibleInInactiveWindow: true,
+                    allowInput: true,
+                    className: "lively.morphic.CodeEditor",
+                    doNotSerialize: ["debugMarker"],
+                    evalEnabled: false,
+                    fixedHeight: true,
+                    name: "FrameSource",
+                    lastSaveSource: '',
+                    layout: {
+                        adjustForNewBounds: true,
+                        resizeWidth: true,
+                        resizeHeight: true
+                    },
+                    sourceModule: "lively.ide.CodeEditor",
+                    submorphs: [],
+                    textMode: "javascript",
+                    boundEval: function boundEval(__evalStatement) {
+                    var frame = this.get("Debugger").currentFrame;
+                    if (!frame) return;
 
-                var interpreter = new lively.ast.AcornInterpreter.Interpreter(),
-                    str, ast,
-                    pc = frame.getPC();
-                frame.setPC(null);
-                try {
-                    ast = lively.ast.acorn.parse(str = '(' + __evalStatement + ')');
-                    acorn.walk.addAstIndex(ast);
-                    acorn.walk.addSource(ast, str);
-                    return interpreter.runWithFrame(ast, frame);
-                } catch (e) {
-                    ast = lively.ast.acorn.parse(str = __evalStatement);
-                    acorn.walk.addAstIndex(ast);
-                    acorn.walk.addSource(ast, str);
-                    // In case str starts with a comment, set str to program node
-                    ast.source = str;
-                    return interpreter.runWithFrame(ast, frame);
-                } finally {
-                    frame.setPC(pc);
-                    this.get("FrameScope").updateList(frame);
-                }
-            },
-                doSave: function doSave() {
-                // part's script
-                var frame = this.get("Debugger").currentFrame,
-                    thisObject = frame.getThis(),
-                    source = this.getTextString(),
-                    saved;
-                if (thisObject && thisObject.isMorph && frame.getOriginalAst().type != 'Program') {
-                    saved = this.tryBoundEval('this.addScript(' + this.textString + ');');
-
-                    if (!saved || saved instanceof Error) {
-                        var msg = saved.message || "not saved";
-                        this.setStatusMessage(msg, Color.red);
-                        return;
+                    var interpreter = new lively.ast.AcornInterpreter.Interpreter(),
+                        str, ast,
+                        pc = frame.getPC();
+                    frame.setPC(null);
+                    try {
+                        ast = lively.ast.acorn.parse(str = '(' + __evalStatement + ')');
+                        acorn.walk.addAstIndex(ast);
+                        acorn.walk.addSource(ast, str);
+                        return interpreter.runWithFrame(ast, frame);
+                    } catch (e) {
+                        ast = lively.ast.acorn.parse(str = __evalStatement);
+                        acorn.walk.addAstIndex(ast);
+                        acorn.walk.addSource(ast, str);
+                        // In case str starts with a comment, set str to program node
+                        ast.source = str;
+                        return interpreter.runWithFrame(ast, frame);
+                    } finally {
+                        frame.setPC(pc);
+                        this.get("FrameScope").updateList(frame);
                     }
-                    this.lastSaveSource = source;
+                },
+                    doSave: function doSave() {
+                    // part's script
+                    var frame = this.get("Debugger").currentFrame,
+                        thisObject = frame.getThis(),
+                        source = this.getTextString(),
+                        saved;
+                    if (thisObject && thisObject.isMorph && frame.getOriginalAst().type != 'Program') {
+                        saved = this.tryBoundEval('this.addScript(' + this.textString + ');');
 
-                    // FIXME: method name might have changed! + do not use internals
-                    var func = frame.func,
-                        scriptName = func.methodName || func.name();
-                    func.node = thisObject[scriptName]._cachedAst;
-                    func.source = source;
+                        if (!saved || saved instanceof Error) {
+                            var msg = saved.message || "not saved";
+                            this.setStatusMessage(msg, Color.red);
+                            return;
+                        }
+                        this.savedTextString = source;
 
-                    alertOK('Saved ' + scriptName + '!');
-                } else
-                    alert('Not saved: Saving not yet supported!');
-            },
-                connectionRebuilder: function connectionRebuilder() {
-                lively.bindings.connect(this, "textString", this, "onTextChange", {});
-            },
-                debugSelection: function debugSelection() {
-              var frame = this.get("Debugger").currentFrame;
-              if (!frame) return;
-              var str = "function(){\n" + this.getSelectionOrLineString() + "\n}";
-              var fun = Function.fromString(str).forInterpretation();
-              fun.lexicalScope = frame;
-              try {
-                return fun.apply(frame.getThis(), [], {breakAtCalls:true});
-              } catch(e) {
-                if (e.isUnwindException) {
-                  lively.ast.openDebugger(e.topFrame);
-                } else {
-                  this.showError(e);
-                }
-              } finally {
-                this.get("FrameScope").updateList(
-                    frame.listItemsForIntrospection()
-                );
-              }
-            },
-                highlightPC: function highlightPC() {
-                var frame = this.get("Debugger").currentFrame;
-                if (frame && frame.pc !== null) {
-                    var context = frame.func.getAst(),
-                        start = frame.pc.start,
-                        end = frame.pc.end;
-                    if (context.type != 'Program') {
-                        start -= context.start;
-                        end -= context.start;
+                        // FIXME: method name might have changed! + do not use internals
+                        var func = frame.func,
+                            scriptName = func.methodName || func.name();
+                        func.node = thisObject[scriptName]._cachedAst;
+                        func.source = source;
+
+                        // disable PC-related actions
+                        this.get("ContinueButton").setActive(false);
+                        this.get("StepIntoButton").setActive(false);
+                        this.get("StepOverButton").setActive(false);
+
+                        alertOK('Saved ' + scriptName + '!');
+                    } else
+                        alert('Not saved: Saving not yet supported!');
+                },
+                    connectionRebuilder: function connectionRebuilder() {
+                    lively.bindings.connect(this, "textString", this, "onTextChange", {});
+                },
+                    debugSelection: function debugSelection() {
+                  var frame = this.get("Debugger").currentFrame;
+                  if (!frame) return;
+                  var str = "function(){\n" + this.getSelectionOrLineString() + "\n}";
+                  var fun = Function.fromString(str).forInterpretation();
+                  fun.lexicalScope = frame;
+                  try {
+                    return fun.apply(frame.getThis(), [], {breakAtCalls:true});
+                  } catch(e) {
+                    if (e.isUnwindException) {
+                      lively.ast.openDebugger(e.topFrame);
+                    } else {
+                      this.showError(e);
                     }
-                    this.addMarker(start, end, 'ace_debugging', 'text', false, function callback(marker) {
+                  } finally {
+                    this.get("FrameScope").updateList(
+                        frame.listItemsForIntrospection()
+                    );
+                  }
+                },
+                    highlightPC: function highlightPC() {
+                    var frame = this.get("Debugger").currentFrame;
+                    if (frame && frame.pc !== null) {
+                        var context = frame.func.getAst(),
+                            start = frame.pc.start,
+                            end = frame.pc.end;
+                        if (context.type != 'Program') {
+                            start -= context.start;
+                            end -= context.start;
+                        }
+                        this.addMarker(start, end, 'ace_debugging', 'text', false, function callback(marker) {
+                            this.removeDebugMarker();
+                            this.debugMarker = marker;
+                        });
+                        this.withAceDo(function(ed) {
+                            var lineStart = ed.session.getDocument().indexToPosition(start).row,
+                                lineEnd = ed.session.getDocument().indexToPosition(end).row;
+                            ed.scrollToLine(Math.floor((lineStart + lineEnd) / 2), true, false);
+                        });
+                    }
+                },
+                    showSource: function showSource(frame) {
+                    var source = (frame && frame.func) ? frame.func.getSource() : '';
+                    this.savedTextString = source;
+                    this.textString = source;
+                },
+                    onTextChange: function onTextChange(newText) {
+                    this.get('ChangeIndicator').indicateUnsavedChanges();
+                    if (newText != this.savedTextString)
                         this.removeDebugMarker();
-                        this.debugMarker = marker;
-                    });
-                    this.withAceDo(function(ed) {
-                        var lineStart = ed.session.getDocument().indexToPosition(start).row,
-                            lineEnd = ed.session.getDocument().indexToPosition(end).row;
-                        ed.scrollToLine(Math.floor((lineStart + lineEnd) / 2), true, false);
-                    });
+                },
+                    removeDebugMarker: function removeDebugMarker() {
+                    if (this.debugMarker !== undefined) {
+                        this.removeMarker(this.debugMarker);
+                        delete this.debugMarker;
+                    }
+                },
+                    reset: function reset() {
+                    this.doNotSerialize = ["debugMarker"];
                 }
-            },
-                showSource: function showSource(frame) {
-                var source = (frame && frame.func) ? frame.func.getSource() : '';
-                this.lastSaveSource = source;
-                this.textString = source;
-            },
-                onTextChange: function onTextChange(newText) {
-                if (newText == this.lastSaveSource) return;
-
-                this.removeDebugMarker();
-
-                // disable PC-related actions
-                this.get("ContinueButton").setActive(false);
-                this.get("StepIntoButton").setActive(false);
-                this.get("StepOverButton").setActive(false);
-            },
-                removeDebugMarker: function removeDebugMarker() {
-                if (this.debugMarker !== undefined) {
-                    this.removeMarker(this.debugMarker);
-                    delete this.debugMarker;
+                },{
+                    _BorderColor: Color.green,
+                    _BorderWidth: 1,
+                    _Extent: lively.pt(8,8),
+                    _Fill: Color.green,
+                    _Position: lively.pt(470.0,1.0),
+                    alarmColor: Color.red,
+                    className: "lively.morphic.Box",
+                    droppingEnabled: false,
+                    grabbingEnabled: false,
+                    draggingEnabled: false,
+                    isLayoutable: false,
+                    layout: {
+                        adjustForNewBounds: true,
+                        moveHorizontal: true
+                    },
+                    name: "ChangeIndicator",
+                    savedColor: Color.green,
+                    indicateUnsavedChanges: function indicateUnsavedChanges() {
+                    if (this.get('FrameSource').hasUnsavedChanges()) {
+                        this.setColors(this.alarmColor);
+                    } else {
+                        this.setColors(this.savedColor);
+                    }
+                },
+                    setColors: function setColors(color) {
+                    this.setFill(color);
+                    this.setBorderColor(color);
                 }
+                }]
             },
-                reset: function reset() {
-                this.doNotSerialize = ["debugMarker"];
-            }
-            },{
+            {
                 _BorderColor: Color.rgb(230,230,230),
                 _BorderRadius: 0,
                 _BorderWidth: 1,
@@ -635,6 +683,7 @@ lively.BuildSpec('lively.ide.tools.Debugger', {
                 this.getWindow().setTitle(result.toString());
         } else
             this.setCurrentFrame(frame); // simple pc advancement
+        this.get('ChangeIndicator').indicateUnsavedChanges();
     },
         getFunctionName: function getFunctionName(func, obj) {
             var fn = func.asFunction(),

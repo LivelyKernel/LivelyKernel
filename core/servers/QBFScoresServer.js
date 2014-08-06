a9c57034aa49f4fa63b50488ac2a98e30da1c1f1
Dan Ingalls <danhhingalls@gmail.com>
2014-08-06 20:28:01 +0000

    QBF server update

--- core/servers/QBFScoresServer.js
+++ core/servers/QBFScoresServer.js
@@ -1,41 +1,44 @@
-module.exports = function(route, app) {
-app.get(route + ':person', function(req, res) {
-    // Retrieve a given player's scores
-    if (!fs.existsSync(filePath)) res.end("Cannot access scores");
-    else {
-        scoresString = fs.readFileSync(filePath);
-        scores = JSON.parse(scoresString);  // inspect(scores['Dan'])
-        result = JSON.stringify(scores[req.params.person] || {})
-        res.end(result);
-        }
-    });
-app.post(route + ':person', function(req, res) {
-    // Update a player's posted scores
-    if (!fs.existsSync(filePath)) res.end("Cannot access scores");
-    else {
-        scoresString = scores = newScores = LastReq = null;
-        LastReq = req;
-        scoresString = fs.readFileSync(filePath);
-        scores = JSON.parse(scoresString);
-        newScores = req.body;
-        scores[req.params.person] = newScores;
-        fs.writeFileSync(filePath, JSON.stringify(scores));
-        res.end("written new scores for " + req.params.person + " " + inspect(req.body));
-        }
-    });
-app.get(route, function(req, res) {
-    // Retrieve all scores as a pile of JSON text
+function doChecks() {
+    // make sure our file exissts
+    if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, '{}');
+    // reset scores at the end of the month
     if (pastMonth != new Date().getMonth()) {
-        // reset scores at the end of the month
         fs.writeFileSync(filePath, '{}');
         pastMonth = new Date().getMonth()
         }
-    if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, '{}');
+    };
+// --------
+
+module.exports = function(route, app) {
+app.get(route + ':person', function(req, res) {
+    // Retrieve a given player's scores
+    doChecks();
+    scoresString = fs.readFileSync(filePath);
+    scores = JSON.parse(scoresString);  // inspect(scores['Dan'])
+    result = JSON.stringify(scores[req.params.person] || {})
+    res.end(result);
+    });
+app.post(route + ':person', function(req, res) {
+    // Update a player's posted scores
+    doChecks();
+    scoresString = scores = newScores = LastReq = null;
+    LastReq = req;
+    scoresString = fs.readFileSync(filePath);
+    scores = JSON.parse(scoresString);
+    newScores = req.body;
+    scores[req.params.person] = newScores;
+    fs.writeFileSync(filePath, JSON.stringify(scores));
+    res.end("written new scores for " + req.params.person + " " + inspect(req.body));
+    });
+app.get(route, function(req, res) {
+    // Retrieve all scores as a pile of JSON text
+    doChecks();
     var content = fs.readFileSync(filePath);
     res.end(content);
     });
 };
 
+//  Initial settings
 pastMonth = new Date().getMonth();
 process.env.PWD;  // can print the process working directory
 path = require('path');
@@ -43,5 +46,3 @@ inspect = require('util').inspect;
 fs = require('fs');
 localFileName = 'QBFHighScores.json';
 filePath = path.join(process.env.WORKSPACE_LK, localFileName);
-
-// to reset scores:  fs.writeFileSync(filePath, '{}')

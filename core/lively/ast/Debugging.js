8f72735c61144f78c155e742051a87e50a3fd3fa
Marko Röder <m.roeder@photon-software.de>
2014-10-06 13:21:57 -0700

    Debugging: Moving flap definition

--- core/lively/ast/Debugging.js
+++ core/lively/ast/Debugging.js
@@ -20,129 +20,129 @@ Object.extend(lively.ast, {
 
 });
 
+lively.BuildSpec('lively.ast.DebuggingFlap', {
+    _FixedPosition: true,
+    _BorderRadius: 20,
+    _Extent: lively.pt(130.0,30.0),
+    _Fill: Color.rgba(255,255,255,0.8),
+    _HandStyle: "pointer",
+    className: "lively.morphic.Box",
+    currentMenu: null,
+    doNotSerialize: ["currentMenu"],
+    droppingEnabled: true,
+    grabbingEnabled: false,
+    isEpiMorph: true,
+    menu: null,
+    name: "lively.ast.DebuggingFlap",
+    style: {zIndex: 997},
+    statusText: {isMorphRef: true,name: "statusText"},
+    submorphs: [{
+        _Align: "center",
+        _ClipMode: "hidden",
+        _Extent: lively.pt(103.0,20.0),
+        _FontFamily: "Helvetica",
+        _HandStyle: "pointer",
+        _InputAllowed: false,
+        _Position: lively.pt(21.5,12.0),
+        allowInput: false,
+        className: "lively.morphic.Text",
+        evalEnabled: false,
+        eventsAreIgnored: true,
+        fixedHeight: true,
+        fixedWidth: true,
+        isLabel: true,
+        name: "statusText",
+        sourceModule: "lively.morphic.TextCore",
+        textString: "Debugging"
+    }],
+    alignInWorld: function alignInWorld() {
+    this.world().cachedWindowBounds = null;
+    var topRight = this.world().visibleBounds().topRight().addXY(-40, -10);
+    this.align(this.worldPoint(this.innerBounds().topRight().scaleBy(3)), topRight);
+    this.alignSubmorphs();
+},
+alignSubmorphs: function alignSubmorphs() {
+    this.statusText.align(this.statusText.bounds().center(), this.innerBounds().bottomCenter().addXY(0,-8));
+    if (lively.Config.get('loadRewrittenCode')) {
+        this.statusText.textString = 'In Debug Session';
+        this.statusText.applyStyle({textColor: Color.green.lighter()});
+    } else {
+        this.statusText.textString = 'Not Debugging';
+        this.statusText.applyStyle({textColor: Color.red});
+    }
+    this.menu && this.menu.align(
+        this.menu.bounds().bottomCenter(),
+        this.innerBounds().bottomCenter().addXY(2, -8-20));
+},
+    collapse: function collapse() {
+    this.withCSSTransitionForAllSubmorphsDo(function() {
+        this.setExtent(lively.pt(130.0,30.0));
+        this.alignSubmorphs();
+    }, 500, function() {
+        if (this.menu) {
+            this.menu.remove();
+            this.menu = null;
+        }
+    });
+},
+    expand: function expand() {
+    var self = this,
+        dbgStmt = !!lively.Config.get('enableDebuggerStatements'),
+        items = [
+        ['[' + (dbgStmt ? 'x' : ' ') + '] break on debugger', function() {
+            lively.Config.set('enableDebuggerStatements', !dbgStmt);
+            self.collapse();
+        }],
+        ['clear cache', function() {
+            if (!lively.IndexedDB.isAvailable()) return self.collapse();
+            lively.IndexedDB.hasStore('Debugging', function(err, exists) {
+                if (err) throw err;
+                if (exists)
+                    lively.IndexedDB.clear(function(err) {
+                        if (err)
+                            alert('Could not clear Debugging cache: ' + err.message);
+                        else
+                            alertOK('Cleared Debugging cache!');
+                    }, 'Debugging');
+                else
+                    alertOK('No cache to be cleared!');
+            });
+            self.collapse();
+        }]
+    ];
+    this.menu = new lively.morphic.Menu(null, items);
+    this.menu.openIn(this, pt(0,0), false);
+    this.menu.setBounds(lively.rect(0, -66, 130, 23*items.length));
+    this.withCSSTransitionForAllSubmorphsDo(function() {
+        this.setExtent(pt(140, 46 + 20*items.length));
+        this.alignSubmorphs();
+    }, 500, function() {});
+},
+    onFromBuildSpecCreated: function onFromBuildSpecCreated() {
+        $super();
+        this.onLoad();
+    },
+    onLoad: function onLoad() {
+    this.whenOpenedInWorld(function() {
+        this.alignInWorld();
+    });
+    this.openInWorld();
+    this.statusText.setHandStyle('pointer');
+    this.isEpiMorph = true;
+},
+    onMouseDown: function onMouseDown(evt) {
+    if (evt.getTargetMorph() !== this.statusText && evt.getTargetMorph() !== this) return false;
+    if (this.menu) this.collapse();
+    else this.expand();
+    evt.stop(); return true;
+},
+    onWorldResize: function onWorldResize() {
+    this.alignInWorld();
+}
+});
+
 (function openDebuggingFlap() {
     lively.whenLoaded(function(world) {
-        lively.BuildSpec('lively.ast.DebuggingFlap', {
-            _FixedPosition: true,
-            _BorderRadius: 20,
-            _Extent: lively.pt(130.0,30.0),
-            _Fill: Color.rgba(255,255,255,0.8),
-            _HandStyle: "pointer",
-            className: "lively.morphic.Box",
-            currentMenu: null,
-            doNotSerialize: ["currentMenu"],
-            droppingEnabled: true,
-            grabbingEnabled: false,
-            isEpiMorph: true,
-            menu: null,
-            name: "lively.ast.DebuggingFlap",
-            style: {zIndex: 997},
-            statusText: {isMorphRef: true,name: "statusText"},
-            submorphs: [{
-                _Align: "center",
-                _ClipMode: "hidden",
-                _Extent: lively.pt(103.0,20.0),
-                _FontFamily: "Helvetica",
-                _HandStyle: "pointer",
-                _InputAllowed: false,
-                _Position: lively.pt(21.5,12.0),
-                allowInput: false,
-                className: "lively.morphic.Text",
-                evalEnabled: false,
-                eventsAreIgnored: true,
-                fixedHeight: true,
-                fixedWidth: true,
-                isLabel: true,
-                name: "statusText",
-                sourceModule: "lively.morphic.TextCore",
-                textString: "Debugging"
-            }],
-            alignInWorld: function alignInWorld() {
-            this.world().cachedWindowBounds = null;
-            var topRight = this.world().visibleBounds().topRight().addXY(-40, -10);
-            this.align(this.worldPoint(this.innerBounds().topRight().scaleBy(3)), topRight);
-            this.alignSubmorphs();
-        },
-        alignSubmorphs: function alignSubmorphs() {
-            this.statusText.align(this.statusText.bounds().center(), this.innerBounds().bottomCenter().addXY(0,-8));
-            if (lively.Config.get('loadRewrittenCode')) {
-                this.statusText.textString = 'In Debug Session';
-                this.statusText.applyStyle({textColor: Color.green.lighter()});
-            } else {
-                this.statusText.textString = 'Not Debugging';
-                this.statusText.applyStyle({textColor: Color.red});
-            }
-            this.menu && this.menu.align(
-                this.menu.bounds().bottomCenter(),
-                this.innerBounds().bottomCenter().addXY(2, -8-20));
-        },
-            collapse: function collapse() {
-            this.withCSSTransitionForAllSubmorphsDo(function() {
-                this.setExtent(lively.pt(130.0,30.0));
-                this.alignSubmorphs();
-            }, 500, function() {
-                if (this.menu) {
-                    this.menu.remove();
-                    this.menu = null;
-                }
-            });
-        },
-            expand: function expand() {
-            var self = this,
-                dbgStmt = !!lively.Config.get('enableDebuggerStatements'),
-                items = [
-                ['[' + (dbgStmt ? 'x' : ' ') + '] break on debugger', function() {
-                    lively.Config.set('enableDebuggerStatements', !dbgStmt);
-                    self.collapse();
-                }],
-                ['clear cache', function() {
-                    if (!lively.IndexedDB.isAvailable()) return self.collapse();
-                    lively.IndexedDB.hasStore('Debugging', function(err, exists) {
-                        if (err) throw err;
-                        if (exists)
-                            lively.IndexedDB.clear(function(err) {
-                                if (err)
-                                    alert('Could not clear Debugging cache: ' + err.message);
-                                else
-                                    alertOK('Cleared Debugging cache!');
-                            }, 'Debugging');
-                        else
-                            alertOK('No cache to be cleared!');
-                    });
-                    self.collapse();
-                }]
-            ];
-            this.menu = new lively.morphic.Menu(null, items);
-            this.menu.openIn(this, pt(0,0), false);
-            this.menu.setBounds(lively.rect(0, -66, 130, 23*items.length));
-            this.withCSSTransitionForAllSubmorphsDo(function() {
-                this.setExtent(pt(140, 46 + 20*items.length));
-                this.alignSubmorphs();
-            }, 500, function() {});
-        },
-            onFromBuildSpecCreated: function onFromBuildSpecCreated() {
-                $super();
-                this.onLoad();
-            },
-            onLoad: function onLoad() {
-            this.whenOpenedInWorld(function() {
-                this.alignInWorld();
-            });
-            this.openInWorld();
-            this.statusText.setHandStyle('pointer');
-            this.isEpiMorph = true;
-        },
-            onMouseDown: function onMouseDown(evt) {
-            if (evt.getTargetMorph() !== this.statusText && evt.getTargetMorph() !== this) return false;
-            if (this.menu) this.collapse();
-            else this.expand();
-            evt.stop(); return true;
-        },
-            onWorldResize: function onWorldResize() {
-            this.alignInWorld();
-        }
-        });
-
         lively.BuildSpec('lively.ast.DebuggingFlap').createMorph().openInWorld();
     });
 })();

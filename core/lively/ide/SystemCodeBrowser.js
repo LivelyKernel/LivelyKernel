a07a591a2b968ff99a77034a5abe7e63f265d56a
Robert Krahn <robert.krahn@gmail.com>
2014-10-27 05:34:33 +0000

    ide: fixing browseURL

--- core/lively/ide/SystemCodeBrowser.js
+++ core/lively/ide/SystemCodeBrowser.js
@@ -287,7 +287,7 @@ Object.extend(lively.ide, {
         // 3. path (String) relative to URL.root, optional browser instance
 
         var args = Array.from(arguments);
-        if (args.length === 0) { this.openSystemCodeBrowser(); return }
+        if (args.length === 0) { this.openSystemCodeBrowser(); return; }
         if (args.length <= 2) { // url or path or options object
             var url = args[0].toString().startsWith('http:') ?
                 new URL(args[0]) : URL.root.withFilename(args[0]);
@@ -337,17 +337,18 @@ Object.extend(lively.ide, {
     },
 
     browseURL: function(url, browser) {
+        var path = lively.ide.sourceDB().mapURLsToRelativeModulePaths([url]).first();
+        if (url.isLeaf() && !lively.ide.sourceDB().addModule(path).exists())
+            return lively.ide.openFile(url);
+
         browser = browser || this.openSystemCodeBrowser();
         if (url.isLeaf()) {
             var dir = url.getDirectory(),
                 fileName = url.filename();
             browser.setTargetURL(dir);
             browser.selectNodeMatching(function(node) {
-                return node && node.url && node.url().filename() == fileName;
-            })
-        } else {
-            browser.setTargetURL(url);
-        }
+                return node && node.url && node.url().filename() == fileName; });
+        } else browser.setTargetURL(url);
         browser.panel.getWindow().comeForward();
         return browser;
     },

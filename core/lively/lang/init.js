804769d9ce4074f573f14ddde4dfd47ec312dc13
Robert Krahn <robert.krahn@gmail.com>
2014-10-28 00:05:07 -0700

    lively.lang: init code

---
+++ core/lively/lang/init.js
@@ -0,0 +1,9 @@
+jsext.installGlobals();
+
+window.$A = Array.from;
+
+// We need to redefine Function.evalJS here b/c the original definition is
+// in a JS 'use strict' block. However, not all function sources we pass in
+// #evalJS from Lively adhere to the strictness rules. To allow those
+// functions for now we define the creator again outside of a strictness block.
+Function.evalJS = jsext.fun.evalJS = function(src) { return eval(src); }

736494a8cea8842270d8d370c9c1a26b53fa0724
Robert Krahn <robert.krahn@gmail.com>
2014-11-14 19:42:06 -0800

    lively.lang: switching definition of library to new config scheme

--- core/lively/lang/init.js
+++ core/lively/lang/init.js
@@ -1,1 +1,3 @@
-jsext.deprecatedLivelyPatches();
+var isNodejs = typeof UserAgent !== "undefined" && UserAgent.isNodejs;
+var livelyLang = isNodejs ? require("lively.lang") : Global.jsext;
+livelyLang.deprecatedLivelyPatches();

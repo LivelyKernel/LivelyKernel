4913fc5decacf3a596fa1d220b5c71be5c5bde3c
Robert Krahn <robert.krahn@gmail.com>
2014-12-18 09:34:52 +0000

    World>>selectMorphWithNextClick

--- core/lively/morphic/Widgets.js
+++ core/lively/morphic/Widgets.js
@@ -2209,7 +2209,31 @@ lively.morphic.World.addMethods(
 
     editPrompt: function(message, callback, defaultInputOrOptions) {
         return this.openDialog(new lively.morphic.EditDialog(message, callback, defaultInputOrOptions))
-    }
+    },
+
+    selectMorphWithNextClick: function(options, thenDo) {
+        if (typeof options === "function") { thenDo = options; options = null; }
+        options = options || {};
+
+        lively.lang.fun.replaceMethodForOneCall($world, "onMouseDown", function(evt) {
+          onClick(); evt.stop(); return true; });
+        lively.lang.fun.replaceMethodForOneCall($world, "onMouseUp", function(evt) {
+          evt.stop(); return true; });
+
+        function onClick() {
+          var pos = $world.hand.getPosition(),
+              morphs = $world.morphsContainingPoint(pos).without($world)
+              .filter(function(ea) {
+                return ea.isVisible()
+                    && ea.ownerChain().every(function(owner) {
+                      return owner.isVisible(); })})
+          if (morphs.length === 0) return thenDo && thenDo(null, null);
+          if (options.useMenu)
+            lively.morphic.Menu.openAtHand("select morph",
+              morphs.map(function(ea) { return [String(ea), function() { thenDo && thenDo(null, ea); }]; }));
+          else thenDo && thenDo(null, morphs[0]);
+        }
+      }
 
 },
 'progress bar', {

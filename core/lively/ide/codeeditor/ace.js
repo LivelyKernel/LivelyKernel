46b66975c16ebaff85dfd3dc2dd72dc48b748769
jenslincke <jens.lincke@hpi.uni-potsdam.de>
2015-03-17 17:52:46 +0000

    fix scaled interaction with ace based CodeEditor

--- core/lively/ide/codeeditor/ace.js
+++ core/lively/ide/codeeditor/ace.js
@@ -49,6 +49,21 @@ module('lively.ide.codeeditor.ace').requires('lively.Network'/*to setup lib*/).r
         });
         this.textInput.focus();
         Global.scrollTo(x, y)
+    };
+    
+    
+ace.require("ace/virtual_renderer").VirtualRenderer.prototype.screenToTextCoordinates = function (x, y) {
+        var canvasPos = this.scroller.getBoundingClientRect();
+        var scale = lively.morphic.World.current().getScale()
+        x = x / scale;
+        y = y / scale;
+        var col = Math.round(
+            (x + this.scrollLeft - (canvasPos.left / scale) - this.$padding) / this.characterWidth
+        );
+
+        var row = (y + this.scrollTop - (canvasPos.top / scale)) / this.lineHeight;
+
+        return this.session.screenToDocumentPosition(row, Math.max(col, 0));
     }
 
 })();

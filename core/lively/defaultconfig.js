f8c9bb508820d38d1e5a5fc08cf30215a386243b
Marko Röder <m.roeder@photon-software.de>
2015-04-02 18:22:42 +0000

    Making user config override settings on boot again (even without quickLoad=false)

--- core/lively/defaultconfig.js
+++ core/lively/defaultconfig.js
@@ -1,5 +1,5 @@
 /**
- * defaultconfig.js.  System default configuration. Reads config.js
+ * defaultconfig.js.  System default configuration. Reads config.json
  *
  * Note that if the files localconfig.json and/or localconfig.js can be found,
  * those will be read immediately after this one, thus allowing any of these settings
@@ -193,7 +193,8 @@ Global.Config = {
         var userName = optUsername || this.get('UserName');
         if (!userName || userName === "undefined") return;
         var userConfigModule = Strings.format('users.%s.config', userName);
-        lively.require(userConfigModule).toRun(this.urlQueryOverride.bind(this));
+        lively.module(userConfigModule).load(true);
+        this.urlQueryOverride();
     },
 
     set: function(name, value) {

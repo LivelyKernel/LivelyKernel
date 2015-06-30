a128a19605dca5d7dbc7cf5979f6ea3af7ea3bcb
Marko Röder <m.roeder@photon-software.de>
2015-06-30 11:28:14 -0700

    FileUpload: Putting warning into big image uploads

--- core/lively/data/ImageUpload.js
+++ core/lively/data/ImageUpload.js
@@ -2,6 +2,8 @@ module('lively.data.ImageUpload').requires('lively.data.FileUpload').toRun(funct
 
 lively.data.FileUpload.Handler.subclass('lively.Clipboard.ImageUploader', {
 
+    uploadThreshold: 1024*1024*1.5, // 1.5MB
+
     handles: function(file) {
         return file.type.match(/image\/.*/);
     },
@@ -9,7 +11,7 @@ lively.data.FileUpload.Handler.subclass('lively.Clipboard.ImageUploader', {
     htmlWrapsImage: function(evt) {
         // when dropping images from one browser page to another we receive a
         // drop event with an item. The item has the mime type html, even if an
-        // image and not a whole DOM selection was dragged&dropped. Howver, in
+        // image and not a whole DOM selection was dragged&dropped. However, in
         // those cases only the image element is the only meaningful element.
         // This method recognizes that.
         var elems = this.getHTMLElementsFromDataTransfer(evt);
@@ -45,8 +47,15 @@ lively.data.FileUpload.Handler.subclass('lively.Clipboard.ImageUploader', {
                 URL.source.withFilename(this.file.name),
                 this.file.type, evt.target.result, this.pos);
         } else {
-            var img = new lively.morphic.Image(this.pos.extent(pt(200,200)), evt.target.result, true).openInWorld();
-            img.name = this.file.name;
+            if ((typeof evt.total == 'number') && (evt.total > this.uploadThreshold)) {
+                var size = Numbers.humanReadableByteSize(evt.total);
+                $world.confirm('WARNING: Uploaded file is rather big (' + size + ').\n' +
+                'Do you want to continue uploading?', function(result) {
+                    if (result === 0)
+                        this.openImage(evt.target.result, this.file.type, this.pos, this.file.name);
+                }.bind(this), ['Yes', 'No']);
+            } else
+            this.openImage(evt.target.result, this.file.type, this.pos, this.file.name);
         }
     },
 
@@ -59,8 +68,8 @@ lively.data.FileUpload.Handler.subclass('lively.Clipboard.ImageUploader', {
         });
     },
 
-    openImage: function(url, mime, pos) {
-        var name = new URL(url).filename(),
+    openImage: function(url, mime, pos, optName) {
+        var name = optName || new URL(url).filename(),
             img = new lively.morphic.Image(pos.extent(pt(200,200)), url, true).openInWorld();
         img.name = name;
     }

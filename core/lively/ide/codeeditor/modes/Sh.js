276dddc8a5e7b788dc054c48461cc932b0939d2e
Robert Krahn <robert.krahn@gmail.com>
2015-08-29 15:02:15 -0700

    ide/shell: shell workspace now shows process output in statusmorph

--- core/lively/ide/codeeditor/modes/Sh.js
+++ core/lively/ide/codeeditor/modes/Sh.js
@@ -3,16 +3,102 @@ module('lively.ide.codeeditor.modes.Sh').requires('lively.ide.codeeditor.ace').t
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // Shell-mode
 lively.ide.ace.require('ace/mode/sh').Mode.addMethods({
+
+    commands: {
+        "lively.shell.Workspace.openRunningCommand": {
+            exec: function(ed) {
+              var mode = ed.session.getMode();
+              var cmd = (mode.getRunningProcessesOf(ed.$morph) || []).last();
+              if (cmd) mode.openProcessOf(ed.$morph, cmd);
+              else if (ed.$morph._statusMorph) ed.$morph._statusMorph.expand();
+              else ed.$morph.setStatusMessage("No process running");
+            }
+        }
+    },
+
+    keybindings: {
+        "lively.shell.Workspace.openRunningCommand": {mac: "Command-Shift-o", win: "Ctrl-Shift-o"}
+    },
+
+    keyhandler: null,
+
+    initKeyHandler: function() {
+        Properties.forEachOwn(this.keybindings, function(commandName, key) {
+            if (this.commands[commandName]) this.commands[commandName].bindKey = key;
+        }, this);
+        return this.keyhandler = lively.ide.ace.createKeyHandler({
+            keyBindings: this.keybindings,
+            commands: this.commands
+        });
+    },
+
+    attach: function(ed) {
+        this.initKeyHandler();
+        ed.keyBinding.addKeyboardHandler(this.keyhandler);
+    },
+
+    detach: function(ed) {
+        this.keyhandler = null;
+        ed.keyBinding.removeKeyboardHandler(this.keyhandler);
+    },
+
+    groupNameForCommand: function(editor) {
+      return "shell-doit-" + editor.id;
+    },
+
+    getRunningProcessesOf: function(editor) {
+        var group = this.groupNameForCommand(editor);
+        return lively.shell.getGroupCommandQueue(group);
+    },
+
+    openProcessOf: function(editor, cmd, thenDo) {
+      lively.lang.fun.composeAsync(
+        function(n) {
+          var m = module("lively.ide.tools.ShellCommandRunner");
+          if (m.isLoaded()) n();
+          else require("lively.ide.tools.ShellCommandRunner").toRun(function() { n(); });
+        },
+        function(n) {
+          lively.ide.tools.ShellCommandRunner.forCommand(cmd).openInWorldCenter().comeForward();
+          n();
+        }
+      )(thenDo);
+    },
+
     doEval: function(codeEditor, insertResult) {
+        var mode = this;
         lively.ide.commands.exec('lively.ide.execShellCommand', codeEditor, {
             insert: insertResult,
-            insertProgress: true,
+            showProgress: true,
             count: 4,
             addToHistory: true,
-            group: "shell-doit-" - codeEditor.id,
+            group: mode.groupNameForCommand(codeEditor),
             shellCommand: codeEditor.getSelectionOrLineString()
         });
-    }
+    },
+
+    morphMenuItems: function(items, editor) {
+        var mode = this,
+            s = editor.getSession(),
+            processes = mode.getRunningProcessesOf(editor);
+
+        items.push([
+          "running processes",
+          processes.length ?
+            processes.map(function(cmd) {
+              return [String(cmd), [
+                ["kill", function() {
+                  lively.shell.run("ls").kill("SIGKILL", function() {
+                    editor.setStatusMessage(cmd + " was killed");
+                  })
+                }],
+                ["open ", function() { mode.openProcessOf(editor, cmd); }]
+              ]]
+            }) : [["no process running", function() {}]]
+        ]);
+        return items;
+    },
+
 });
 
 }) // end of module
--- core/lively/ide/commands/default.js
+++ core/lively/ide/commands/default.js
@@ -1088,7 +1088,7 @@ Object.extend(lively.ide.commands.byName, {
         description: 'execute shell command',
         exec: function(codeEditor, args) {
             var insertResult   = !args || typeof args.insert === 'undefined' || !!args.insert,
-                insertProgress = args  && !!args.insertProgress,
+                showProgress   = args  && !!args.showProgress,
                 openInWindow   = !codeEditor || (args && args.count !== 4)/*universal argument*/,
                 addToHistory   = args && args.addToHistory,
                 group          = (args && args.group) || 'interactive-shell-command',
@@ -1117,35 +1117,70 @@ Object.extend(lively.ide.commands.byName, {
             }
 
             function runCommand(command) {
-                var ed = ensureCodeEditor(command), mergeUndos;
+                var ed = ensureCodeEditor(command),
+                    mergeUndos, connections = [],
+                    title = ed.getWindow().getTitle(),
+                    msgMorph;
 
-                var title = ed.getWindow().getTitle();
                 if (ed.getWindow()) ed.getWindow().setTitle("[running] " + title);
 
-                ed.mergeUndosOf(function(triggerMerge) {
-                    mergeUndos = triggerMerge;
+                var cmd = lively.shell.run(command, {addToHistory: addToHistory, group: group}, function(err, cmd) {
+                  connections.invoke("disconnect");
+                  var result = cmd.resultString(true);
 
-                    var cmd = lively.shell.run(command, {addToHistory: addToHistory, group: group}, function(err, cmd) {
-                        if (!insertProgress && insertResult)
-                            ed.printObject(null, cmd.resultString(true));
+                  if (!showProgress && insertResult) ed.printObject(null, result);
+                  if (msgMorph) {
+                    msgMorph.insertion = result;
+                    msgMorph.appendRichText("\nExited with " + cmd.getCode());
+                    msgMorph.enableRemoveOnTargetMorphChange();
+                    msgMorph.ensureOpenFor(ed);
+                  }
+                  if (ed.getWindow()) ed.getWindow().setTitle(title);
 
-                        if (ed.getWindow()) ed.getWindow().setTitle(title);
-
-                        mergeUndos && mergeUndos();
-                    });
-
-                    if (insertProgress) {
-                        ed.collapseSelection('end');
-                        ed.addScript(function insertAndGrowSelection(string) {
-                            var rangeBefore = this.getSelectionRangeAce();
-                            this.printObject(null,string);
-                            var rangeAfter = this.getSelectionRangeAce();
-                            this.setSelectionRangeAce({start: rangeBefore.start, end: rangeAfter.end});
-                        });
-                        lively.bindings.connect(cmd, 'stdout', ed, 'insertAtCursor', {updater: function($upd, string) { $upd(string, false, false, true); }});
-                        lively.bindings.connect(cmd, 'stderr', ed, 'insertAtCursor', {updater: function($upd, string) { $upd(string, false, false, true); }});
-                    }
+                  mergeUndos && mergeUndos();
                 });
+
+                if (insertResult) {
+                  ed.mergeUndosOf(function(triggerMerge) {
+                      mergeUndos = triggerMerge;
+                      if (showProgress) {
+                          ed.collapseSelection('end');
+                          ed.addScript(function insertAndGrowSelection(string) {
+                              var rangeBefore = this.getSelectionRangeAce();
+                              this.printObject(null,string);
+                              var rangeAfter = this.getSelectionRangeAce();
+                              this.setSelectionRangeAce({start: rangeBefore.start, end: rangeAfter.end});
+                          });
+                          connections.pushAll([
+                            lively.bindings.connect(cmd, 'stdout', ed, 'insertAtCursor', {updater: function($upd, string) { $upd(string, false, false, true); }}),
+                            lively.bindings.connect(cmd, 'stderr', ed, 'insertAtCursor', {updater: function($upd, string) { $upd(string, false, false, true); }})]);
+                      }
+                  });
+                } else {
+                  if (showProgress) {
+                    // show progress in status message morph, don't modify source text
+                    msgMorph = ed.ensureStatusMessageMorph();
+                    msgMorph.disableRemoveOnTargetMorphChange();
+                    if (!msgMorph.owner) { ed.setStatusMessage(""); }
+  
+                    var connectionSpec = {
+                      updater: function($upd, newContent) {
+                        if (this.targetObj.textString.length > 1000) {
+                          newContent = "...";
+                          connections.invoke("disconnect");
+                        }
+                        $upd(newContent, {});
+                        this.targetObj.alignAtBottomOf(ed);
+                      },
+                      varMapping: {ed: ed, connections: connections}
+                    }
+  
+                    connections.pushAll([
+                      lively.bindings.connect(cmd, 'stdout', msgMorph, 'appendRichText', connectionSpec),
+                      lively.bindings.connect(cmd, 'stderr', msgMorph, 'appendRichText', connectionSpec)]);
+                  }
+
+                }
             }
 
         }
@@ -1208,11 +1243,11 @@ Object.extend(lively.ide.commands.byName, {
     },
     'lively.ide.CommandLineInterface.showRunningShellCommands': {
         description: 'show shell command processes',
-        exec: function() {
-            var ed =$world.addCodeEditor({
-                title: 'Running commands',
-                textMode: 'text'
-            });
+        exec: function(opts) {
+            opts = opts || {};
+
+            var ed = $world.addCodeEditor({title: 'Running commands', textMode: 'text'});
+
             ed.addScript(function update() {
                 // this.startStepping(100, 'update')
                 var q = lively.ide.CommandLineInterface.commandQueue;
@@ -1349,7 +1384,7 @@ Object.extend(lively.ide.commands.byName, {
 
             var editors;
 
-            Functions.composeAsync(
+            lively.lang.fun.composeAsync(
                 loadRequiredModules,
                 fetchEditorsIfRequired,
                 selectEditor1,

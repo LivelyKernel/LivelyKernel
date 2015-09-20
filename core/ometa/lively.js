a45008e032356b34bbe8386c33fe8ebd576d1eb1
Robert Krahn <robert.krahn@gmail.com>
2015-09-20 23:10:01 +0000

    cleanup

--- core/ometa/lively.js
+++ core/ometa/lively.js
@@ -3,14 +3,14 @@
 // supported by lively.Module>>requireLib
 var allOMetaDependenciesLoaded = false;
 var dependencies = [
-    {url: Config.codeBase + 'ometa/lib.js', loadTest: function() { return typeof objectThatDelegatesTo !== 'undefined'; }},
-    {url: Config.codeBase + 'ometa/ometa-base.js', loadTest: function() { return typeof OMeta !== 'undefined'; }},
-    {url: Config.codeBase + 'ometa/parser.js', loadTest: function() { return typeof Parser !== 'undefined'; }},
-    {url: Config.codeBase + 'ometa/ChunkParser.js', loadTest: function() { return typeof ChunkParser !== 'undefined'; }},
-    {url: Config.codeBase + 'ometa/bs-ometa-compiler.js', loadTest: function() { return typeof BSOMetaParser !== 'undefined'; }},
-    {url: Config.codeBase + 'ometa/bs-js-compiler.js', loadTest: function() { return typeof BSJSParser !== 'undefined'; }},
+    {url: Config.codeBase + 'ometa/lib.js',                  loadTest: function() { return typeof objectThatDelegatesTo !== 'undefined'; }},
+    {url: Config.codeBase + 'ometa/ometa-base.js',           loadTest: function() { return typeof OMeta !== 'undefined'; }},
+    {url: Config.codeBase + 'ometa/parser.js',               loadTest: function() { return typeof Parser !== 'undefined'; }},
+    {url: Config.codeBase + 'ometa/ChunkParser.js',          loadTest: function() { return typeof ChunkParser !== 'undefined'; }},
+    {url: Config.codeBase + 'ometa/bs-ometa-compiler.js',    loadTest: function() { return typeof BSOMetaParser !== 'undefined'; }},
+    {url: Config.codeBase + 'ometa/bs-js-compiler.js',       loadTest: function() { return typeof BSJSParser !== 'undefined'; }},
     {url: Config.codeBase + 'ometa/bs-ometa-js-compiler.js', loadTest: function() { return typeof BSOMetaJSParser !== 'undefined'; }},
-    {url: Config.codeBase + 'ometa/bs-ometa-optimizer.js', loadTest: function() { return typeof BSOMetaOptimizer !== 'undefined'; }},
+    {url: Config.codeBase + 'ometa/bs-ometa-optimizer.js',   loadTest: function() { return typeof BSOMetaOptimizer !== 'undefined'; }},
     {url: Config.codeBase + 'ometa/lk-parser-extensions.js', loadTest: function() { return typeof LKJSParser !== 'undefined'; }}
 ];
 

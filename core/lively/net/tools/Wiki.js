ea92d77ab7cfe8e2860390e4248e1dd57d057981
Robert Krahn <robert.krahn@gmail.com>
2015-12-16 14:57:30 -0800

    users: wiki menu bar entry shows current user name

--- core/lively/net/tools/Wiki.js
+++ core/lively/net/tools/Wiki.js
@@ -25,8 +25,32 @@ lively.BuildSpec('lively.wiki.MenuBarEntry', lively.BuildSpec('lively.morphic.to
             ['Show login info', cmd('lively.net.wiki.tools.showLoginInfo')],
             ['World versions', cmd('lively.ide.openVersionsViewer')]
         ];
-    }
+    },
 
+    update: function update() {
+      var menuBarItem = this;
+      $world.getUserName(true, function(err, user) {
+        if (user === "undefined" || user === "unknown_user") user = null;
+        var label = user ? user : "not logged in";
+        menuBarItem.textString = label;
+
+        var morphsLeft = menuBarItem.owner.submorphs.without(menuBarItem).filter(function(ea) { return ea.bounds().right() < menuBarItem.bounds().left() && ea.menuBarAlign === "right"; });
+        var morphRight = menuBarItem.owner.submorphs.without(menuBarItem).min(function(ea) { return Math.abs(ea.bounds().left() - menuBarItem.bounds().right()); });
+
+        menuBarItem.applyStyle({fixedWidth: false});
+        var oldExtent = menuBarItem.getExtent();
+        menuBarItem.fitThenDo(function() {
+          menuBarItem.applyStyle({fixedWidth: true});
+          var delta = menuBarItem.getExtent().subPt(oldExtent);
+          menuBarItem.align(menuBarItem.bounds().topRight(), morphRight.bounds().topLeft());
+          morphsLeft.invoke("moveBy", delta.negated().withY(0));
+        });
+      });
+    },
+
+    onLoad: function onLoad() { this.update.bind(this).delay(.4); this.startStepping(10 * 1000, "update"); },
+
+    onFromBuildSpecCreated: function onFromBuildSpecCreated() { this.onLoad(); }
 }));
 
 lively.BuildSpec("lively.wiki.LoginInfo", {
@@ -832,7 +856,7 @@ lively.BuildSpec('lively.wiki.VersionViewer', {
         if (!path) { $world.inform('No resource selected'); return; }
         var sel = this.get('VersionList').selection;
         if (!sel) { $world.inform('No version selected'); return; }
-        
+
         var prompt = 'Do you really want to revert \n'
                     + path
                     + '\nto its version from\n'

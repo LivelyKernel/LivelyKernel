6a202398a212b19af783148f0429f48cba5362b3
Robert Krahn <robert.krahn@gmail.com>
2016-10-04 18:01:44 +0000

    removing core/lively/fixes.js, lively.ast should now wor withou it

--- core/lively/fixes.js
+++
@@ -1,7 +0,0 @@
-// rk 2016-09-20
-// Fix for using a newer version of Lively with the old module system
-var ast = lively.ast, acorn = ast.acorn;
-delete lively.ast;
-Object.assign(lively.module("lively.ast"), ast, {acorn: Object.assign(lively.module("lively.ast.acorn"), acorn)});
-lively.module("lively.ast").isLoaded = lively.lang.fun.True;
-lively.module("lively.ast.acorn").isLoaded = lively.lang.fun.True;
